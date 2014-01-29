4cdbaca856ac61b122459168e8d3ed7a49452d6f
Kirk Swenson <kswenson@kcptech.com>
2014-01-28 20:43:38 -0800

    Build 0231

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -195,7 +195,7 @@ DG = SC.Application.create( (function() // closure
   /*
    * Build number
    */
-  BUILD_NUM: '0230',
+  BUILD_NUM: '0231',
 
   /**
    * The subdomain for the Drupal site which must be hosted on the same domain.  This is used for various interactions
