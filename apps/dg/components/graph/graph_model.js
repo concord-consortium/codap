ff1c2a1f2e7f13514d3bda80e93ab5c4bffab236
Bill Finzer <bfinzer@kcptech.com>
2014-05-28 14:40:34 -0700

    Refactored GraphModel to use DataDisplayModel as a base class

--- apps/dg/components/graph/graph_model.js
+++ apps/dg/components/graph/graph_model.js
@@ -18,49 +18,17 @@
 //  limitations under the License.
 // ==========================================================================
 
-sc_require('alpha/destroyable');
+sc_require('components/graph_map_common/data_display_model');
 
 /** @class  DG.GraphModel - The model for a graph.
 
- @extends SC.Object
+ @extends DG.DataDisplayModel
  */
-DG.GraphModel = SC.Object.extend( DG.Destroyable,
+DG.GraphModel = DG.DataDisplayModel.extend(
   /** @scope DG.GraphModel.prototype */
   {
-    autoDestroyProperties: [ 'dataConfiguration', 'plot', 'xAxis', 'yAxis', 'legend' ],
+    autoDestroyProperties: [ 'plot', 'xAxis', 'yAxis', 'legend' ],
     
-    /**
-     @property { DG.GraphDataConfiguration }
-     */
-    dataConfiguration: null,
-
-    /**
-      @property { DG.CollectionClient }
-    */
-    collectionClient: null,
-    collectionClientBinding: '*dataConfiguration.collectionClient',
-
-    /**
-      The plot model needs access to the cases controller that is stored in my dataConfiguration's
-        collection client.
-      @property { SC.ArrayController }
-    */
-    casesController: null,
-    casesControllerBinding: '*dataConfiguration.collectionClient.casesController',
-
-    /**
-     * @property {Array} of DG.Case
-     */
-    cases: null,
-    casesBinding: '*dataConfiguration.cases',
-
-    /**
-      @property { SC.SelectionSet }
-    */
-    selection: function() {
-      return this.getPath('dataConfiguration.selection');
-    }.property('dataConfiguration.selection'),
-
     /**
      * @property {DG.NumberToggleModel}
      */
@@ -194,13 +162,6 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
     },
 
     /**
-     Work around current notification bug whereby we get notified that number of cases has
-     changed even when it hasn't.
-     @property {Number}
-     */
-    _oldNumberOfCases: 0,
-
-    /**
      Prepare dependencies.
      */
     init: function() {
@@ -217,9 +178,10 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
           return DG.AxisModel;
       }
 
+      this.set( 'dataConfiguration', DG.GraphDataConfiguration.create() );
+
       this._plots = [];
 
-      this.set( 'dataConfiguration', DG.GraphDataConfiguration.create() );
       if( DG.IS_INQUIRY_SPACE_BUILD) {
         this.set('numberToggle', DG.NumberToggleModel.create( { dataConfiguration: this.get('dataConfiguration')}));
       }
@@ -246,71 +208,7 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
     },
 
     destroy: function() {
-      if( this._dataContext)
-         this._dataContext.removeObserver('changeCount', this, 'handleDataContextNotification');
       sc_super();
-    },
-
-    /**
-      The data context for the graph. Set by caller after construction initially and
-      reset for graphs restored from document to point to the restored data context.
-      @property   {DG.DataContext}
-     */
-    _dataContext: null,
-    dataContext: function( iKey, iValue) {
-      // We use a computed property so that we can add/remove observers when necessary.
-      if( iValue) {
-        if( iValue !== this._dataContext) {
-          if( this._dataContext){
-             this._dataContext.removeObserver('changeCount', this, 'handleDataContextNotification');
-          }
-          this._dataContext = iValue;
-          if( this._dataContext) {
-            this._dataContext.addObserver('changeCount', this, 'handleDataContextNotification');
-          }
-        }
-        return this;
-      }
-      return this._dataContext;
-    }.property(),
-
-    /**
-      Called when the 'dataContext' property is changed.
-     */
-    dataContextDidChange: function() {
-      var dataConfiguration = this.get('dataConfiguration');
-      if( dataConfiguration)
-        dataConfiguration.set('dataContext', this.get('dataContext'));
-    }.observes('dataContext'),
-
-    /**
-      Delete the currently selected cases.
-      Passes the request on to the data context to do the heavy lifting.
-     */
-    deleteSelectedCases: function() {
-      var tChange = {
-            operation: 'deleteCases',
-            cases: DG.copy( this.get('selection'))
-          };
-      this.get('dataContext').applyChange( tChange);
-    },
-
-    /**
-      Select/deselect all of the points in all the plots.
-      @param  {Boolean}   iSelect -- True to select all points, false to deselect all.
-                                      Defaults to true (select all) if undefined/null.
-     */
-    selectAll: function( iSelect) {
-      var tSelect = SC.none( iSelect) ? true : !!iSelect,
-          tCases = tSelect ? this.get('cases') : null,  // null means all cases, even hidden ones
-          tChange = {
-            operation: 'selectCases',
-            collection: this.get('collectionClient'),
-            cases: tCases,
-            select: tSelect
-          };
-      this.get('dataContext').applyChange( tChange);
-      DG.logUser( iSelect ? "selectAll" : "deselectAll");
     },
 
     /**
@@ -613,27 +511,6 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
           tCurrentYAxisClass = tPrevYAxis.constructor,
           tDataConfig = this.get('dataConfiguration');
 
-      function instantiateAttributeRef( iCollLinkName, iAttrLinkName) {
-        var kNullResult = { collection: null, attributes: [] };
-        if( SC.empty( iCollLinkName) || SC.empty( iAttrLinkName) || !iStorage || !iStorage._links_)
-          return kNullResult;
-          
-        var collLink = iStorage._links_[ iCollLinkName],
-            coll = collLink && collLink.id && tDataContext.getCollectionByID( collLink.id),
-            attrs = [],
-            tLinkCount = DG.ArchiveUtils.getLinkCount( iStorage, iAttrLinkName ),
-            tIndex;
-        if( !coll)
-          return kNullResult;
-
-        for( tIndex = 0; tIndex < tLinkCount; tIndex++) {
-          var tAttr = coll.getAttributeByID( DG.ArchiveUtils.getLinkID( iStorage, iAttrLinkName, tIndex));
-          if( tAttr)
-            attrs.push(tAttr);
-        }
-        return { collection: coll, attributes: attrs };
-      }
-
       var instantiateArrayOfPlots = function( iPlots) {
         iPlots.forEach( function( iModelDesc, iIndex) {
           if( !iModelDesc.plotClass)
@@ -654,10 +531,12 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
         }.bind( this));
       }.bind( this);
 
+      sc_super();
+
       // Instantiate the attribute references
-      xAttrRef = instantiateAttributeRef('xColl', 'xAttr');
-      yAttrRef = instantiateAttributeRef('yColl', 'yAttr');
-      legendAttrRef = instantiateAttributeRef('legendColl', 'legendAttr');
+      xAttrRef = this.instantiateAttributeRefFromStorage(iStorage, 'xColl', 'xAttr');
+      yAttrRef = this.instantiateAttributeRefFromStorage(iStorage, 'yColl', 'yAttr');
+      legendAttrRef = this.instantiateAttributeRefFromStorage(iStorage, 'legendColl', 'legendAttr');
 
       this.set('aboutToChangeConfiguration', true ); // signals dependents to prepare
 
@@ -686,7 +565,6 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
                                   iStorage.plotModels ||
                                   []);
 
-      tDataConfig.restoreHiddenCases( iStorage.hiddenCases);
       this.get('plots' ).forEach( function( iPlot) {
         iPlot._isBeingRestored = null;  // Reenable rescaling
       });
@@ -822,82 +700,52 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
       return SC.none( parent);
     },
 
+    handleOneDataContextChange: function( iNotifier, iChange) {
+      var plotModel = this.get('plot'),
+          operation = iChange && iChange.operation;
+
+      // No response necessary if the change doesn't affect us.
+      if( plotModel && !plotModel.isAffectedByChange( iChange))
+        return;
+
+      // The GraphModel response is operation-specific.
+      switch( operation) {
+        case 'createCase':
+        case 'createCases':
+        case 'deleteCases':
+          this.dataDidChange( null, null, iChange);
+          break;
+        case 'updateCases':
+        case 'createAttributes':
+        case 'updateAttributes':
+          // We must invalidate before we build indices because the change may
+          // have affected the set of included cases, which affects indices.
+          // It would be better not to be dealing with indices at all, but
+          // that refactoring is left for another day.
+          this.get('dataConfiguration').invalidateCaches( null, iChange);
+          iChange.indices = this.buildIndices( iChange);
+          this.dataRangeDidChange( this, 'revision', this, iChange.indices);
+          break;
+        case 'selectCases':
+          //this.selectionDidChange();
+          break;
+      }
+
+      // Forward the notification to the plots, so they can respond as well.
+      this.get('plots' ).forEach( function( iPlot) {
+        iPlot.handleDataContextNotification( iNotifier, iChange);
+      });
+    },
+
     /**
       Responder for notifications from the DataContext.
      */
     handleDataContextNotification: function( iNotifier) {
-      var newChanges = iNotifier.get('newChanges');
-      
-      /**
-        Returns an array of indices corresponding to the indices
-        of the cases affected by the specified change.
-       */
-      var buildIndices = function( iChange) {
-        var srcCases = this.getPath('dataConfiguration.cases');
-        if( (iChange.operation === 'updateCases') &&
-            srcCases && !SC.none( iChange.cases) &&
-            !this.getPath('dataConfiguration.hasAggregates')) {
-          var updatedCasesByID = {},
-              indices = SC.IndexSet.create();
-          // Build a map of IDs for affected cases
-          iChange.cases.forEach( function( iCase) {
-                                  updatedCasesByID[ iCase.get('id')] = iCase;
-                                 });
-          // Loop through source cases to determine indices for the affected cases
-          srcCases.forEach( function( iCase, iIndex) {
-                              var caseID = iCase.get('id');
-                              if( updatedCasesByID[ caseID])
-                                indices.add( iIndex);
-                            });
-          return indices;
-        }
-        return SC.IndexSet.create( 0, this.getPath('dataConfiguration.cases.length'));
-      }.bind( this);
+      sc_super(); // Bulk of work will be done in calls to handleOneDataContextChange
 
-      var handleOneDataContextChange = function( iChange) {
-        var plotModel = this.get('plot'),
-            operation = iChange && iChange.operation;
-        
-        // No response necessary if the change doesn't affect us.
-        if( plotModel && !plotModel.isAffectedByChange( iChange))
-          return;
-
-        // The GraphModel response is operation-specific.
-        switch( operation) {
-          case 'createCase':
-          case 'createCases':
-          case 'deleteCases':
-            this.dataDidChange( null, null, iChange);
-            break;
-          case 'updateCases':
-          case 'createAttributes':
-          case 'updateAttributes':
-            // We must invalidate before we build indices because the change may
-            // have affected the set of included cases, which affects indices.
-            // It would be better not to be dealing with indices at all, but
-            // that refactoring is left for another day.
-            this.get('dataConfiguration').invalidateCaches( null, iChange);
-            iChange.indices = buildIndices( iChange);
-            this.dataRangeDidChange( this, 'revision', this, iChange.indices);
-            break;
-          case 'selectCases':
-            //this.selectionDidChange();
-            break;
-        }
-  
-        // Forward the notification to the plots, so they can respond as well.
-        this.get('plots' ).forEach( function( iPlot) {
-          iPlot.handleDataContextNotification( iNotifier, iChange);
-        });
-      }.bind( this);
-      
-      // Process all data context changes
-      if( newChanges.length) {
-        newChanges.forEach( handleOneDataContextChange);
-        if( this.get('numberToggle')) {
-          // The numberToggle does not need to respond to individual notifications
-          this.get('numberToggle' ).handleDataContextNotification( iNotifier);
-        }
+      if( this.get('numberToggle')) {
+        // The numberToggle does not need to respond to individual notifications
+        this.get('numberToggle' ).handleDataContextNotification( iNotifier);
       }
     },
     
@@ -940,17 +788,10 @@ DG.GraphModel = SC.Object.extend( DG.Destroyable,
     /**
      @private
      */
-    dataRangeDidChange: function( iSource, iKey, iObject, iIndices) {
-      this.invalidate();
-    },
-
-    /**
-     @private
-     */
     invalidate: function( iChange, iInvalidatePlotCaches) {
-      var tDataConfiguration = this.get( 'dataConfiguration' ),
-          tPlot = this.get( 'plot' );
-      tDataConfiguration.invalidateCaches( null, iChange);
+      var tPlot = this.get( 'plot' );
+      sc_super();
+
       if( iInvalidatePlotCaches && !SC.none( tPlot ) )
         tPlot.invalidateCaches();
     }
