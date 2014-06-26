2532eca581437df0422b6eec878155f6093a9850
Bill Finzer <bfinzer@kcptech.com>
2014-06-26 15:38:56 -0700

    Extracts of the portions of protovis that we actually use in CODAP, enabling us to get rid of the rest of protovis.

---
+++ apps/dg/utilities/pv_color.js
@@ -0,0 +1,311 @@
+/**
+ * Taken from Mike Bostock's Protovis project <http://mbostock.github.io/protovis/>. Thank you!
+ *
+ * Returns the {@link DG.Color} for the specified color format string. Colors
+ * may have an associated opacity, or alpha channel. Color formats are specified
+ * by CSS Color Modular Level 3, using either in RGB or HSL color space. For
+ * example:<ul>
+ *
+ * <li>#f00 // #rgb
+ * <li>#ff0000 // #rrggbb
+ * <li>rgb(255, 0, 0)
+ * <li>rgb(100%, 0%, 0%)
+ * <li>hsl(0, 100%, 50%)
+ * <li>rgba(0, 0, 255, 0.5)
+ * <li>hsla(120, 100%, 50%, 1)
+ *
+ * </ul>The SVG 1.0 color keywords names are also supported, such as "aliceblue"
+ * and "yellowgreen". The "transparent" keyword is supported for fully-
+ * transparent black.
+ *
+ * <p>If the <tt>format</tt> argument is already an instance of <tt>Color</tt>,
+ * the argument is returned with no further processing.
+ *
+ * @param {string} format the color specification string, such as "#f00".
+ * @returns {DG.Color} the corresponding <tt>Color</tt>.
+ * @see <a href="http://www.w3.org/TR/SVG/types.html#ColorKeywords">SVG color
+ * keywords</a>
+ * @see <a href="http://www.w3.org/TR/css3-color/">CSS3 color module</a>
+ */
+DG.color = function(format) {
+  if (format.rgb) return format.rgb();
+
+  /* Handle hsl, rgb. */
+  var m1 = /([a-z]+)\((.*)\)/i.exec(format);
+  if (m1) {
+    var m2 = m1[2].split(","), a = 1;
+    switch (m1[1]) {
+      case "hsla":
+      case "rgba": {
+        a = parseFloat(m2[3]);
+        if (!a) return DG.Color.transparent;
+        break;
+      }
+    }
+    switch (m1[1]) {
+      case "hsla":
+      case "hsl": {
+        var h = parseFloat(m2[0]), // degrees
+            s = parseFloat(m2[1]) / 100, // percentage
+            l = parseFloat(m2[2]) / 100; // percentage
+        return (new DG.Color.Hsl(h, s, l, a)).rgb();
+      }
+      case "rgba":
+      case "rgb": {
+        function parse(c) { // either integer or percentage
+          var f = parseFloat(c);
+          return (c[c.length - 1] == '%') ? Math.round(f * 2.55) : f;
+        }
+        var r = parse(m2[0]), g = parse(m2[1]), b = parse(m2[2]);
+        return DG.rgb(r, g, b, a);
+      }
+    }
+  }
+
+  /* Named colors. */
+  // Not needed for DG
+//  var named = DG.Color.names[format];
+//  if (named) return named;
+
+  /* Hexadecimal colors: #rgb and #rrggbb. */
+  if (format.charAt(0) == "#") {
+    var r, g, b;
+    if (format.length == 4) {
+      r = format.charAt(1); r += r;
+      g = format.charAt(2); g += g;
+      b = format.charAt(3); b += b;
+    } else if (format.length == 7) {
+      r = format.substring(1, 3);
+      g = format.substring(3, 5);
+      b = format.substring(5, 7);
+    }
+    return DG.rgb(parseInt(r, 16), parseInt(g, 16), parseInt(b, 16), 1);
+  }
+
+  /* Otherwise, pass-through unsupported colors. */
+  return new DG.Color(format, 1);
+};
+
+/**
+ * Constructs a color with the specified color format string and opacity. This
+ * constructor should not be invoked directly; use {@link DG.color} instead.
+ *
+ * @class Represents an abstract (possibly translucent) color. The color is
+ * divided into two parts: the <tt>color</tt> attribute, an opaque color format
+ * string, and the <tt>opacity</tt> attribute, a float in [0, 1]. The color
+ * space is dependent on the implementing class; all colors support the
+ * {@link #rgb} method to convert to RGB color space for interpolation.
+ *
+ * <p>See also the <a href="../../api/Color.html">Color guide</a>.
+ *
+ * @param {string} color an opaque color format string, such as "#f00".
+ * @param {number} opacity the opacity, in [0,1].
+ * @see DG.color
+ */
+DG.Color = function(color, opacity) {
+  /**
+   * An opaque color format string, such as "#f00".
+   *
+   * @type string
+   * @see <a href="http://www.w3.org/TR/SVG/types.html#ColorKeywords">SVG color
+   * keywords</a>
+   * @see <a href="http://www.w3.org/TR/css3-color/">CSS3 color module</a>
+   */
+  this.color = color;
+
+  /**
+   * The opacity, a float in [0, 1].
+   *
+   * @type number
+   */
+  this.opacity = opacity;
+};
+
+/**
+ * Returns a new color that is a brighter version of this color. The behavior of
+ * this method may vary slightly depending on the underlying color space.
+ * Although brighter and darker are inverse operations, the results of a series
+ * of invocations of these two methods might be inconsistent because of rounding
+ * errors.
+ *
+ * @param [k] {number} an optional scale factor; defaults to 1.
+ * @see #darker
+ * @returns {DG.Color} a brighter color.
+ */
+DG.Color.prototype.brighter = function(k) {
+  return this.rgb().brighter(k);
+};
+
+/**
+ * Returns a new color that is a brighter version of this color. The behavior of
+ * this method may vary slightly depending on the underlying color space.
+ * Although brighter and darker are inverse operations, the results of a series
+ * of invocations of these two methods might be inconsistent because of rounding
+ * errors.
+ *
+ * @param [k] {number} an optional scale factor; defaults to 1.
+ * @see #brighter
+ * @returns {DG.Color} a darker color.
+ */
+DG.Color.prototype.darker = function(k) {
+  return this.rgb().darker(k);
+};
+
+/**
+ * Constructs a new RGB color with the specified channel values.
+ *
+ * @param {number} r the red channel, an integer in [0,255].
+ * @param {number} g the green channel, an integer in [0,255].
+ * @param {number} b the blue channel, an integer in [0,255].
+ * @param {number} [a] the alpha channel, a float in [0,1].
+ * @returns DG.Color.Rgb
+ */
+DG.rgb = function(r, g, b, a) {
+  return new DG.Color.Rgb(r, g, b, (arguments.length == 4) ? a : 1);
+};
+
+/**
+ * Constructs a new RGB color with the specified channel values.
+ *
+ * @class Represents a color in RGB space.
+ *
+ * @param {number} r the red channel, an integer in [0,255].
+ * @param {number} g the green channel, an integer in [0,255].
+ * @param {number} b the blue channel, an integer in [0,255].
+ * @param {number} a the alpha channel, a float in [0,1].
+ * @extends DG.Color
+ */
+DG.Color.Rgb = function(r, g, b, a) {
+  DG.Color.call(this, a ? ("rgb(" + r + "," + g + "," + b + ")") : "none", a);
+
+  /**
+   * The red channel, an integer in [0, 255].
+   *
+   * @type number
+   */
+  this.r = r;
+
+  /**
+   * The green channel, an integer in [0, 255].
+   *
+   * @type number
+   */
+  this.g = g;
+
+  /**
+   * The blue channel, an integer in [0, 255].
+   *
+   * @type number
+   */
+  this.b = b;
+
+  /**
+   * The alpha channel, a float in [0, 1].
+   *
+   * @type number
+   */
+  this.a = a;
+};
+DG.Color.Rgb.prototype = SC.extend(DG.Color);
+
+/**
+ * Constructs a new RGB color with the same green, blue and alpha channels as
+ * this color, with the specified red channel.
+ *
+ * @param {number} r the red channel, an integer in [0,255].
+ */
+DG.Color.Rgb.prototype.red = function(r) {
+  return DG.rgb(r, this.g, this.b, this.a);
+};
+
+/**
+ * Constructs a new RGB color with the same red, blue and alpha channels as this
+ * color, with the specified green channel.
+ *
+ * @param {number} g the green channel, an integer in [0,255].
+ */
+DG.Color.Rgb.prototype.green = function(g) {
+  return DG.rgb(this.r, g, this.b, this.a);
+};
+
+/**
+ * Constructs a new RGB color with the same red, green and alpha channels as
+ * this color, with the specified blue channel.
+ *
+ * @param {number} b the blue channel, an integer in [0,255].
+ */
+DG.Color.Rgb.prototype.blue = function(b) {
+  return DG.rgb(this.r, this.g, b, this.a);
+};
+
+/**
+ * Constructs a new RGB color with the same red, green and blue channels as this
+ * color, with the specified alpha channel.
+ *
+ * @param {number} a the alpha channel, a float in [0,1].
+ */
+DG.Color.Rgb.prototype.alpha = function(a) {
+  return DG.rgb(this.r, this.g, this.b, a);
+};
+
+/**
+ * Returns the RGB color equivalent to this color. This method is abstract and
+ * must be implemented by subclasses.
+ *
+ * @returns {DG.Color.Rgb} an RGB color.
+ * @function
+ * @name DG.Color.prototype.rgb
+ */
+
+/**
+ * Returns this.
+ *
+ * @returns {DG.Color.Rgb} this.
+ */
+DG.Color.Rgb.prototype.rgb = function() { return this; };
+
+/**
+ * Returns a new color that is a brighter version of this color. This method
+ * applies an arbitrary scale factor to each of the three RGB components of this
+ * color to create a brighter version of this color. Although brighter and
+ * darker are inverse operations, the results of a series of invocations of
+ * these two methods might be inconsistent because of rounding errors.
+ *
+ * @param [k] {number} an optional scale factor; defaults to 1.
+ * @see #darker
+ * @returns {DG.Color.Rgb} a brighter color.
+ */
+DG.Color.Rgb.prototype.brighter = function(k) {
+  k = Math.pow(0.7, arguments.length ? k : 1);
+  var r = this.r, g = this.g, b = this.b, i = 30;
+  if (!r && !g && !b) return DG.rgb(i, i, i, this.a);
+  if (r && (r < i)) r = i;
+  if (g && (g < i)) g = i;
+  if (b && (b < i)) b = i;
+  return DG.rgb(
+      Math.min(255, Math.floor(r / k)),
+      Math.min(255, Math.floor(g / k)),
+      Math.min(255, Math.floor(b / k)),
+      this.a);
+};
+
+/**
+ * Returns a new color that is a darker version of this color. This method
+ * applies an arbitrary scale factor to each of the three RGB components of this
+ * color to create a darker version of this color. Although brighter and darker
+ * are inverse operations, the results of a series of invocations of these two
+ * methods might be inconsistent because of rounding errors.
+ *
+ * @param [k] {number} an optional scale factor; defaults to 1.
+ * @see #brighter
+ * @returns {DG.Color.Rgb} a darker color.
+ */
+DG.Color.Rgb.prototype.darker = function(k) {
+  k = Math.pow(0.7, arguments.length ? k : 1);
+  return DG.rgb(
+      Math.max(0, Math.floor(k * this.r)),
+      Math.max(0, Math.floor(k * this.g)),
+      Math.max(0, Math.floor(k * this.b)),
+      this.a);
+};
+
---
+++ apps/dg/utilities/svg_scene.js
@@ -0,0 +1,102 @@
+/**
+ * Taken from Mike Bostock's Protovis project <http://mbostock.github.io/protovis/>. Thank you!
+ *
+ * @private
+ * @namespace
+ */
+DG.Scene = DG.SvgScene = { };
+
+/**
+ * Creates a new SVG element of the specified type.
+ *
+ * @param type {string} an SVG element type, such as "rect".
+ * @returns a new SVG element.
+ */
+DG.SvgScene.create = function(type) {
+  return document.createElementNS(this.svg, type);
+};
+
+/**
+ * @private Converts the specified b-spline curve segment to a bezier curve
+ * compatible with SVG "C".
+ *
+ * @param p0 the first control point.
+ * @param p1 the second control point.
+ * @param p2 the third control point.
+ * @param p3 the fourth control point.
+ */
+DG.SvgScene.pathBasis = (function() {
+
+  /**
+   * Matrix to transform basis (b-spline) control points to bezier control
+   * points. Derived from FvD 11.2.8.
+   */
+  var basis = [
+    [ 1/6, 2/3, 1/6,   0 ],
+    [   0, 2/3, 1/3,   0 ],
+    [   0, 1/3, 2/3,   0 ],
+    [   0, 1/6, 2/3, 1/6 ]
+  ];
+
+  /**
+   * Returns the point that is the weighted sum of the specified control points,
+   * using the specified weights. This method requires that there are four
+   * weights and four control points.
+   */
+  function weight(w, p0, p1, p2, p3) {
+    return {
+      x: w[0] * p0.left + w[1] * p1.left + w[2] * p2.left + w[3] * p3.left,
+      y: w[0] * p0.top  + w[1] * p1.top  + w[2] * p2.top  + w[3] * p3.top
+    };
+  }
+
+  var convert = function(p0, p1, p2, p3) {
+    var b1 = weight(basis[1], p0, p1, p2, p3),
+        b2 = weight(basis[2], p0, p1, p2, p3),
+        b3 = weight(basis[3], p0, p1, p2, p3);
+    return "C" + b1.x + "," + b1.y
+        + "," + b2.x + "," + b2.y
+        + "," + b3.x + "," + b3.y;
+  };
+
+  convert.segment = function(p0, p1, p2, p3) {
+    var b0 = weight(basis[0], p0, p1, p2, p3),
+        b1 = weight(basis[1], p0, p1, p2, p3),
+        b2 = weight(basis[2], p0, p1, p2, p3),
+        b3 = weight(basis[3], p0, p1, p2, p3);
+    return "M" + b0.x + "," + b0.y
+        + "C" + b1.x + "," + b1.y
+        + "," + b2.x + "," + b2.y
+        + "," + b3.x + "," + b3.y;
+  };
+
+  return convert;
+})();
+
+/**
+ * @private Interpolates the given points using the basis spline interpolation.
+ * Returns an SVG path without the leading M instruction to allow path
+ * appending.
+ *
+ * @param points the array of points.
+ */
+DG.SvgScene.curveBasis = function(points) {
+  if (points.length <= 2) return "";
+  var path = "",
+      p0 = points[0],
+      p1 = p0,
+      p2 = p0,
+      p3 = points[1];
+  path += this.pathBasis(p0, p1, p2, p3);
+  for (var i = 2; i < points.length; i++) {
+    p0 = p1;
+    p1 = p2;
+    p2 = p3;
+    p3 = points[i];
+    path += this.pathBasis(p0, p1, p2, p3);
+  }
+  /* Cycle through to get the last point. */
+  path += this.pathBasis(p1, p2, p3, p3);
+  path += this.pathBasis(p2, p3, p3, p3);
+  return path;
+};
