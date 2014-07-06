133e1b022f0c69529cffd80c101718e110146893
Bill Finzer <bfinzer@kcptech.com>
2014-07-06 14:27:43 -0700

    Moved changeAttributeForLegend into this base class from subclass.

--- apps/dg/components/graph_map_common/data_display_model.js
+++ apps/dg/components/graph_map_common/data_display_model.js
@@ -221,7 +221,32 @@ DG.DataDisplayModel = SC.Object.extend( DG.Destroyable,
         this.handleOneDataContextChange( iNotifier, newChanges[ i]);
       }
     },
-    
+
+    /**
+     Sets the attribute for the legend.
+     @param  {DG.DataContext}      iDataContext -- The data context for this graph
+     @param  {Object}              iAttrRefs -- The attribute to set for the axis
+     {DG.CollectionClient} iAttrRefs.collection -- The collection that contains the attribute
+     {DG.Attribute}        iAttrRefs.attribute -- Array of attributes to set for the legend
+     */
+    changeAttributeForLegend: function( iDataContext, iAttrRefs) {
+      var tAttribute = iAttrRefs && iAttrRefs.attributes[0];
+      if( tAttribute)
+        DG.logUser("legendAttributeChange: { to attribute %@ }", tAttribute.get('name'));
+      else
+        DG.logUser("legendAttributeRemoved:");
+
+      this.set('aboutToChangeConfiguration', true ); // signals dependents to prepare
+
+      var dataConfiguration = this.get('dataConfiguration');
+      if( iDataContext)
+        dataConfiguration.set('dataContext', iDataContext);
+      dataConfiguration.setAttributeAndCollectionClient('legendAttributeDescription', iAttrRefs);
+
+      this.invalidate();
+      this.set('aboutToChangeConfiguration', false ); // reset for next time
+    },
+
     /**
      @private
      */
