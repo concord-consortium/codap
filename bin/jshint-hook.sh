183a796ca4dce00239e32ac47e9d8ac6eb4e6f63
Evangeline Ireland <eireland@concord.org>
2014-08-25 16:24:21 -0700

    Adds precommit hooks with JSHint and a precommit hook installer and changes to the JSHint configuration files. [Fixes #58203342]

---
+++ bin/jshint-hook.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+# Usage:
+#   1.  cd to the top-level dg directory (where the Makefile lives)
+#   2a. Type bin/jshint-hook.sh
+#       or
+#   2b. Type make jshint-hook
+
+files=$(git diff --cached --name-only --diff-filter=ACMR -- *.js **/*.js)
+pass=true
+
+
+if [ "$files" != "" ]; then
+    for file in ${files}; do
+        result=$(jshint ${file})
+
+        if [ "$result" != "" ]; then
+            echo $result
+            pass=false
+        fi
+    done
+fi
+
+
+if $pass; then
+    exit 0
+else
+    echo ""
+    echo "COMMIT FAILED:"
+    echo "Some JavaScript files are invalid. Please fix errors and try committing again."
+    exit 1
+fi
