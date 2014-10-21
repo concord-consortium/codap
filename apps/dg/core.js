4064243336024c6055ab3edf78f339be7a5c86bb
Jonathan Sandoe <jsandoe@concord.org>
2014-10-21 11:00:27 -0700

    Build 0270

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -209,7 +209,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0269',
+    BUILD_NUM: '0270',
 
     /**
      * The subdomain for the Drupal site which must be hosted on the same domain.  This is used for various interactions
