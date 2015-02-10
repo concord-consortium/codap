4de29104165a3636c97cf54cb3350215c7eddcd2
William Finzer <wfinzer@concord.org>
2015-02-09 18:01:11 -0800

    A data interactive can now set an attribute's blockDisplayOfEmptyCategories property to true. We carry that along and detect it when computing categorical attribute stats.

--- apps/dg/components/graph/data_model/attribute_stats.js
+++ apps/dg/components/graph/data_model/attribute_stats.js
@@ -405,10 +405,12 @@ DG.AttributeStats = SC.Object.extend(
           tCases.forEach( function ( iCase ) {
             addCaseValueToStats( iCase, iCase.getValue( tVarID ));
           } );
-          DG.ObjectMap.forEach( tCellMap, function( iKey, iValue) {
-            if( iValue.length === 0)
-              delete tCellMap[iKey];
-          });
+          if( iAttribute.get('blockDisplayOfEmptyCategories')) {
+            DG.ObjectMap.forEach(tCellMap, function (iKey, iValue) {
+              if (iValue.length === 0)
+                delete tCellMap[iKey];
+            });
+          }
         });
 
         this.categoricalStats.beginPropertyChanges();
