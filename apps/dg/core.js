bb4dfd02c4576f427f33e3a54d8505df931c09fe
Jonathan Sandoe <jsandoe@concord.org>
2015-02-10 20:14:15 -0800

    Build 0282

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -189,7 +189,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0281',
+    BUILD_NUM: '0282',
 
     /**
      * The subdomain for the Drupal site which must be hosted on the same domain.  This is used for various interactions
