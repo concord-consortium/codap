2833de6a701b2c9e5af464d2e270efb7eee4f072
William Finzer <wfinzer@concord.org>
2015-10-18 17:44:28 -0700

    Fixed reference to undefined attribute in log statement.

--- apps/dg/components/graph/graph_controller.js
+++ apps/dg/components/graph/graph_controller.js
@@ -276,7 +276,7 @@ DG.GraphController = DG.DataDisplayController.extend(
                   attributes: [iDragData.attribute]
                 });
 
-              this.log = 'Attribute dragged and dropped: %@, %@'.fmt(iAxis.get('orientation'), iDragData.attribute.get('name'));
+              this.log = 'Attribute dragged and dropped: %@, %@'.fmt('vertical', iDragData.attribute.get('name'));
             },
             undo: function() {
               var controller = this._controller();
