147105b5fd22c8428d66f822208cc4b6de10a461
William Finzer <wfinzer@concord.org>
2015-11-19 08:50:01 -0800

    Comment added to explain rationale for hooking up observer to dataConfiguration during init.

--- apps/dg/components/graph_map_common/data_display_model.js
+++ apps/dg/components/graph_map_common/data_display_model.js
@@ -127,6 +127,8 @@ DG.DataDisplayModel = SC.Object.extend( DG.Destroyable,
 
       var tConfiguration = this.get('dataConfigurationClass').create(),
           tContext = tConfiguration.get('dataContext');
+      // If the context has been discovered in the init of the configuration, we take this opportunity
+      // to hook up our observer to it.
       if( tContext) {
         tContext.addObserver('changeCount', this, 'handleDataContextNotification');
       }
