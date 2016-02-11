e67038e3bac4f8b4c433ae479856c8a2e4cf2cf8
Jonathan Sandoe <jsandoe@concord.org>
2016-02-11 11:16:05 -0800

    WIP on expand/collapse. Fix hidden table computation.
    This fixes issues observed by EIreland where collapse-all could become
    confused.

--- apps/dg/components/case_table/case_table_model.js
+++ apps/dg/components/case_table/case_table_model.js
@@ -124,7 +124,7 @@ DG.CaseTableModel = SC.Object.extend(/** @scope DG.CaseTableModel.prototype */ {
     }
     collapseState.isCollapsed = true;
     this.collapsedNodes.forEach(function (cs) {
-      collapseState.isHidden = this.isHiddenNode(cs.collapsedCase);
+      cs.isHidden = this.isHiddenNode(cs.collapsedCase);
     }.bind(this));
   },
 
