1b09e416072d2989d7ba57c029c2a46bf9c8e15d
Jonathan Sandoe <jsandoe@concord.org>
2016-03-14 09:42:03 -0700

    Modify build label by hand to label this as a branch build.

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -201,7 +201,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0330',
+    BUILD_NUM: '0330-a',
 
     /**
      * The subdomain for the Drupal site which must be hosted on the same domain.  This is used for various interactions
