fd8944fdde826fffb967601acb529e4b95965e40
William Finzer <wfinzer@concord.org>
2016-03-19 15:31:30 -0700

    [Fixes #115786297] In SproutCore 1.9 Drag::StartDrag would pass 'drag Started' to _all_ drop targets. This was changed in 1.11 so that drop targets not visible are skipped. We reinstate by overriding StartDrag to provide previous behavior.

--- apps/dg/components/graph/utilities/dg_drag.js
+++ apps/dg/components/graph/utilities/dg_drag.js
@@ -30,6 +30,77 @@
 */
 DG.Drag = SC.Drag.extend({
 
+  /** @private
+   This will actually start the drag process. Called by SC.Drag.start().
+   [Finzer - Concord Consortium] We override to get a previous behavior with regard dragStarted for views
+        that are not visible.
+   */
+  startDrag: function () {
+    if (this.get('sourceIsDraggable')) {
+      // create the ghost view
+      this._createGhostView();
+    }
+
+    var evt = this.event;
+
+    // compute the ghost offset from the original start location
+
+    var loc = { x: evt.pageX, y: evt.pageY };
+    this.set('location', loc);
+
+    if (this.get('sourceIsDraggable')) {
+      var dv = this._getDragView();
+      var pv = dv.get('parentView');
+
+      // convert to global coordinates
+      var origin = pv ? pv.convertFrameToView(dv.get('frame'), null) : dv.get('frame');
+
+      if (this.get('ghost')) {
+        // Hide the dragView
+        this._dragViewWasVisible = dv.get('isVisible');
+        dv.set('isVisible', NO);
+      }
+
+      if (this.ghostActsLikeCursor) this.ghostOffset = { x: 14, y: 14 };
+      else this.ghostOffset = { x: (loc.x - origin.x), y: (loc.y - origin.y) };
+
+      // position the ghost view
+      if (!this._ghostViewHidden) this._positionGhostView(evt);
+
+      if (evt.makeTouchResponder) {
+        // Should use invokeLater if I can figure it out
+        var self = this;
+        SC.Timer.schedule({
+          target: evt,
+          action: function () {
+            if (!evt.hasEnded) evt.makeTouchResponder(self, YES);
+          },
+          interval: 1
+        });
+      }
+      else {
+        // notify root responder that a drag is in process
+        this.ghostView.rootResponder.dragDidStart(this, evt);
+      }
+    }
+
+    var source = this.source;
+    if (source && source.dragDidBegin) source.dragDidBegin(this, loc);
+
+    // let all drop targets know that a drag has started
+    var ary = this._dropTargets();
+
+    for (var idx = 0, len = ary.length; idx < len; idx++) {
+      var target = ary[idx];
+      // This change to ignore views that are not visible was introduced post SC 1.9.
+      // We rely on get dragStarted in the various axis views and there is no easy alternative.
+      // If the target is not visible, it is not valid.
+      //if (!target.get('isVisibleInWindow')) continue;
+
+      target.tryToPerform('dragStarted', this, evt);
+    }
+  },
+
   _findDropTarget: function( iEvent) {
     var tTargets = this._dropTargets(),
         tLoc = { x: iEvent.pageX, y: iEvent.pageY },
