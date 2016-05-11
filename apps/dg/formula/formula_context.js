e6baaf4a3b060515b36ec8490ce2b33d1f4e428a
William Finzer <wfinzer@concord.org>
2016-05-11 14:57:31 -0700

    #119410759 - greatCircleDistance was reporting half of the true value.

--- apps/dg/formula/formula_context.js
+++ apps/dg/formula/formula_context.js
@@ -240,7 +240,7 @@ DG.FormulaContext = SC.Object.extend( (function() {
     		a = Math.pow(Math.sin((Math.PI / 180) * deltaLat/2), 2) + 
     			Math.cos(lat1 * Math.PI / 180) * Math.cos (lat2 * Math.PI / 180) * 
     				Math.pow(Math.sin((Math.PI / 180) * deltaLong/2), 2);
-    	return 6371*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
+    	return 2*6371*Math.atan2(Math.sqrt(a),Math.sqrt(1-a));
     },
 
     /**
