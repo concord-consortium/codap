6bb4d71969e9af5dba0866645014327c3d568052
Jonathan Sandoe <jsandoe@concord.org>
2016-08-29 14:15:32 -0700

    [#101304924] Fix Bug: Could not click into a case table cell edit field to position caret.
    When you clicked, the cell would remain selected. You had to use arrow keys or type characters.
    Could not find the entity that was preventing this caret positioning, so implemented an event handler to do it.

--- apps/dg/components/case_table/case_table_adapter.js
+++ apps/dg/components/case_table/case_table_adapter.js
@@ -661,6 +661,7 @@ DG.CaseTableCellEditor = function CaseTableCellEditor(args) {
   var defaultValue;
 
   this.init = function () {
+
     var kLeftArrowKeyCode = 37,
         kRightArrowKeyCode = 39;
     $input = SC.$("<INPUT type=text class='editor-text' />")
@@ -673,6 +674,30 @@ DG.CaseTableCellEditor = function CaseTableCellEditor(args) {
         .bind("blur", function(e) {
           // Attempt to complete the edit whenever we lose focus
           DG.globalEditorLock.commitCurrentEdit();
+        })
+        .bind('click', function (e) {
+          // for unknown reasons, click in the input box does not
+          // position the input caret, so we do it ourselves...
+          function positionCaret($el, text, xPosition) {
+            var $div = $('<div>').text(text);
+            var length = text.length;
+            var pos;
+            $div.css({
+              position: 'absolute',
+              left: '-100px',
+              top: '-100px',
+              fontWeight: $el.css('font-weight'),
+              fontFamily: $el.css('font-family'),
+              fontSize: $el.css('font-size')
+            });
+            $('body').append($div);
+            pos = Math.round(xPosition*length/$div.width());
+            $div.remove();
+            $el[0].setSelectionRange(pos, pos);
+          }
+          positionCaret($(this), $(this).val(), e.offsetX);
+          e.preventDefault();
+          e.stopImmediatePropagation();
         })
         .focus()
         .select();
