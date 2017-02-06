badcd11d8e2222020020608771fb1c4fc64cafd7
William Finzer <wfinzer@concord.org>
2017-02-06 14:06:53 -0800

    [#138077425] Least squares line does not show a multiplication sign when displaying the equation of the line. Add a space an asterisk to slopeIntercept string

--- apps/dg/english.lproj/strings.js
+++ apps/dg/english.lproj/strings.js
@@ -501,7 +501,7 @@ SC.stringsFor('English', {
   // DG.ScatterPlotModel
   'DG.ScatterPlotModel.sumSquares': ",\nSum of squares = %@", // sumOfResidualsSquared
   'DG.ScatterPlotModel.rSquared': ",\nr^2 = %@", // r-squared
-  'DG.ScatterPlotModel.slopeIntercept': "%@ = %@ %@ %@ %@",// y,slope,x,signInt,Int
+  'DG.ScatterPlotModel.slopeIntercept': "%@ = %@* %@ %@ %@",// y,slope,x,signInt,Int
   'DG.ScatterPlotModel.infiniteSlope': "%@ = %@",// x,constant
   'DG.ScatterPlotModel.daysLabel': "per day",// per day - used in equation for line when x is a datetime axis
   'DG.ScatterPlotModel.hoursLabel': "per hour",// per hour - used in equation for line when x is a datetime axis
