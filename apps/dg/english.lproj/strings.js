bf3d6c447260d58a5473963c3e38acadb9f04089
William Finzer <wfinzer@concord.org>
2017-04-08 14:44:33 -0700

    [Fixes #136058231] In the "Edit Attribute Properties" menu, change attribute type from "nominal" to "categorical"

--- apps/dg/english.lproj/strings.js
+++ apps/dg/english.lproj/strings.js
@@ -470,7 +470,7 @@ SC.stringsFor('English', {
   // DG.CaseTableDropTarget
   'DG.CaseTableDropTarget.dropMessage': "drop attribute to create new collection",
   'DG.CaseTable.attribute.type.none': '',
-  'DG.CaseTable.attribute.type.nominal': 'nominal',
+  'DG.CaseTable.attribute.type.nominal': 'categorical',
   'DG.CaseTable.attribute.type.numeric': 'numeric',
   'DG.CaseTable.attribute.type.date': 'date',
   'DG.CaseTable.attribute.type.qualitative': 'qualitative',
