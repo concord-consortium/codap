concord-consortium/codap

commit b425053e1616ff43884484b7546969f8b9b1e11e

[#144224433] Attribute creation does not accept 'categorical' value

apps/dg/controllers/collection_client.js
@@ -345,15 +345,12 @@ DG.CollectionClient = SC.Object.extend(
         if (!SC.none( iProperties.colormap)) {
           tAttribute.set('colormap', SC.clone( iProperties.colormap));
         }
-        // copy 'blockDisplayOfEmptyCategories' property
-        if (!SC.none( iProperties.blockDisplayOfEmptyCategories)) {
-          tAttribute.set('blockDisplayOfEmptyCategories', iProperties.blockDisplayOfEmptyCategories);
-        }
-        // copy 'editable' property
-        if (!SC.none( iProperties.editable)) {
-          tAttribute.set('editable', iProperties.editable);
-        }
-        // Eventually we should copy all properties here.
+
+        ['title', 'type', 'description', 'editable', 'hidden', 'precision', 'unit', 'blockDisplayOfEmptyCategories'].foreach(function (prop) {
+          if (!SC.none( iProperties[prop])) {
+            tAttribute.set(prop, iProperties[prop]);
+          }
+        });
       }
 
       // If the attribute doesn't exist, we must create it

apps/dg/components/case_table/attribute_editor_view.js
@@ -198,6 +198,10 @@ DG.AttributeEditorView = SC.PalettePane.extend( (function() // closure
         if (attrRef) {
           DG.ObjectMap.forEach(attr, function(key, val) {
             var ctlName = key + 'Ctl';
+            // Convert attribute type nominal to categorical
+            if (key === 'type' && val === DG.Attribute.TYPE_NOMINAL) {
+              val = DG.Attribute.TYPE_CATEGORICAL;
+            }
             if (!SC.none(contentView.get(ctlName)) && !SC.none(val)) {
               contentView.setPath(ctlName + '.controlView.value', val);
             }

apps/dg/components/graph/data_model/attribute_stats.js
@@ -236,6 +236,7 @@ DG.AttributeStats = SC.Object.extend(
           // If our first attribute has a type assigned, we return it
           switch (tAttributes[0].get('type')) {
             case 'nominal':
+            case 'categorical':
               this._attributeType = DG.Analysis.EAttributeType.eCategorical;
               break;
             case 'numeric':

apps/dg/english.lproj/strings.js
@@ -485,6 +485,7 @@ SC.stringsFor('English', {
   'DG.CaseTableDropTarget.dropMessage': "drop attribute to create new collection",
   'DG.CaseTable.attribute.type.none': '',
   'DG.CaseTable.attribute.type.nominal': 'categorical',
+  'DG.CaseTable.attribute.type.categorical': 'categorical',
   'DG.CaseTable.attribute.type.numeric': 'numeric',
   'DG.CaseTable.attribute.type.date': 'date',
   'DG.CaseTable.attribute.type.qualitative': 'qualitative',

apps/dg/models/attribute_model.js
@@ -433,6 +433,9 @@ DG.Attribute.createAttribute = function( iProperties) {
   if( iProperties.colormap) {
     newAttribute.colormap = SC.clone( iProperties.colormap);
   }
+  if ( iProperties.type && iProperties.type===DG.Attribute.TYPE_NOMINAL) {
+    iProperties.type = DG.Attribute.TYPE_CATEGORICAL;
+  }
 
   DG.store.commitRecords();
   DG.Attribute.idMap[ newAttribute.get('id')] = newAttribute;
@@ -453,7 +456,7 @@ DG.Attribute.legalizeAttributeName = function (iName) {
     tNewName = tName.replace(tReg, '');  // Get rid of parenthesized units
   }
   // TODO: We are eliminating all but Latin characters here. We should be more general and allow
-  // non-Latin alphameric characters.
+  // non-Latin alphanumeric characters.
   tNewName = tNewName.trim(); // Get rid of trailing white space
   tNewName = tNewName.replace(/\W/g, '_');  // Replace white space with underscore
   // if after all this we have an empty string replace with a default name.
@@ -478,14 +481,15 @@ DG.Attribute.destroyAttribute = function( iAttribute) {
  * @type {string[]}
  */
 DG.Attribute.TYPE_UNSPECIFIED = 'none';
-DG.Attribute.TYPE_NOMINAL = 'nominal';
+DG.Attribute.TYPE_NOMINAL = 'nominal'; // no longer used
+DG.Attribute.TYPE_CATEGORICAL = 'categorical';
 DG.Attribute.TYPE_NUMERIC = 'numeric';
 DG.Attribute.TYPE_DATE = 'date';
 DG.Attribute.TYPE_QUALITATIVE = 'qualitative';
 
 DG.Attribute.attributeTypes = [
   DG.Attribute.TYPE_UNSPECIFIED,
-  DG.Attribute.TYPE_NOMINAL,
+  DG.Attribute.TYPE_CATEGORICAL,
   DG.Attribute.TYPE_NUMERIC,
   DG.Attribute.TYPE_DATE,
   DG.Attribute.TYPE_QUALITATIVE

apps/dg/spanish.lproj/strings.js
@@ -462,7 +462,8 @@ SC.stringsFor('es', {
   // DG.CaseTableDropTarget
   'DG.CaseTableDropTarget.dropMessage': "(es)drop attribute to create new collection",
   'DG.CaseTable.attribute.type.none': '(es)',
-  'DG.CaseTable.attribute.type.nominal': '(es)nominal',
+  'DG.CaseTable.attribute.type.nominal': '(es)categorical',
+  'DG.CaseTable.attribute.type.categorical': '(es)categorical',
   'DG.CaseTable.attribute.type.numeric': '(es)numeric',
   'DG.CaseTable.attribute.type.date': '(es)date',
   'DG.CaseTable.attribute.type.qualitative': '(es)qualitative',

commit eefb1b8aad12524f7e3c3fa4d144d14ecdc91003

[#144224433] Fix bug: attribute create or update from plugin fails
Introduced in commit b425053e.

'forEach' was spelled 'foreach' in CollectionClient.guaranteeAttribute.
Caused setting attribute properties from plugin to fail.

apps/dg/controllers/collection_client.js
@@ -346,7 +346,7 @@ DG.CollectionClient = SC.Object.extend(
           tAttribute.set('colormap', SC.clone( iProperties.colormap));
         }
 
-        ['title', 'type', 'description', 'editable', 'hidden', 'precision', 'unit', 'blockDisplayOfEmptyCategories'].foreach(function (prop) {
+        ['title', 'type', 'description', 'editable', 'hidden', 'precision', 'unit', 'blockDisplayOfEmptyCategories'].forEach(function (prop) {
           if (!SC.none( iProperties[prop])) {
             tAttribute.set(prop, iProperties[prop]);
           }