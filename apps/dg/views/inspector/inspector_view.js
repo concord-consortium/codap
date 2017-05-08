cc050d11c2214b3bea7aab43fafd7950d6c0a1c5
William Finzer <wfinzer@concord.org>
2017-05-08 07:48:47 -0700

    [Fixes #145039989] Revert componentMode to requiring a click to activate inspector for a component. It will no longer be the case that in componentMode, the first component is always selected.

--- apps/dg/views/inspector/inspector_view.js
+++ apps/dg/views/inspector/inspector_view.js
@@ -103,18 +103,11 @@ DG.InspectorView = DG.DraggableView.extend(
         }.observes('targetComponent'),
 
         selectedComponentDidChange: function () {
-          // In component mode, the inspector should always be visible and
-          // attached to the first component (it is expected there is only one
-          // component in component mode.)
-          if (DG.componentMode === 'yes') {
-            this.setIfChanged('targetComponent', this.getPath('componentContainer.componentViews')[0]);
-          } else {
-            var tTarget = this.get('targetComponent');
-            if (tTarget) {
-              tTarget.removeObserver('layout', this, 'targetLayoutDidChange');
-            }
-            this.set('targetComponent', this.getPath('componentContainer.selectedChildView'));
+          var tTarget = this.get('targetComponent');
+          if (tTarget) {
+            tTarget.removeObserver('layout', this, 'targetLayoutDidChange');
           }
+          this.set('targetComponent', this.getPath('componentContainer.selectedChildView'));
         }.observes('*componentContainer.selectedChildView'),
 
         targetLayoutDidChange: function () {
