59fa6712d5af6ac1ce0c06f975a385ebbc57388a
Jonathan Sandoe <jsandoe@concord.org>
2017-08-17 15:37:28 -0700

    Build 0397

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -233,7 +233,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0396',
+    BUILD_NUM: '0397',
 
     IS_DG_BUILD: isDGBuild.property(),
 
