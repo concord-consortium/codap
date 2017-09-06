0164b0fbc45ba4f57531259e5f4da7e29f5fb7f4
Kirk Swenson <kswenson@concord.org>
2017-09-06 12:55:45 -0700

    Shell scripts for pulling translations from/pushing changes to POEditor.com - allow comments in JSON source

--- bin/process-strings.sh
+++ bin/process-strings.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cp -v lang/strings/en-US.json apps/dg/english.lproj/strings.js
 sed -i "" 's/^{/SC.stringsFor("English", {/; s/^}$/});/' apps/dg/english.lproj/strings.js
+
+cp -v lang/strings/he.json apps/dg/he.lproj/strings.js
+sed -i "" 's/^{/SC.stringsFor("Hebrew", {/; s/^}$/});/' apps/dg/he.lproj/strings.js
+
+cp -v lang/strings/tr.json apps/dg/tr.lproj/strings.js
+sed -i "" 's/^{/SC.stringsFor("Turkish", {/; s/^}$/});/' apps/dg/tr.lproj/strings.js
+
 cp -v lang/strings/zh-TW.json apps/dg/zh.lproj/strings.js
 sed -i "" 's/^{/SC.stringsFor("Chinese", {/; s/^}$/});/' apps/dg/zh.lproj/strings.js
