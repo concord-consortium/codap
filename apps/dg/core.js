4fbb5a43139afe635522dcfbe0a6cbddb7242f7d
Evangeline Ireland <eireland@concord.org>
2017-10-02 14:05:13 -0700

    Build 0402

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -267,7 +267,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0401',
+    BUILD_NUM: '0402',
 
     IS_DG_BUILD: isDGBuild.property(),
 
