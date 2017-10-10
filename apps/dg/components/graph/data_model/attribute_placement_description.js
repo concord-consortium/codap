940cb3c44741e67b0fbfcd57d8cccdf3608a1ade
William Finzer <wfinzer@concord.org>
2017-10-10 16:26:01 -0700

    [#151858583] Bug fix: Selection by graph legend causes an exception
    Caused by a recent change to the cellMap data structure.

--- apps/dg/components/graph/data_model/attribute_placement_description.js
+++ apps/dg/components/graph/data_model/attribute_placement_description.js
@@ -266,7 +266,7 @@ DG.AttributePlacementDescription = SC.Object.extend(
 
   casesForCategory: function( iCellName) {
     var tCellMap = this.getPath('attributeStats.cellMap');
-    return tCellMap ? tCellMap[ iCellName] : [];
+    return tCellMap ? tCellMap[ iCellName].cases : [];
   }
 
 });
