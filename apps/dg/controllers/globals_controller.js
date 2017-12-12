0b6156e2fd6508badac6b0943aeed89c509f82cc
William Finzer <wfinzer@concord.org>
2017-12-12 15:22:19 -0800

    [#153429300] Bug fix: Hurricanes sample document crashes CODAP
    We revert an earlier change between 0414 and 0415 that made it so formulas referring to globals could evaluate early enough for their usage in a map to be valid during initialization.

--- apps/dg/controllers/globals_controller.js
+++ apps/dg/controllers/globals_controller.js
@@ -71,7 +71,7 @@ DG.globalsController = SC.Controller.create( (function() {
       @returns  {DG.GlobalValue}  The matching global value or null if not found
      */
     getGlobalValueByName: function( iName) {
-      return this._globalsMap[iName];
+      return DG.activeDocument.getGlobalValueByName(iName);
     },
   
     /**
