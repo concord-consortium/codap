838c785a1ac7d546c5f5396cb4cfb18b1ea67d8c
Jonathan Sandoe <jsandoe@concord.org>
2018-02-12 10:25:23 -0800

    [#154946500] fix bug: Graph Connecting lines are not drawn correctly.
    Use lodash.sortBy instead of browser sort to get a stable sort. Chrome sort is not stable.

--- apps/dg/components/graph_map_common/plot_data_configuration.js
+++ apps/dg/components/graph_map_common/plot_data_configuration.js
@@ -17,6 +17,8 @@
 //  See the License for the specific language governing permissions and
 //  limitations under the License.
 // ==========================================================================
+sc_require('_');
+/*global _:true*/
 
 /** @class  DG.PlotDataConfiguration - The object that describes the manner in which attributes are
     assigned to places in a graph.
@@ -570,7 +572,9 @@ DG.PlotDataConfiguration = SC.Object.extend(
             tLegendAttrDesc.get('attributeType') === DG.Analysis.EAttributeType.eCategorical) {
           var tLegendID = tLegendAttrDesc.getPath('attribute.id'),
               tCategoryMap = tLegendAttrDesc.getPath('attribute.categoryMap');
-          tMapOriginalToSorted.sort( function( iIndex1, iIndex2) {
+          // we use lodash sort here because Chrome's native sort is not stable
+          // https://stackoverflow.com/questions/42731205/stable-sorting-in-google-chrome
+          _.sortBy(tMapOriginalToSorted, function( iIndex1, iIndex2) {
             var tValue1 = tResult[iIndex1].getStrValue( tLegendID),
                 tValue2 = tResult[iIndex2].getStrValue( tLegendID);
             return tCategoryMap.__order.indexOf( tValue2) - tCategoryMap.__order.indexOf( tValue1);
