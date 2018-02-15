a2dc5877368f262d9bd641bcfc7bb128369df9d5
Jonathan Sandoe <jsandoe@concord.org>
2018-02-15 14:01:54 -0800

    Build 0432

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -267,7 +267,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0431',
+    BUILD_NUM: '0432',
 
     IS_DG_BUILD: isDGBuild.property(),
 
