37a54a9828e8ff68b92da8265b8d2155aad842e9
William Finzer <wfinzer@concord.org>
2018-03-23 15:15:02 -0700

    Started work on text input

--- apps/dg/components/case_card/text_input.js
+++ apps/dg/components/case_card/text_input.js
@@ -2,21 +2,23 @@
 // sc_require('react/dg-react');
 
 DG.React.ready(function () {
-  // var div = React.DOM.div//,
+  var
+      // div = React.DOM.div//,
       // p = React.DOM.p,
       // ul = React.DOM.ul,
       // li = React.DOM.li,
-      // span = React.DOM.span, //,
+      span = React.DOM.span,
       // italic = React.DOM.i,
       // h1 = React.DOM.h1,
       // h2 = React.DOM.h2,
       // table = React.DOM.table,
       // tbody = React.DOM.tbody,
       // tr = React.DOM.tr,
-  var td = React.DOM.td
-  // kLeftAngleBracketChar = '&#x2039;',
-  // kRightAngleBracketChar = '&#x203a;',
-  // kInfoIconChar = '&#9432;';
+      td = React.DOM.td,
+      input = React.DOM.input
+      // kLeftAngleBracketChar = '&#x2039;',
+      // kRightAngleBracketChar = '&#x203a;',
+      // kInfoIconChar = '&#9432;';
   ;
 
   DG.React.Components.TextInput = DG.React.createComponent(
@@ -24,21 +26,59 @@ DG.React.ready(function () {
 
         return {
 
+/*
+          constructor: function( props) {
+            super( props);
+            this.setState({ value: props.value });
+          },
+*/
+
           getInitialState: function () {
             return {
-              count: 0
+              editing: false,
+              value: this.props.value
             };
           },
 
           componentDidMount: function () {
           },
 
-          componentWillUnmount: function() {
+          componentWillUnmount: function () {
+          },
+
+          componentWillReceiveProps: function ( iNewProps) {
+            if( iNewProps.value !== this.state.value)
+              this.setState({ value: iNewProps.value});
+          },
+
+          handleChange: function( iEvent) {
+            console.log('handleChange: ' + iEvent.target.value);
+            this.setState({ value: iEvent.target.value});
           },
 
           render: function () {
-            var tValue = this.props.value;
-            return td({}, tValue);
+            var tInput = input({
+                  type: 'text',
+                  value: this.state.value,
+                  onChange: this.handleChange,
+                  autoFocus: true,
+                  onBlur: function( iEvent) {
+                    console.log('blur');
+                  }
+                }),
+                tValue =
+                    span({
+                      className: 'react-data-card-value',
+                      onClick: function () {
+                        if (!this.state.editing)
+                          this.props.onToggleEditing(this);
+                      }.bind(this)
+                    }, this.state.value),
+                tResult =
+                    this.state.editing ?
+                        tInput :
+                        tValue;
+            return td({}, tResult);
           }
         };
       })(), []);
