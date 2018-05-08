548e1b52920cdb71bb05c0fe489248d781864e4e
Jonathan Sandoe <jsandoe@concord.org>
2018-05-08 16:25:20 -0700

    New German translations, and new strings for all locales.

--- apps/dg/tr.lproj/strings.js
+++ apps/dg/tr.lproj/strings.js
@@ -669,5 +669,10 @@ SC.stringsFor("tr", {
     "DG.Locale.name.tr": "Turkish",
     "DG.Locale.name.zh": "Chinese",
     "DG.Undo.graph.swapCategories": "Undo reordering categories",
-    "DG.Redo.graph.swapCategories": "Redo reordering categories"
+    "DG.Redo.graph.swapCategories": "Redo reordering categories",
+    "DG.TableController.collectionTitleText": "%@1 (%@2 cases)",
+    "DG.TableController.collectionTitleTextWithSetAside": "%@1 (%@2 cases, set aside: %@3)",
+    "DG.Inspector.setaside.setAsideSelectedCases": "Set Aside Selected Cases",
+    "DG.Inspector.setaside.setAsideUnselectedCases": "Set Aside Unselected Cases",
+    "DG.Inspector.setaside.restoreSetAsideCases": "Restore %@ Set Aside Cases"
 });
