6c1d25263e9d43512a533e2f4211a65247862e22
Jonathan Sandoe <jsandoe@concord.org>
2018-09-06 14:39:36 -0700

    Update build number manually.
    Build 0458 was done on a branch. To avoid confusion we update to one beyond this build number, 0459

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -267,7 +267,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0457',
+    BUILD_NUM: '0459',
 
     IS_DG_BUILD: isDGBuild.property(),
 
