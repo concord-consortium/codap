358575a61c5a0d9ee4aaadc6e7ba07609f50007f
Jonathan Sandoe <jsandoe@concord.org>
2018-09-05 21:41:50 -0700

    Update build number.
    Build was performed on the "build" branch, not on master. It excluded
    commits relating to the new map layers feature, for expediency. These are:
    
    * [#160190459] Feature: Hiding and showing cases works with multiple map layers William Finzer 8/30/18, 9:35 PM
    * [#158802615] Feature: Maps respond to changes in the available lat/long attributes - Any time the number of data contexts changes, such as dragging a CSV into a document, now causes maps to scan the new data context, add any called-for point or polygon layers, and rescale the map to encompass them. - When a data context is removed, the map responds by removing any relevant layers. William Finzer 8/15/18, 8:28 AM
    * [#159715216] Feature: User can control connecting lines, appearance of points, and appearance of grid for each point layer individually William Finzer 8/12/18, 8:03 PM
    * [#159711680] Feature: Map layers can be hidden or shown using check boxes in Layers palette [#159643936] Feature: Maps can display a legend for each of the legend attributes applied to the map William Finzer 8/10/18, 3:59 PM
    * [#159643921] Feature: When a legend attribute is dropped on a map, the map applies it to the appropriate layer William Finzer 8/8/18, 5:32 PM
    * [#158802692] Feature: Maps can display multiple sets of polygons William Finzer 8/4/18, 2:38 PM
    * [#158802477] Feature: Maps can display lat/long points from more than one dataset William Finzer 7/3/18, 1:31 PM
    
    Because of the exclusion, the version number update cannot be brought
    back into the master codeline, so we manually upgrade the build number.

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -267,7 +267,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0455',
+    BUILD_NUM: '0457',
 
     IS_DG_BUILD: isDGBuild.property(),
 
