cbf2e5ad8aee7028e945993ef266fec81775baf1
William Finzer <wfinzer@concord.org>
2018-09-12 09:47:21 -0700

    Tech debt: removed no-longer-relevant code from DataDisplayController.

--- apps/dg/components/graph_map_common/data_display_controller.js
+++ apps/dg/components/graph_map_common/data_display_controller.js
@@ -200,13 +200,6 @@ DG.DataDisplayController = DG.ComponentController.extend(
               sc_super();
               this_.get('dataDisplayModel').addObserver('canSupportConfigurations', this, 'plotDidChange');
 
-              // Todo: Deal with fact that maps no longer have x&y attributes at this level
-              var tXAttrDescr = this_.getPath('dataDisplayModel.dataConfiguration.xAttributeDescription');
-              if( tXAttrDescr)
-                tXAttrDescr.addObserver('attribute', this, 'plotDidChange');
-              var tYAttrDescr = this_.getPath('dataDisplayModel.dataConfiguration.yAttributeDescription');
-              if( tYAttrDescr)
-                tYAttrDescr.addObserver('attribute', this, 'plotDidChange');
               this.plotDidChange(); // For initialization of visibility
             },
             plotDidChange: function () {
