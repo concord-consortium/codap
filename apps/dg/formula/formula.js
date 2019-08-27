concord-consortium/codap

commit 7fc79f50d5528a662af14eb4bfe75192ceb0db82

[#167411644] Fix bug: date math no longer works
Bug was introduced in build 0489.

apps/dg/formula/formula.js
@@ -286,10 +286,10 @@ DG.Formula.functionRegExp = (function() {
 DG.Formula.add = function(iOperand1, iOperand2) {
   var empty1 = SC.empty(iOperand1),
       empty2 = SC.empty(iOperand2),
-      isDate1 = DG.isDate(iOperand1),
-      isDate2 = DG.isDate(iOperand2),
-      num1 = Number(iOperand1),
-      num2 = Number(iOperand2),
+      isDate1 = DG.isDate(iOperand1) || DG.isDateString(iOperand1),
+      isDate2 = DG.isDate(iOperand2) || DG.isDateString(iOperand2),
+      num1 = Number(iOperand1) || Number(DG.createDate(iOperand1)),
+      num2 = Number(iOperand2) || Number(DG.createDate(iOperand2)),
       // booleans and strings (if possible) converted, not null values
       isNumeric1 = !empty1 && (num1 === num1),
       isNumeric2 = !empty2 && (num2 === num2);
@@ -329,10 +329,10 @@ DG.Formula.add = function(iOperand1, iOperand2) {
 DG.Formula.subtract = function(iOperand1, iOperand2) {
   var empty1 = SC.empty(iOperand1),
       empty2 = SC.empty(iOperand2),
-      isDate1 = DG.isDate(iOperand1),
-      isDate2 = DG.isDate(iOperand2),
-      num1 = Number(iOperand1),
-      num2 = Number(iOperand2),
+      isDate1 = DG.isDate(iOperand1) || DG.isDateString(iOperand1),
+      isDate2 = DG.isDate(iOperand2) || DG.isDateString(iOperand2),
+      num1 = Number(iOperand1) || Number(DG.createDate(iOperand1)),
+      num2 = Number(iOperand2) || Number(DG.createDate(iOperand2)),
       // booleans and strings (if possible) converted, not null values
       isNumeric1 = !empty1 && (num1 === num1),
       isNumeric2 = !empty2 && (num2 === num2);
@@ -369,8 +369,8 @@ DG.Formula.subtract = function(iOperand1, iOperand2) {
 DG.Formula.binaryOperator = function(iOperator, iOperand1, iOperand2) {
   var empty1 = SC.empty(iOperand1),
       empty2 = SC.empty(iOperand2),
-      isDate1 = DG.isDate(iOperand1),
-      isDate2 = DG.isDate(iOperand2),
+      isDate1 = DG.isDate(iOperand1) || DG.isDateString(iOperand1),
+      isDate2 = DG.isDate(iOperand2) || DG.isDateString(iOperand2),
       num1 = Number(iOperand1),
       num2 = Number(iOperand2),
       // booleans and strings (if possible) converted, not null values

commit da27683bcbdfa7083dc610692f84b2de917ddd4f

Fix broken formula unit tests.
Unit tests were broken by commit 7fc79f50d5528a662af14eb4bfe75192ceb0db82 (PT story [#167411644])

Formulas were no longer propagating NaNs. Adjust the order of tests to assure that NaN's propagate.

apps/dg/formula/formula.js
@@ -293,10 +293,6 @@ DG.Formula.add = function(iOperand1, iOperand2) {
       // booleans and strings (if possible) converted, not null values
       isNumeric1 = !empty1 && (num1 === num1),
       isNumeric2 = !empty2 && (num2 === num2);
-  // values interpretable as numeric are added numerically
-  if (isNumeric1 && isNumeric2)
-    return isDate1 !== isDate2 ? DG.createDate((num1 + num2)) : num1 + num2;
-
   // errors propagate
   if (iOperand1 instanceof Error) return iOperand1;
   if (iOperand2 instanceof Error) return iOperand2;
@@ -304,6 +300,10 @@ DG.Formula.add = function(iOperand1, iOperand2) {
   // NaNs propagate
   if ((iOperand1 !== iOperand1) || (iOperand2 !== iOperand2))
     return NaN;
+
+  // values interpretable as numeric are added numerically
+  if (isNumeric1 && isNumeric2)
+    return isDate1 !== isDate2 ? DG.createDate((num1 + num2)) : num1 + num2;
 
   // null values propagate
   if (empty1 && empty2)
@@ -336,13 +336,6 @@ DG.Formula.subtract = function(iOperand1, iOperand2) {
       // booleans and strings (if possible) converted, not null values
       isNumeric1 = !empty1 && (num1 === num1),
       isNumeric2 = !empty2 && (num2 === num2);
-  // values interpretable as numeric are subtracted numerically
-  if (isNumeric1 && isNumeric2) {
-    // date minus number results in date
-    // all other combinations result in a number
-    return (isDate1 && !isDate2) ? DG.createDate((num1 - num2)) : num1 - num2;
-  }
-
   // errors propagate
   if (iOperand1 instanceof Error) return iOperand1;
   if (iOperand2 instanceof Error) return iOperand2;
@@ -350,6 +343,13 @@ DG.Formula.subtract = function(iOperand1, iOperand2) {
   // NaNs propagate
   if ((iOperand1 !== iOperand1) || (iOperand2 !== iOperand2))
     return NaN;
+
+  // values interpretable as numeric are subtracted numerically
+  if (isNumeric1 && isNumeric2) {
+    // date minus number results in date
+    // all other combinations result in a number
+    return (isDate1 && !isDate2) ? DG.createDate((num1 - num2)) : num1 - num2;
+  }
 
   // null values propagate
   if (empty1 && empty2)