46c81023f70afcfcb714d0a04150ec70e8e81eef
eireland <eireland@concord.org>
2019-12-11 06:51:26 -0800

    WIP - updating example document test to verify element visibility

--- cypress/support/elements/WebViewObject.js
+++ cypress/support/elements/WebViewObject.js
@@ -2,5 +2,8 @@ class WebviewTile {
     getWebviewTile(){
         return cy.get('.dg-web-view');
     }
+    getGuideViewTile(){
+        return cy.get('.sc-web-view')
+    }
 }
 export default WebviewTile
