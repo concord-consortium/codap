61e3a43b66c5bfe91ab4cdbbb789bcf7717e8f29
eireland <eireland@concord.org>
2020-01-29 14:49:34 -0800

    Added tests for sample docs and plugins

--- cypress/integration/embeded_docs_test.js
+++ cypress/integration/embeded_docs_test.js
@@ -1,27 +1,20 @@
-const fs = require('fs-extra')
-const path = require('path')
-
-//need to rethink this one because it opens a new tab when it clicks on the listing
-const links_file = "CODAPEmbedLinks.txt";
-const CODAP_URL = "https://codap.concord.org/releases/staging/";
-const queryParam = "?";
-const max_attempts = 10;
-
-var listingArray = []
+//for some reason wants to append codap link in front of links in the file so run the test from the separate codap-cypress-tests project
+before(()=>{
+    cy.fixture("CODAPEmbedLinks.txt").as("exampleDocsData")
+})
 
 context('Load all Sample documents in the CODAP sample doc page', ()=>{
-    var attempt=0
-
     it('will load public page and take a screenshot', ()=>{
         //read in links_file and for each line, visit the site and take a screenshot
-        listingArray = cy.readFile(links_file);
-        listingArray.forEach((listing)=>{
-            cy.visit(listing);
-            cy.wait(5000);
-            cy.matchImageSnapshot(title)
-
+        cy.get('@exampleDocsData').then((file)=>{
+            let i=0;
+            let listing=file.split(',')
+            for (i;i<listing.length;i++){
+                cy.log(listing[i])
+                cy.visit(listing[i]);
+                cy.wait(5000);
+            // cy.matchImageSnapshot(title)
+            }
         })
-
     })
-
 })
