d3ec772cdf9474728586a36a99689917cafc30d7
Kirk Swenson <kswenson@users.noreply.github.com>
2022-09-02 11:52:25 -0700

    V3: Move axis bounds into GraphLayout (#458)

---
+++ v3/src/components/graph/components/droppable-axis.tsx
@@ -0,0 +1,30 @@
+import { Active, useDroppable } from "@dnd-kit/core"
+import { observer } from "mobx-react-lite"
+import React, { CSSProperties } from "react"
+import { createPortal } from "react-dom"
+import { useAxisBounds } from "../hooks/use-axis-bounds"
+import { AxisPlace } from "../models/axis-model"
+
+import "./droppable-svg.scss"
+
+interface IProps {
+  place: AxisPlace
+  portal: HTMLElement | null
+  target: SVGGElement | null
+  dropId: string
+  dropData: any
+  onIsActive?: (active: Active) => boolean
+}
+export const DroppableAxis = observer(({ place, portal, target, dropId, dropData, onIsActive }: IProps) => {
+  const axisBounds = useAxisBounds(place)
+  const { active, isOver, setNodeRef } = useDroppable({ id: dropId, data: dropData })
+  const isActive = active && onIsActive?.(active)
+
+  // calculate the position of the overlay
+  const style: CSSProperties = { ...axisBounds }
+  const classes = `droppable-axis droppable-svg ${place} ${isActive ? "active" : ""} ${isOver ? "over" : ""}`
+  return portal && target && createPortal(
+    <div ref={setNodeRef} className={classes} style={style} />,
+    portal
+  )
+})
