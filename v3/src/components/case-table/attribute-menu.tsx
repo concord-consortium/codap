267851167866ab91802e6bc1357f4036e392c884
Evangeline Ireland <eireland@concord.org>
2022-09-16 15:28:52 -0700

    V3: 183170686 attribute menu UI (#464)
    * Adds attribute menu to case table
    
    * limits cypress thread to one for now
    
    * Fix column header menu
    
    Co-authored-by: Kirk Swenson <kswenson@concord.org>

---
+++ v3/src/components/case-table/attribute-menu.tsx
@@ -0,0 +1,39 @@
+import { MenuItem, MenuList, useToast } from "@chakra-ui/react"
+import React, { forwardRef } from "react"
+import { CalculatedColumn } from "react-data-grid"
+import { TRow } from "./case-table-types"
+
+interface IProps {
+  disableToolTips?: (show: boolean) => void
+  column: CalculatedColumn<TRow, unknown>
+}
+
+// eslint-disable-next-line react/display-name
+export const AttributeMenuList = forwardRef<HTMLDivElement, IProps>(({disableToolTips, column}, ref) => {
+  const toast = useToast()
+
+  const handleMenuItemClick = (menuItem: string) => {
+    toast({
+      title: 'Menu item clicked',
+      description: `You clicked on ${menuItem} on ${column.name}`,
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    })
+  }
+
+  return (
+    <MenuList ref={ref}>
+      <MenuItem onClick={()=>handleMenuItemClick("Rename")}>Rename</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Fit width")}>Fit width to content</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Edit Attribute Properties")}>Edit Attribute Properties...</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Edit Formula")}>Edit Formula...</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Delete Formula")}>Delete Formula (Keeping Values)</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Rerandomize")}>Rerandomize</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Sort Ascending")}>Sort Ascending (A→Z, 0→9)</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Sort Descending")}>Sort Descending (9→0, Z→A)</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Hide Attribute")}>Hide Attribute</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Delete Attribute")}>Delete Attribute</MenuItem>
+    </MenuList>
+  )
+})
