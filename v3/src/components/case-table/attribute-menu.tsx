f4c173facb89a778cf2ee5414e51760495afb24d
eireland <eireland@concord.org>
2022-09-16 15:06:46 -0700

    PR Fixes

--- v3/src/components/case-table/attribute-menu.tsx
+++ v3/src/components/case-table/attribute-menu.tsx
@@ -4,153 +4,36 @@ import { CalculatedColumn } from "react-data-grid"
 import { TRow } from "./case-table-types"
 
 interface IProps {
-  disableToolTips?: (show: boolean) => void;
-  column: CalculatedColumn<TRow, unknown>;
+  disableToolTips?: (show: boolean) => void
+  column: CalculatedColumn<TRow, unknown>
 }
 
 // eslint-disable-next-line react/display-name
 export const AttributeMenuList = forwardRef<HTMLDivElement, IProps>(({disableToolTips, column}, ref) => {
   const toast = useToast()
 
-  const handleRenameAttribute = (e: any) => {
-    console.log("in handleRename")
-
-    e.preventDefault()
+  const handleMenuItemClick = (menuItem: string) => {
     toast({
       title: 'Menu item clicked',
-      description: `You clicked on Rename ${column.name}`,
+      description: `You clicked on ${menuItem} on ${column.name}`,
       status: 'success',
       duration: 5000,
       isClosable: true,
     })
   }
 
-  const handleFitWidth = (e: any) => {
-    console.log("in handleFitWidth")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Fit Width ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleEditAttributeProps  = (e: any) => {
-    console.log("in handleEditAt")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Edit Attribute ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleEditFormula = (e: any) => {
-    console.log("in handleEditFormula")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Edit Formula ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleDeleteFormula = (e: any) => {
-    console.log("in handleDelete")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Delete Formula ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleRerandomize = (e: any) => {
-    console.log("in handleRerandomize")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Rerandomize ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleSortAscending = (e: any) => {
-    console.log("in handleSortAsc")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Sort Ascending ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleSortDescending = (e: any) => {
-    console.log("in handleSortDesc")
-
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Sort Descending ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleHideAttribute = (e: any) => {
-    console.log("in handleHide")
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Hide Attribute ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleDeleteAttribute = (e: any) => {
-    console.log("in handleDelete")
-    e.preventDefault()
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on Delete Attribute ${column.name}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
   return (
     <MenuList ref={ref}>
-      <MenuItem onClick={handleRenameAttribute}>Rename</MenuItem>
-      <MenuItem onClick={handleFitWidth}>Fit width to content</MenuItem>
-      <MenuItem onClick={handleEditAttributeProps}>Edit Attribute Properties...</MenuItem>
-      <MenuItem onClick={handleEditFormula}>Edit Formula...</MenuItem>
-      <MenuItem onClick={handleDeleteFormula}>Delete Formula (Keeping Values)</MenuItem>
-      <MenuItem onClick={handleRerandomize}>Rerandomize</MenuItem>
-      <MenuItem onClick={handleSortAscending}>Sort Ascending (A→Z, 0→9)</MenuItem>
-      <MenuItem onClick={handleSortDescending}>Sort Descending (9→0, Z→A)</MenuItem>
-      <MenuItem onClick={handleHideAttribute}>Hide Attribute</MenuItem>
-      <MenuItem onClick={handleDeleteAttribute}>Delete Attribute</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Rename")}>Rename</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Fit width")}>Fit width to content</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Edit Attribute Properties")}>Edit Attribute Properties...</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Edit Formula")}>Edit Formula...</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Delete Formula")}>Delete Formula (Keeping Values)</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Rerandomize")}>Rerandomize</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Sort Ascending")}>Sort Ascending (A→Z, 0→9)</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Sort Descending")}>Sort Descending (9→0, Z→A)</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Hide Attribute")}>Hide Attribute</MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Delete Attribute")}>Delete Attribute</MenuItem>
     </MenuList>
   )
 })
