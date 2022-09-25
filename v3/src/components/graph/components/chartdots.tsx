e765aaea02a46eb285c25658a9d4699692614a14
William Finzer <wfinzer@concord.org>
2022-09-25 09:50:53 -0700

    * Dot chart now can be on either axis

--- v3/src/components/graph/components/chartdots.tsx
+++ v3/src/components/graph/components/chartdots.tsx
@@ -10,17 +10,23 @@ import {setPointSelection} from "../utilities/graph_utils"
 export const ChartDots = memo(function ChartDots(props: {
   casesRef: React.MutableRefObject<string[]>,
   xAttrID: string,
+  yAttrID: string,
   dotsRef: React.RefObject<SVGSVGElement>
   enableAnimation: React.MutableRefObject<boolean>
 }) {
 
   useInstanceIdContext()
-  const {casesRef, dotsRef, enableAnimation, xAttrID} = props,
+  const {casesRef, dotsRef, enableAnimation, xAttrID, yAttrID} = props,
     dataset = useDataSetContext(),
     layout = useGraphLayoutContext(),
-    xScale = layout.axisScale('bottom') as ScaleType,
-    yScale = layout.axisScale('left') as ScaleType,
-    attribute = dataset?.attrFromID(xAttrID),
+    xAttributeType = dataset?.attrFromID(xAttrID)?.type,
+    // yAttributeType = dataset?.attrFromID(yAttrID)?.type,
+    primaryPlace = xAttributeType === 'categorical' ? 'bottom' : 'left',
+    primaryAttributeID = primaryPlace === 'left' ? yAttrID : xAttrID,
+    secondaryPlace = primaryPlace === 'left' ? 'bottom' : 'left',
+    primaryScale = layout.axisScale(primaryPlace) as ScaleType,
+    secondaryScale = layout.axisScale(secondaryPlace) as ScaleType,
+    attribute = dataset?.attrFromID(primaryAttributeID),
     categories = Array.from(new Set(attribute?.strValues)),
     categoriesMapRef = useRef< { [index: string]: { cell: number, numSoFar: number } }>({})
 
@@ -47,10 +53,7 @@ export const ChartDots = memo(function ChartDots(props: {
     const
       selection = select(dotsRef.current).selectAll(selectedOnly ? '.graph-dot-highlighted' : '.graph-dot'),
       duration = enableAnimation.current ? transitionDuration : 0,
-      onComplete = enableAnimation.current ? () => {
-        enableAnimation.current = false
-      } : undefined,
-      cellWidth = (xScale as ScaleBand<string>).bandwidth(),
+      cellWidth = (primaryScale as ScaleBand<string>).bandwidth(),
       pointDiameter = 2 * defaultRadius
 
     const computeCellParams = () => {
@@ -58,14 +61,14 @@ export const ChartDots = memo(function ChartDots(props: {
           categoriesMapRef.current[cat] = {cell: i, numSoFar: 0}
         })
 
-        const cellHeight = layout.axisLength('left'),
+        const cellHeight = layout.axisLength(secondaryPlace),
           maxInCell = computeMaxOverAllCells(),
           allowedPointsPerColumn = Math.max(1, Math.floor(cellHeight / pointDiameter)),
           allowedPointsPerRow = Math.max(1, Math.floor(cellWidth / pointDiameter)),
           numPointsInRow = Math.max(1, Math.min(allowedPointsPerRow,
             Math.ceil(maxInCell / allowedPointsPerColumn))),
           actualPointsPerColumn = Math.ceil(maxInCell / numPointsInRow),
-          overlap = Math.max(0, ((actualPointsPerColumn + 1) * pointDiameter - cellHeight) /
+          overlap = -Math.max(0, ((actualPointsPerColumn + 1) * pointDiameter - cellHeight) /
             actualPointsPerColumn)
         return {numPointsInRow, overlap}
       },
@@ -74,7 +77,7 @@ export const ChartDots = memo(function ChartDots(props: {
       buildMapOfIndicesByCase = () => {
         const indices: { [index: string]: { cell: number, row: number, column: number } } = {}
         casesRef.current.forEach(anID => {
-          const cat = dataset?.getValue(anID, xAttrID),
+          const cat = dataset?.getValue(anID, primaryAttributeID),
             cell = categoriesMapRef.current[cat].cell,
             numInCell = categoriesMapRef.current[cat].numSoFar++,
             row = Math.floor(numInCell / cellParams.numPointsInRow),
@@ -83,33 +86,40 @@ export const ChartDots = memo(function ChartDots(props: {
         })
         return indices
       },
-      cellIndices = buildMapOfIndicesByCase()
+      onComplete = enableAnimation.current ? () => {
+        enableAnimation.current = false
+      } : undefined,
+      cellIndices = buildMapOfIndicesByCase(),
+      baseCoord = primaryPlace === 'bottom' ? 0 : layout.axisLength('left'),
+      signForOffset = primaryPlace === 'bottom' ? 1 : -1,
+      primaryCenterKey = primaryPlace === 'bottom' ? 'cx' : 'cy',
+      secondaryCenterKey = primaryPlace === 'bottom' ? 'cy' : 'cx'
 
     selection
       .transition()
       .duration(duration)
       .on('end', (id, i) => (i === selection.size() - 1) && onComplete?.())
-      .attr('cx', (anID: string) => {
+      .attr(primaryCenterKey, (anID: string) => {
         if(cellIndices[anID]) {
           const {cell, column} = cellIndices[anID]
-          return (cell + 0.5) * cellWidth + (column + 0.5) * pointDiameter -
+          return baseCoord + signForOffset * ((cell + 0.5) * cellWidth) + (column + 0.5) * pointDiameter -
             cellParams.numPointsInRow * pointDiameter / 2
         }
         else {
           return NaN
         }
       })
-      .attr('cy', (anID: string) => {
+      .attr(secondaryCenterKey, (anID: string) => {
         if(cellIndices[anID]) {
           const {row} = cellIndices[anID]
-          return yScale.range()[0] - (row + 0.5) * pointDiameter + row * cellParams.overlap
+          return secondaryScale.range()[0] - signForOffset * ((row + 0.5) * pointDiameter + row * cellParams.overlap)
         }
         else {
           return NaN
         }
       })
-  }, [dotsRef, enableAnimation, xScale, yScale, dataset, xAttrID, casesRef,
-    layout, categories, categoriesMapRef, computeMaxOverAllCells])
+  }, [dotsRef, enableAnimation, primaryScale, secondaryScale, dataset, casesRef, primaryPlace, secondaryPlace,
+    layout, categories, categoriesMapRef, computeMaxOverAllCells, primaryAttributeID])
 
   useEffect(()=>{
     select(dotsRef.current).on('click', (event) => {
