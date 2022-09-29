e6639af0dd6d5915767e1c12bcf83d8ecda3b149
Tejal Shah <tejal.shah@tscube.com>
2022-09-29 00:14:30 -0400

    Updating Cypress and removing deprecated plugins/index.js

--- v3/cypress.config.ts
+++ v3/cypress.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("@cypress/code-coverage/task")(on, config)
     },
     baseUrl: 'http://localhost:8080',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
