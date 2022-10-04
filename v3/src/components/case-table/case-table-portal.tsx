0f6505468b76eef1a72931b244408af2e20cf415
Kirk Swenson <kswenson@users.noreply.github.com>
2022-10-04 08:55:31 -0700

    V3: Fix click handling in case table column header menus (#494)

---
+++ v3/src/components/case-table/case-table-portal.tsx
@@ -0,0 +1,21 @@
+import React, { ReactNode } from "react"
+import { PortalWithEventBlock } from "../portal-with-event-block"
+
+/*
+  See comment in PortalWithEventBlock for details on the broader problem being addressed here.
+  In the specific case of the case table, ReactDataGrid adds an onPointerDown handler to resizable
+  column headers to implement user-resizing of columns. That onPointerDown handler calls setPointerCapture,
+  which prevents subsequent mouse events from being dispatched in the normal fashion. When portaled
+  menus or dialogs are rendered from clicks in the case table header, events targeting the portaled
+  menus or dialogs can bubble up to the case table, triggering the onPointerDown handler and preventing
+  the portaled menus or dialogs from responding as expected. Therefore, we make use of PortalWithEventBlock
+  to implement a case table-specific portal which blocks the onPointerDown event. If necessary, other events
+  can be added to the list of blocked events as the need arises.
+ */
+export const CaseTablePortal = ({ children }: { children: ReactNode }) => {
+  return (
+    <PortalWithEventBlock blockedEvents={["onPointerDown"]}>
+      {children}
+    </PortalWithEventBlock>
+  )
+}
--- v3/src/components/case-table/column-header.tsx
+++ v3/src/components/case-table/column-header.tsx
@@ -1,19 +1,13 @@
 import { Tooltip, Menu, MenuButton, Input } from "@chakra-ui/react"
 import { useDndContext } from "@dnd-kit/core"
-import React, { useEffect, useRef, useState } from "react"
-import { createPortal } from "react-dom"
+import React, { useRef, useState } from "react"
 import { kIndexColumnKey, THeaderRendererProps } from "./case-table-types"
 import { ColumnHeaderDivider } from "./column-header-divider"
+import { useDataSetContext } from "../../hooks/use-data-set-context"
 import { IUseDraggableAttribute, useDraggableAttribute } from "../../hooks/use-drag-drop"
 import { useInstanceIdContext } from "../../hooks/use-instance-id-context"
 import { AttributeMenuList } from "./attribute-menu"
-import { useDataSetContext } from "../../hooks/use-data-set-context"
-
-const isClickInElement = (click: MouseEvent, elt: HTMLElement | null) => {
-  const bounds = elt?.getBoundingClientRect()
-  return !!bounds && (bounds.left <= click.clientX) && (click.clientX <= bounds.right) &&
-                      (bounds.top <= click.clientY) && (click.clientY <= bounds.bottom)
-}
+import { CaseTablePortal } from "./case-table-portal"
 
 export const ColumnHeader = ({ column }: Pick<THeaderRendererProps, "column">) => {
   const { active } = useDndContext()
@@ -21,7 +15,6 @@ export const ColumnHeader = ({ column }: Pick<THeaderRendererProps, "column">) =
   const instanceId = useInstanceIdContext() || "table"
   const [contentElt, setContentElt] = useState<HTMLElement | null>(null)
   const cellElt = contentElt?.parentElement || null
-  const [codapComponentElt, setCodapComponentElt] = useState<HTMLElement | null>(null)
   const isMenuOpen = useRef(false)
   const menuListElt = useRef<HTMLDivElement>(null)
   const [editingAttrId, setEditingAttrId] = useState("")
@@ -36,33 +29,6 @@ export const ColumnHeader = ({ column }: Pick<THeaderRendererProps, "column">) =
     setContentElt(elt)
     setDragNodeRef(elt?.parentElement || null)
   }
-
-  useEffect(() => {
-    // Find the parent CODAP component to display the attribute menu above the grid
-    const codapComponent = contentElt?.closest(".codap-component") as HTMLDivElement
-    setCodapComponentElt(codapComponent ?? null)
-
-    // There is a bizarre bug somewhere in the intersection of Chrome, React, Chakra menus, and
-    // ReactDataGrid that causes clicks on menu items roughly to the right of the original column
-    // bounds to be dispatched _by the browser_ directly to the .codap-component, bypassing the
-    // menu and its items altogether. Clicks on the left side of the menu items are dispatched
-    // properly, however. Furthermore, the problem doesn't occur in Firefox or Safari.
-    // The same menu works fine when used in other components besides the case table, and the
-    // index menu works fine within the case table. It's only the column header menus that fail.
-    // The workaround in this case is to detect that this has occurred and manually dispatch a
-    // copy of the event to the appropriate menu item. ¯\_(ツ)_/¯
-    const handleClick = (e: MouseEvent) => {
-      const targetIsComponent = (e.target as HTMLElement).className.includes("codap-component")
-      if (isMenuOpen.current && targetIsComponent && isClickInElement(e, menuListElt.current)) {
-        const items = menuListElt.current?.querySelectorAll("button")
-        const clickedItem = Array.from(items || []).find(item => isClickInElement(e, item))
-        clickedItem?.dispatchEvent(new MouseEvent(e.type, e))
-      }
-    }
-
-    codapComponent?.addEventListener("click", handleClick)
-    return () => codapComponent?.removeEventListener("click", handleClick)
-  }, [contentElt])
 
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     const { key } = e
@@ -98,7 +64,7 @@ export const ColumnHeader = ({ column }: Pick<THeaderRendererProps, "column">) =
           <>
             <Tooltip label={column.name || "attribute"} h="20px" fontSize="12px" color="white"
                 openDelay={1000} placement="bottom" bottom="15px" left="15px" data-testid="case-table-attribute-tooltip"
-                isDisabled={dragging || editingAttrId === column.key} closeOnMouseDown={true}>
+                isDisabled={dragging || isOpen || editingAttrId === column.key} closeOnMouseDown={true}>
               <div className="codap-column-header-content" ref={setCellRef} {...attributes} {...listeners}>
                 { editingAttrId
                   ? <Input value={editingAttrName} data-testid="column-name-input" size="xs" autoFocus={true}
@@ -110,13 +76,13 @@ export const ColumnHeader = ({ column }: Pick<THeaderRendererProps, "column">) =
                       {column?.name}
                     </MenuButton>
                 }
+                <CaseTablePortal>
+                  <AttributeMenuList ref={menuListElt} column={column} onRenameAttribute={handleRenameAttribute}/>
+                </CaseTablePortal>
                 {column &&
                   <ColumnHeaderDivider key={column?.key} columnKey={column?.key} cellElt={cellElt}/>}
               </div>
             </Tooltip>
-            {codapComponentElt && createPortal((
-              <AttributeMenuList ref={menuListElt} column={column} onRenameAttribute={handleRenameAttribute}/>
-            ), codapComponentElt)}
           </>
         )
     }}
