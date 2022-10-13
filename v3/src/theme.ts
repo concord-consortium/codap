0fe3046142e0779028ee31538181e4adf99b85b7
eireland <eireland@concord.org>
2022-10-13 16:25:26 -0700

    Changes theme medium font size to 12px

--- v3/src/theme.ts
+++ v3/src/theme.ts
@@ -1,9 +1,18 @@
 import { extendTheme } from '@chakra-ui/react'
 
 export const theme = extendTheme({
+  config: {
+    cssVarPrefix: 'codap',
+  },
   fonts: {
     // CODAP v2 default font stack
-    body: "Montserrat, sans-serif"
+    body: "Montserrat, sans-serif",
+  },
+  fontSizes: {
+    md: "12px"
+  },
+  colors: {
+    darkTeal: "#01879E"
   },
   components: {
     Button: {
