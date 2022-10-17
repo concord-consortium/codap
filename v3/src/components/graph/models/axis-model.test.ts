2a5d2fa9cad0d790df5b9a85fe68e87d620246a3
William Finzer <wfinzer@concord.org>
2022-10-17 10:35:37 -0700

    * When a numeric axis is rescaled by dragging, the zero of the scale will snap to the lower or upper bound when the zero is sufficiently close to that boundary. (#516)

--- v3/src/components/graph/models/axis-model.test.ts
+++ v3/src/components/graph/models/axis-model.test.ts
@@ -5,8 +5,21 @@ describe("AxisModel", () => {
     expect(() => AxisModel.create({ place: "left" })).toThrow()
   })
 
-  it("should work as expected", () => {
+})
+
+describe("NumericAxisModel", () => {
+  it("should compute the correct orientation", () => {
     expect(NumericAxisModel.create({ place: "bottom", min: 0, max: 10 }).orientation).toBe("horizontal")
     expect(NumericAxisModel.create({ place: "left", min: 0, max: 10 }).orientation).toBe("vertical")
+  })
+  it("should snap to zero for minimum", () => {
+    const aModel = NumericAxisModel.create({ place: "bottom", min: 10, max: 100 })
+    aModel.setDomain(0.5, 100)
+    expect(aModel.min).toBe(0)
+  })
+  it("should snap to zero for maximum", () => {
+    const aModel = NumericAxisModel.create({ place: "bottom", min: -100, max: -10 })
+    aModel.setDomain(-100, -0.5)
+    expect(aModel.max).toBe(0)
   })
 })
