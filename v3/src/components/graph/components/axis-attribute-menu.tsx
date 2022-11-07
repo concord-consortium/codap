437eed1605a2d35a5c0231c3334a1370deba892e
Joe Bacal <bacalj@gmail.com>
2022-11-06 21:06:04 -0500

    reorganized, but same issues

--- v3/src/components/graph/components/axis-attribute-menu.tsx
+++ v3/src/components/graph/components/axis-attribute-menu.tsx
@@ -1,8 +1,8 @@
-import { Menu, MenuItem, MenuList, MenuButton, MenuDivider, useToast } from "@chakra-ui/react"
-import React, { CSSProperties, useEffect, useState } from "react"
+import { Menu, MenuItem, MenuList, MenuButton, MenuDivider } from "@chakra-ui/react"
+import React, { CSSProperties, useLayoutEffect, useState } from "react"
 import { useDataSetContext } from "../../../hooks/use-data-set-context"
-import { GraphPlace } from "../models/axis-model"
-import { Bounds, useGraphLayoutContext } from "../models/graph-layout"
+import { AxisPlace, GraphPlace } from "../models/axis-model"
+import { useGraphLayoutContext, Bounds} from "../models/graph-layout"
 import { measureText } from "../../../hooks/use-measure-text"
 
 import "./axis-attribute-menu"
@@ -20,43 +20,40 @@ export const AxisAttributeMenu = ({ attrId, place, onChangeAttribute, onTreatAs 
   const treatAs = attribute?.type === "numeric" ? "categorical" : "numeric"
   const layout = useGraphLayoutContext()
   const { margin } = layout
-  const textLength = measureText(attribute?.name as string)
-  const [menuButtonLeft, setMenuButtonLeft] = useState<CSSProperties>({ left: 0 })
+  const textLength = measureText(attribute?.name || "")
+  const [buttonLeft, setButtonLeft] = useState<CSSProperties>({ left: 0 })
 
-  // TODO - there must be a better, more functional way to do this
-  // also calculation is still just ok
-  useEffect(()=>{
-    setTimeout(()=>{
-      const foundBounds = layout.getAxisBounds(place as any)
-      console.log("foundbonds left and width: ", foundBounds?.left, foundBounds?.width)
-      const axisWidth = foundBounds ? foundBounds.width : 0
-      if (foundBounds){
-        if (place === "left"){
-          const lCalc = 0 - (.5 * axisWidth)
-          setMenuButtonLeft({ left: lCalc})
-          console.log("I had axisWidth: ", axisWidth, " for attr ", attribute?.name ," and set menuButtonLeft to:  ", lCalc)
-        }
-        else {
-          const bCalc = (axisWidth * .5) - (textLength * .5) + margin.left - 5 //compensate for non-centered text?
-          setMenuButtonLeft({ left: bCalc })
-        }
-      }
-    }, 100)
-  }, [attrId])
-
-  const menuButtonStyles: CSSProperties = {
+  const buttonStyles: CSSProperties = {
     position: "absolute",
     color: "transparent",
+    background: "blue",
+    opacity: 0.1,
     rotate: place === "left" ? "270deg" : "0deg",
-    top: place === "left" ? (.5 * layout.plotHeight) - (.2 * textLength) : layout.plotHeight + 20,
-    opacity:.2,
-    background: "blue"
+    top: place === "left" ? (.5 * layout.plotHeight) - (.2 * textLength) : layout.plotHeight + 20
   }
+
+  const calculateButtonLeft = (bounds: Bounds) => {
+    const axisWidth = bounds.width
+    if (place === "left") return { left: 0 - (.5 * axisWidth) }
+    if (place === "bottom") return { left: (axisWidth * .5) - (textLength * .5) + margin.left - 5 }
+  }
+
+  useLayoutEffect(() => {
+    setTimeout(()=>{
+      const foundBounds = layout.getAxisBounds(place as AxisPlace)
+      if (foundBounds){
+        const newLeft = calculateButtonLeft(foundBounds)
+        setButtonLeft(newLeft || { left: 0 })
+      }
+    }, 100)
+  },[attrId])
+
+  const compiledStyles = {...buttonStyles, ...buttonLeft }
 
   return (
     <div className="axis-attribute-menu">
       <Menu>
-        <MenuButton style={{...menuButtonStyles, ...menuButtonLeft}}>{attribute?.name}</MenuButton>
+        <MenuButton style={{...compiledStyles }}>{attribute?.name}</MenuButton>
         <MenuList>
           { data?.attributes?.map((attr) => {
             return (
