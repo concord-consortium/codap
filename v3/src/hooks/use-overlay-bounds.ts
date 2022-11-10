a2cecfd25d0af6cfa4235932a8e1098d86323f82
Joe Bacal <bacalj@gmail.com>
2022-11-10 14:18:00 -0500

    falls apart when both dimensions change, about to branch for experiment

--- v3/src/hooks/use-overlay-bounds.ts
+++ v3/src/hooks/use-overlay-bounds.ts
@@ -15,26 +15,31 @@ interface IBounds {
 export function useOverlayBounds({ target, portal }: IProps) {
   const [overlayBounds, setOverlayBounds] = useState<IBounds | null>(null)
   const {graphHeight, legendHeight} = useGraphLayoutContext()
-  const { ...all } = useGraphLayoutContext()
-  console.log("ALL!", all)
+
+  const measureAndSetBounds = () => {
+    const portalBounds = portal?.getBoundingClientRect()
+    const targetBounds = target?.getBoundingClientRect()
+    if (targetBounds) {
+      setOverlayBounds({
+        left: targetBounds.x - (portalBounds?.x ?? 0),
+        top: targetBounds.y - (portalBounds?.y ?? 0),
+        width: targetBounds.width,
+        height: targetBounds.height
+      })
+    }
+  }
   useEffect(() => {
-    // track the bounds of the target element relative to the portal element
     const observer = target && new ResizeObserver(() => {
-      const portalBounds = portal?.getBoundingClientRect()
-      const targetBounds = target?.getBoundingClientRect()
-      if (targetBounds) {
-        setOverlayBounds({
-          left: targetBounds.x - (portalBounds?.x ?? 0),
-          top: targetBounds.y - (portalBounds?.y ?? 0),
-          width: targetBounds.width,
-          height: targetBounds.height
-        })
-      }
+      console.log("NEW?")
+      measureAndSetBounds()
     })
     target && observer?.observe(target)
-
     return () => observer?.disconnect()
-  }, [portal, target, graphHeight, legendHeight])
+  }, [portal, target])
+
+  useEffect(() => {
+    measureAndSetBounds()
+  }, [graphHeight, legendHeight])
 
   return overlayBounds
 }
