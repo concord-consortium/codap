01c76c00ab365b19eb0613c80523d2a3658f2f22
eireland <eireland@concord.org>
2022-11-28 12:48:04 -0800

    Changes click handler to toasts

--- v3/src/components/case-table/inspector-panel/ruler-menu-list.tsx
+++ v3/src/components/case-table/inspector-panel/ruler-menu-list.tsx
@@ -1,4 +1,4 @@
-import { MenuItem, MenuList } from "@chakra-ui/react"
+import { MenuItem, MenuList, useToast } from "@chakra-ui/react"
 import React from "react"
 import { useDataSetContext } from "../../../hooks/use-data-set-context"
 import { uniqueName } from "../../../utilities/js-utils"
@@ -6,6 +6,16 @@ import t from "../../../utilities/translation/translate"
 
 export const RulerMenuList = () => {
   const data = useDataSetContext()
+  const toast = useToast()
+  const handleMenuItemClick = (menuItem: string) => {
+    toast({
+      title: 'Menu item clicked',
+      description: `You clicked on ${menuItem}`,
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    })
+  }
 
   const handleAddNewAttribute = () => {
     const newAttrName = uniqueName("newAttr",
@@ -14,25 +24,20 @@ export const RulerMenuList = () => {
     data?.addAttribute({name: newAttrName})
   }
 
-  const handleDeleteSelectedCases = () => {
-    data?.removeCases(Array.from(data.selection))
-  }
-
-   const handleDeleteUnselectedCases = () => {
-    data?.removeCases(data.cases.filter(c => !data.selection.has(c.__id__)).map(c => c.__id__))
-  }
-
-  const handleDeleteAllCases = () => {
-    data?.removeCases(Array.from(data.cases.map(c => c.__id__)))
-  }
-
   return (
     <MenuList data-testid="trash-menu-list">
-      <MenuItem onClick={handleAddNewAttribute}>{t("DG.Inspector.newAttribute")}</MenuItem>
-      <MenuItem onClick={handleDeleteSelectedCases}>{t("DG.Inspector.randomizeAllAttributes")}</MenuItem>
-      <MenuItem onClick={handleDeleteUnselectedCases}>{t("DG.Inspector.exportCaseData")}</MenuItem>
-      <MenuItem onClick={handleDeleteAllCases}>{t("DG.Inspector.copyCaseDataToClipboard")}</MenuItem>
-      <MenuItem onClick={handleDeleteAllCases}>{t("DG.Inspector.getCaseDataFromClipboard")}</MenuItem>
+      <MenuItem
+        onClick={handleAddNewAttribute}>{t("DG.Inspector.newAttribute", {vars:{varName: data?.name || ""}})}
+      </MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Rerandomize All")}>{t("DG.Inspector.randomizeAllAttributes")}
+      </MenuItem>
+      <MenuItem onClick={()=>handleMenuItemClick("Export Case Data")}>{t("DG.Inspector.exportCaseData")}</MenuItem>
+      <MenuItem
+        onClick={()=>handleMenuItemClick("Copy Case Data To Clipboard")}>{t("DG.Inspector.copyCaseDataToClipboard")}
+      </MenuItem>
+      <MenuItem
+        onClick={()=>handleMenuItemClick("Get Case Data From Clipboard")}>{t("DG.Inspector.getCaseDataFromClipboard")}
+      </MenuItem>
     </MenuList>
   )
 }
