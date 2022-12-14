a7a61f193ffff37cb36b546abbe7ac111703388a
eireland <eireland@concord.org>
2022-12-13 17:26:02 -0800

    Refactors insert cases modal and adds aria labels to inputs

--- v3/src/components/case-table/insert-cases-modal.tsx
+++ v3/src/components/case-table/insert-cases-modal.tsx
@@ -1,5 +1,5 @@
 import React, { useRef } from "react"
-import { FormControl, FormLabel, HStack, NumberDecrementStepper, NumberIncrementStepper, NumberInput,
+import { Flex, FormControl, FormLabel, HStack, NumberDecrementStepper, NumberIncrementStepper, NumberInput,
   NumberInputField, NumberInputStepper, Radio, RadioGroup } from "@chakra-ui/react"
 import t from "../../utilities/translation/translate"
 
@@ -15,21 +15,28 @@ export const InsertCasesModalContent: React.FC<IInsertCasesModalProps> =
   const initialRef = useRef(null)
 
   return (
-    <FormControl display="flex" flexDirection="column">
-      <FormLabel h="20px" display="flex" flexDirection="row" alignItems="center">
-        {t("DG.CaseTable.insertCasesDialog.numCasesPrompt")}
+    <Flex flexDirection="column">
+      <FormControl className="form-control">
+        <FormLabel mb="0" aria-label={t("DG.CaseTable.insertCasesDialog.numCasesPrompt")}>
+          {t("DG.CaseTable.insertCasesDialog.numCasesPrompt")}
+        </FormLabel>
         <NumberInput size="xs" w="80px" min={0} ml={5} defaultValue={1} data-testid="num-case-input"
                     value={numCasesToInsert} onFocus={(e) => e.target.select()}
                     onChange={value => onChangeNumCasesToInsert(value)}>
-          <NumberInputField ref={initialRef} placeholder="0" />
+          <NumberInputField ref={initialRef} placeholder="0"
+            aria-label={t("DG.CaseTable.insertCasesDialog.numCasesPrompt")} />
           <NumberInputStepper>
             <NumberIncrementStepper data-testid="num-case-input-increment-up"/>
             <NumberDecrementStepper data-testid="num-case-input-incement-down"/>
           </NumberInputStepper>
         </NumberInput>
-      </FormLabel>
-      <FormLabel display="flex" flexDirection="row">{t("DG.CaseTable.insertCasesDialog.beforeAfter.prompt")}
-        <RadioGroup onChange={value => onChangeInsertPosition(value)} value={insertPosition} ml={5}>
+      </FormControl>
+      <FormControl className="form-control">
+        <FormLabel mb="0" aria-label={t("DG.CaseTable.insertCasesDialog.beforeAfter.prompt")}>
+          {t("DG.CaseTable.insertCasesDialog.beforeAfter.prompt")}
+        </FormLabel>
+        <RadioGroup value={insertPosition} ml={5} aria-label={t("DG.CaseTable.insertCasesDialog.beforeAfter.prompt")}
+            onChange={value => onChangeInsertPosition(value)}>
           <HStack>
             <Radio value="before" data-testid="add-before">
               {t("DG.CaseTable.insertCasesDialog.beforeAfter.before")}
@@ -39,7 +46,7 @@ export const InsertCasesModalContent: React.FC<IInsertCasesModalProps> =
             </Radio>
           </HStack>
         </RadioGroup>
-      </FormLabel>
-    </FormControl>
+      </FormControl>
+    </Flex>
   )
 }
