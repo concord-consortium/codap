ea8e15d4679a8b595c4933f5e6159aed1dafc74a
eireland <eireland@concord.org>
2022-12-15 16:17:02 -0800

    Refactors codap modal so that content of modal is rendered in child component instead of in the modal component

--- v3/src/components/codap-modal.tsx
+++ v3/src/components/codap-modal.tsx
@@ -1,81 +1,25 @@
 import { Modal,
-  ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter,
-  Button,
-  Tooltip} from "@chakra-ui/react"
-import React, { forwardRef } from "react"
+  ModalOverlay, ModalContent } from "@chakra-ui/react"
+import React, { ReactNode, forwardRef } from "react"
 
 import "./codap-modal.scss"
 
-interface IModalButton {
-  className?: string;
-  label: string | React.FC<any>;
-  tooltip?: string
-  isDefault?: boolean;
-  isDisabled?: boolean;
-  onClick?: (() => void) | (() => boolean); // close dialog on falsy return value
-}
-
-interface IProps<TContentProps> {
-  children?: any
+interface IProps {
+  children?: ReactNode
   isOpen: boolean
   onClose: () => void
-  className?: string
-  title: string
-  Icon?: React.FC<any>
-  Content: React.FC<TContentProps>
-  contentProps?: TContentProps | any
-  focusElement?: string
-  canCancel?: boolean
-  hasCloseButton?: boolean //top right "X" button
-  // defined left-to-right, e.g. Extra Button, Cancel, OK
-  buttons: IModalButton[]
-  onCustomClose?: () => void
+  modalWidth?: string
+
 }
 
-export const CodapModal = forwardRef(<IContentProps,>({ isOpen, onClose,
-  className, Icon, title, Content, contentProps, hasCloseButton, buttons, onCustomClose
-}: IProps<IContentProps>, ref: React.LegacyRef<HTMLElement> | undefined) => {
-
-  const handleModalButtonKeydown = (e:React.KeyboardEvent, clickHandler: any) => {
-    const { key } = e
-    e.stopPropagation()
-    switch (key) {
-      case "Space":
-      case "Enter":
-        clickHandler()
-        break
-    }
-  }
+export const CodapModal = forwardRef(({ children, isOpen, onClose, modalWidth
+}: IProps, ref: React.LegacyRef<HTMLElement> | undefined) => {
 
   return (
     <Modal isOpen={isOpen} onClose={onClose} data-testid="codap-modal" size="xs">
       <ModalOverlay />
-      <ModalContent ref={ref} className="codap-modal-content" w={contentProps.modalWidth || "400px"}>
-        <ModalHeader h="30" className="codap-modal-header" fontSize="md" data-testid="codap-modal-header">
-          <div className="codap-modal-icon-container">
-            {Icon && <Icon />}
-          </div>
-          <div className="codap-header-title">{title}</div>
-          {hasCloseButton && <ModalCloseButton onClick={onClose} data-testid="modal-close-button"/>}
-        </ModalHeader>
-        <ModalBody>
-          <Content {...contentProps} />
-        </ModalBody>
-        <ModalFooter mt="-5">
-          {buttons.map((b: any, i)=>{
-            const key = `${i}-${b.className}`
-            return (
-              <Tooltip key={key} label={b.tooltip} h="20px" fontSize="12px"
-                color="white" openDelay={1000} placement="bottom" bottom="15px" left="15px"
-                data-testid="modal-tooltip">
-                <Button key={key} size="xs" variant="ghost" ml="5" onClick={b.onClick} data-testid={`${b.label}-button`}
-                  onKeyDown={(e)=>handleModalButtonKeydown(e, b.onClick)}>
-                  {b.label}
-                </Button>
-              </Tooltip>
-            )
-          })}
-        </ModalFooter>
+      <ModalContent ref={ref} className="codap-modal-content" w={modalWidth || "400px"}>
+        {children}
       </ModalContent>
     </Modal>
   )
