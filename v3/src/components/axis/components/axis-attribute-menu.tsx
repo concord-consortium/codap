e50d4b0cee26525f8c544d938364aea42b23454d
Joe Bacal <bacalj@gmail.com>
2022-12-19 16:56:04 -0500

    initial setup of useCodapOutsideClick, identifying at click event thse d3 elements where mousedowns have been named drags

--- v3/src/components/axis/components/axis-attribute-menu.tsx
+++ v3/src/components/axis/components/axis-attribute-menu.tsx
@@ -1,11 +1,12 @@
 import { Menu, MenuItem, MenuList, MenuButton, MenuDivider } from "@chakra-ui/react"
-import React, { CSSProperties } from "react"
+import React, { CSSProperties, useRef, useState } from "react"
 import { useDataSetContext } from "../../../hooks/use-data-set-context"
 import { GraphPlace } from "../../graph/graphing-types"
-import { useGraphLayoutContext} from "../../graph/models/graph-layout"
+import { useGraphLayoutContext } from "../../graph/models/graph-layout"
 import t from "../../../utilities/translation/translate"
 import { useOverlayBounds } from "../../../hooks/use-overlay-bounds"
 import { useDataConfigurationContext } from "../../graph/hooks/use-data-configuration-context"
+import { useCodapOutsideClick } from "../../../hooks/use-codap-outside-click"
 
 interface IProps {
   place: GraphPlace,
@@ -25,42 +26,67 @@ export const AxisAttributeMenu = ({ place, target, portal, onChangeAttribute, on
   const treatAs = dataConfig?.attributeType(role) === "numeric" ? "categorical" : "numeric"
   const overlayBounds = useOverlayBounds({target, portal})
   const buttonStyles: CSSProperties = { position: "absolute", color: "transparent" }
+  const menu = useRef<any>()
+  const [menuStatus, setMenuStatus] = useState(false)
 
   if (!attrId && place === "bottom"){
     buttonStyles.top = plotHeight + 4
     buttonStyles.left = ( plotWidth * .5 ) - 8 // ~ width of y scale
   }
 
+
+  const closeIfNeeded = (e: any, m: any) => {
+    /* I found that where D3 drag events had been defined (drag rects, plot) that
+      e.composedPath()[0].__on[0] ends up as something like this:
+
+      {type: 'mousedown', name: 'drag', ... }
+
+      "renaming" the event so Chakra does not recognize it?
+    */
+    const mouseDownRenamedAtTarget = e.composedPath()[0].__on !== undefined
+    console.log("handle specially: ", mouseDownRenamedAtTarget)
+    console.log("menu: ", menu)
+  }
+
+  useCodapOutsideClick({
+    ref: menu,
+    handler: (e) => closeIfNeeded(e, menu)
+  })
+
   return (
-    <div className="axis-attribute-menu">
+    <div className="axis-attribute-menu" ref={menu}>
       <Menu>
+      {({ isOpen }) => (
+        <>
         <MenuButton style={{ ...overlayBounds, ...buttonStyles }}>{attribute?.name}</MenuButton>
-        <MenuList>
-          { data?.attributes?.map((attr) => {
-            return (
-              <MenuItem onClick={() => onChangeAttribute(place, attr.id)} key={attr.id}>
-                {attr.name}
-              </MenuItem>
-            )
-          })}
-          { attribute &&
-            <>
-              <MenuDivider />
-              <MenuItem onClick={() => onChangeAttribute(place, "")}>
-                { place === "left" &&
-                  t("DG.DataDisplayMenu.removeAttribute_y", {vars: [attribute?.name]})
-                }
-                { place === "bottom" &&
-                  t("DG.DataDisplayMenu.removeAttribute_x", {vars: [attribute?.name]})
-                }
-              </MenuItem>
-              <MenuItem onClick={() => onTreatAttributeAs(place, attribute?.id, treatAs)}>
-                {treatAs === "categorical" && t("DG.DataDisplayMenu.treatAsCategorical")}
-                {treatAs === "numeric" && t("DG.DataDisplayMenu.treatAsNumeric")}
-              </MenuItem>
-            </>
-          }
-        </MenuList>
+          <MenuList>
+            { data?.attributes?.map((attr) => {
+              return (
+                <MenuItem onClick={() => onChangeAttribute(place, attr.id)} key={attr.id}>
+                  {attr.name}
+                </MenuItem>
+              )
+            })}
+            { attribute &&
+              <>
+                <MenuDivider />
+                <MenuItem onClick={() => onChangeAttribute(place, "")}>
+                  { place === "left" &&
+                    t("DG.DataDisplayMenu.removeAttribute_y", {vars: [attribute?.name]})
+                  }
+                  { place === "bottom" &&
+                    t("DG.DataDisplayMenu.removeAttribute_x", {vars: [attribute?.name]})
+                  }
+                </MenuItem>
+                <MenuItem onClick={() => onTreatAttributeAs(place, attribute?.id, treatAs)}>
+                  {treatAs === "categorical" && t("DG.DataDisplayMenu.treatAsCategorical")}
+                  {treatAs === "numeric" && t("DG.DataDisplayMenu.treatAsNumeric")}
+                </MenuItem>
+              </>
+            }
+          </MenuList>
+        </>
+      )}
       </Menu>
     </div>
   )
---
+++ v3/src/hooks/use-codap-outside-click.ts
@@ -0,0 +1,99 @@
+/* substitute, for the moment, for Chakra's useOutsideClick hook */
+
+import { useEffect, useRef } from "react"
+import { useCallbackRef } from "@chakra-ui/react-use-callback-ref"
+
+export interface UseCodapOutsideClickProps {
+  /**
+   * Whether the hook is enabled
+   */
+  enabled?: boolean
+  /**
+   * The reference to a DOM element.
+   */
+  ref: React.RefObject<HTMLElement>
+  /**
+   * Function invoked when a click is triggered outside the referenced element.
+   */
+  handler?: (e: Event) => void
+}
+
+/**
+ * Example, used in components like Dialogs and Popovers, so they can close
+ * when a user clicks outside them.
+ */
+export function useCodapOutsideClick(props: UseCodapOutsideClickProps) {
+  const { ref, handler, enabled = true } = props
+  const savedHandler = useCallbackRef(handler)
+
+  const stateRef = useRef({
+    isPointerDown: false,
+    ignoreEmulatedMouseEvents: false,
+  })
+
+  const state = stateRef.current
+
+  useEffect(() => {
+    if (!enabled) return
+    const onPointerDown: any = (e: PointerEvent) => {
+      if (isValidEvent(e, ref)) {
+        state.isPointerDown = true
+      }
+    }
+
+    const onMouseUp: any = (event: MouseEvent) => {
+      if (state.ignoreEmulatedMouseEvents) {
+        state.ignoreEmulatedMouseEvents = false
+        return
+      }
+
+      if (state.isPointerDown && handler && isValidEvent(event, ref)) {
+        state.isPointerDown = false
+        savedHandler(event)
+      }
+    }
+
+    const onTouchEnd = (event: TouchEvent) => {
+      state.ignoreEmulatedMouseEvents = true
+      if (handler && state.isPointerDown && isValidEvent(event, ref)) {
+        state.isPointerDown = false
+        savedHandler(event)
+      }
+    }
+
+    // after events have passed through D3 target, they are Pointer events, not "mouseup"
+    // so we need Chakra to register "pointerup" and "pointerdown"  here
+    const doc = getOwnerDocument(ref.current)
+    doc.addEventListener("pointerdown", onPointerDown, true)
+    doc.addEventListener("pointerup", onMouseUp, true)
+    doc.addEventListener("mousedown", onPointerDown, true)
+    doc.addEventListener("mouseup", onMouseUp, true)
+    doc.addEventListener("touchstart", onPointerDown, true)
+    doc.addEventListener("touchend", onTouchEnd, true)
+
+    return () => {
+      doc.removeEventListener("pointerdown", onPointerDown, true)
+      doc.removeEventListener("pointerup", onMouseUp, true)
+      doc.removeEventListener("mousedown", onPointerDown, true)
+      doc.removeEventListener("mouseup", onMouseUp, true)
+      doc.removeEventListener("touchstart", onPointerDown, true)
+      doc.removeEventListener("touchend", onTouchEnd, true)
+    }
+  }, [handler, ref, savedHandler, state, enabled])
+}
+
+function isValidEvent(event: any, ref: React.RefObject<HTMLElement>) {
+  const target = event.target as HTMLElement
+  if (event.button > 0) return false
+  // if the event target is no longer in the document
+  if (target) {
+    const doc = getOwnerDocument(target)
+    if (!doc.contains(target)) return false
+  }
+
+  return !ref.current?.contains(target)
+}
+
+function getOwnerDocument(node?: Element | null): Document {
+  return node?.ownerDocument ?? document
+}
