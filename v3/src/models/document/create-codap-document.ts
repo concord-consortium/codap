4f7575913fa892cfd15497f906788ce747abaa3e
Kirk Swenson <kswenson@concord.org>
2023-01-11 18:26:55 -0800

    Implement free layout

--- v3/src/models/document/create-codap-document.ts
+++ v3/src/models/document/create-codap-document.ts
@@ -3,12 +3,13 @@ import { DataSummaryModel } from "../../components/data-summary/data-summary-mod
 import { GraphModel } from "../../components/graph/models/graph-model"
 import { HelloCodapModel } from "../../components/hello/hello-model"
 import { SliderModel } from "../../components/slider/slider-model"
+import { urlParams } from "../../utilities/url-params"
 import { TileModel } from "../tiles/tile-model"
 import { createDocumentModel } from "./create-document-model"
 import { IDocumentModel, IDocumentModelSnapshot } from "./document"
 import { DocumentContentModel } from "./document-content"
-// import { FreeTileRow } from "./free-tile-row"
-import { MosaicTileRow } from "./mosaic-tile-row"
+import { FreeTileRow, IFreeTileInRowOptions } from "./free-tile-row"
+import { IMosaicTileInRowOptions, isMosaicTileRow, MosaicTileRow } from "./mosaic-tile-row"
 
 export function createCodapDocument(snapshot?: IDocumentModelSnapshot): IDocumentModel {
   const document = createDocumentModel({ type: "CODAP", ...snapshot })
@@ -16,11 +17,14 @@ export function createCodapDocument(snapshot?: IDocumentModelSnapshot): IDocumen
     document.setContent(DocumentContentModel.create() as any)
   }
   if (document.content?.rowCount === 0) {
+    const isFreeLayout = urlParams.layout === "free"
     // CODAP v2/v3 documents have a single "row" containing all tiles/components
     // const rowCreator = () => FreeTileRow.create()
     // But for now we use a mosaic to preserve the current dashboard behavior until
     // we have the ability to create/move components.
-    const rowCreator = () => MosaicTileRow.create()
+    const rowCreator = isFreeLayout
+                        ? () => FreeTileRow.create()
+                        : () => MosaicTileRow.create()
     const row = rowCreator()
     document.content.setRowCreator(rowCreator)
     document.content.insertRow(row)
@@ -40,6 +44,8 @@ export function setCurrentDocument(snapshot?: IDocumentModelSnapshot) {
   gCurrentDocument = createCodapDocument(snapshot)
 }
 
+type ILayoutOptions = IFreeTileInRowOptions | IMosaicTileInRowOptions | undefined
+
 // TODO: Eliminate (or hide behind a URL parameter) default dashboard content
 export function addDefaultComponents() {
   const content = gCurrentDocument.content
@@ -48,16 +54,40 @@ export function addDefaultComponents() {
   const row = content.getRowByIndex(0)
   if (!row) return
 
+  const kFullWidth = 580
+  const kFullHeight = 300
+  const kHalfHeight = kFullHeight / 2
+  const kGap = 10
+
   setTimeout(() => {
     const summaryTile = TileModel.create({ content: DataSummaryModel.create() })
-    content.insertTileInRow(summaryTile, row)
+    const summaryOptions: ILayoutOptions = isMosaicTileRow(row)
+            ? undefined
+            : { x: 2, y: 2, width: kFullWidth, height: kFullHeight }
+    content.insertTileInRow(summaryTile, row, summaryOptions)
+
     const tableTile = TileModel.create({ content: CaseTableModel.create() })
-    content.insertTileInRow(tableTile, row, { splitTileId: summaryTile.id, direction: "column" })
+    const tableOptions: ILayoutOptions = isMosaicTileRow(row)
+            ? { splitTileId: summaryTile.id, direction: "column" }
+            : { x: 2, y: kFullHeight + kGap, width: kFullWidth, height: kFullHeight }
+    content.insertTileInRow(tableTile, row, tableOptions)
+
     const helloTile = TileModel.create({ content: HelloCodapModel.create() })
-    content.insertTileInRow(helloTile, row, { splitTileId: summaryTile.id, direction: "row" })
+    const helloOptions = isMosaicTileRow(row)
+            ? { splitTileId: summaryTile.id, direction: "row" }
+            : { x: kFullWidth + kGap, y: 2, width: kFullWidth, height: kHalfHeight }
+    content.insertTileInRow(helloTile, row, helloOptions)
+
     const sliderTile = TileModel.create({ content: SliderModel.create() })
-    content.insertTileInRow(sliderTile, row, { splitTileId: helloTile.id, direction: "column" })
+    const sliderOptions = isMosaicTileRow(row)
+            ? { splitTileId: helloTile.id, direction: "column" }
+            : { x: kFullWidth + kGap, y: kHalfHeight + kGap / 2, width: kFullWidth, height: kHalfHeight }
+    content.insertTileInRow(sliderTile, row, sliderOptions)
+
     const graphTile = TileModel.create({ content: GraphModel.create() })
-    content.insertTileInRow(graphTile, row, { splitTileId: tableTile.id, direction: "row" })
+    const graphOptions = isMosaicTileRow(row)
+            ? { splitTileId: tableTile.id, direction: "row" }
+            : { x: kFullWidth + kGap, y: kFullHeight + kGap, width: kFullWidth, height: kFullHeight }
+    content.insertTileInRow(graphTile, row, graphOptions)
   })
 }
