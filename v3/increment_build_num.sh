0ead2aa9d085aff1b93fa004c8f867993705bc57
eireland <eireland@concord.org>
2023-01-12 12:37:12 -0800

    update log messages

--- v3/increment_build_num.sh
+++ v3/increment_build_num.sh
@@ -1,5 +1,5 @@
-echo $(grep -Eo '[0-9]+' ./build_number.json)
 CURRENT_BUILD_NUM=$(grep -Eo '[0-9]+' ./build_number.json)
+echo current build num $CURRENT_BUILD_NUM
 NEW_BUILD_NUM=$(( $CURRENT_BUILD_NUM + 1))
 echo new build num $NEW_BUILD_NUM
 sed -i '' -e "s/$CURRENT_BUILD_NUM/$NEW_BUILD_NUM/" build_number.json
