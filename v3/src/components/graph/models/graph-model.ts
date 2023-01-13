65bd5e89925e1796b213cbe78f79411f65e14517
Kirk Swenson <kswenson@concord.org>
2023-01-12 16:24:08 -0800

    Code review: add comment

--- v3/src/components/graph/models/graph-model.ts
+++ v3/src/components/graph/models/graph-model.ts
@@ -33,6 +33,7 @@ export const GraphModel = TileContentModel
     type: types.optional(types.literal(kGraphTileType), kGraphTileType),
     // keys are AxisPlaces
     axes: types.map(types.maybe(AxisModelUnion)),
+    // TODO: should the default plot be something like "nullPlot" (which doesn't exist yet)?
     plotType: types.optional(types.enumeration([...PlotTypes]), "casePlot"),
     config: types.optional(DataConfigurationModel, () => DataConfigurationModel.create()),
     // Visual properties
