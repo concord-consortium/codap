505e8fe4f797c6b8a459cc45781f5f77dbfcdfd8
Evangeline Ireland <eireland@concord.org>
2023-01-13 16:48:47 -0800

    Test inc branch (#632)
    * Adds path to the build num file
    Removes initial console log change
    
    * Changed inc build num script to executable
    changed folder path to ./

--- v3/increment_build_num.sh
+++ v3/increment_build_num.sh
@@ -1,7 +1,7 @@
-CURRENT_BUILD_NUM=$(grep -Eo '[0-9]+' ./v3/build_number.json)
+CURRENT_BUILD_NUM=$(grep -Eo '[0-9]+' ./build_number.json)
 echo current build num $CURRENT_BUILD_NUM
 NEW_BUILD_NUM=$(( $CURRENT_BUILD_NUM + 1))
 echo new build num $NEW_BUILD_NUM
-sed -i '' -e "s/$CURRENT_BUILD_NUM/$NEW_BUILD_NUM/" ./v3/build_number.json
-echo $(cat ./v3/build_number.json)
+sed -i '' -e "s/$CURRENT_BUILD_NUM/$NEW_BUILD_NUM/" ./build_number.json
+echo $(cat ./build_number.json)
 exit 0
