c07a07e7ea99af1ca91545f592884d7252eb0195
eireland <eireland@concord.org>
2023-01-24 11:01:54 -0800

    Adds component UI elements to component tiles

---
+++ v3/src/components/component-header.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react"
+import { CloseButton, Flex } from "@chakra-ui/react"
+import { EditableComponentTitle } from "./editable-component-title"
+import MinimizeIcon from "../assets/icons/icon-minimize.svg"
+import TableIcon from "../assets/icons/icon-table.svg"
+import CardIcon from "../assets/icons/icon-case-card.svg"
+
+import "./component-header.scss"
+
+interface IProps {
+  tileType: string
+  datasetName?: string
+}
+
+export const ComponentHeader = ({tileType, datasetName}: IProps) => {
+  const [componentTitle, setComponentTitle] = useState(datasetName || "New Dataset")
+  const [showCaseCard, setShowCaseCard] = useState(false)
+
+  const handleTitleChange = (title?: string) => {
+    title && setComponentTitle(title)
+  }
+
+  return (
+    <Flex className="component-header">
+      {tileType === "codap-case-table" &&
+        <Flex className="header-left">
+          {showCaseCard
+            ? <TableIcon className="card-table-icon"/>
+            : <CardIcon className="card-table-icon"/>
+          }
+        </Flex>
+      }
+      <EditableComponentTitle componentTitle={componentTitle} onEndEdit={handleTitleChange} />
+      <Flex className="header-right">
+        <MinimizeIcon className="component-minimize-icon"/>
+        <CloseButton className="component-close-button"/>
+      </Flex>
+    </Flex>
+
+  )
+}
