4a6321f3ee685f69b590880ffa3f2ff2580d8498
eireland <eireland@concord.org>
2023-01-24 11:01:54 -0800

    Adds component UI elements to component tiles

--- v3/src/components/codap-component.tsx
+++ v3/src/components/codap-component.tsx
@@ -1,10 +1,11 @@
 import { observer } from "mobx-react-lite"
-import React, { useState } from "react"
+import React from "react"
 import { DataSetContext } from "../hooks/use-data-set-context"
 import { gDataBroker } from "../models/data/data-broker"
-import { EditableComponentTitle } from "./editable-component-title"
 import { ITileBaseProps } from "./tiles/tile-base-props"
 import { ITileModel } from "../models/tiles/tile-model"
+import { ComponentHeader } from "./component-header"
+import ResizeHandle from "../assets/icons/icon-corner-resize-handle.svg"
 
 import "./codap-component.scss"
 
@@ -15,17 +16,13 @@ export interface IProps extends ITileBaseProps {
 }
 export const CodapComponent = observer(({ tile, Component, tileEltClass }: IProps) => {
   const dataset = gDataBroker?.selectedDataSet || gDataBroker?.last
-  const [componentTitle, setComponentTitle] = useState("")
-
-  const handleTitleChange = (title?: string) => {
-    title && setComponentTitle(title)
-  }
 
   return (
     <DataSetContext.Provider value={dataset}>
       <div className={`codap-component ${tileEltClass}`}>
-        <EditableComponentTitle componentTitle={componentTitle} onEndEdit={handleTitleChange} />
+        <ComponentHeader tileType={tileEltClass} datasetName={dataset?.name}/>
         <Component tile={tile} />
+        <ResizeHandle className="component-resize-handle"/>
       </div>
     </DataSetContext.Provider>
   )
