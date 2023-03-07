1819a578dd0770e3f8209cab3d249625e0fa8e4b
eireland <eireland@concord.org>
2023-03-07 10:00:41 -0800

    Remove irrelevant comment

--- v3/src/components/dnd-detect-collision.ts
+++ v3/src/components/dnd-detect-collision.ts
@@ -24,7 +24,6 @@ export const dndDetectCollision: CollisionDetection = (args) => {
   // if this is a tile drag, then ignore all collisions other than the container
   if (args.active.data.current?.type === "tile") {
     const containers = args.droppableContainers.filter(({id}) => id === "codap-container-drop")
-    // apply the collection detection function specified by the tile
     return rectIntersection({ ...args, droppableContainers: containers })
   }
 
