34dda43b0f5249cfda44e6b32a830f2233c1111a
William Finzer <wfinzer@concord.org>
2023-03-31 12:31:35 -0700

    [#184805909] Bug fix: Incorrect responses to changing attributes on graph * Using an observable changeCount property in MultiScale, we detect changes to the domain of an axis and refresh subaxis * Change the default numeric axis formatting to 9 significant digits

--- v3/src/components/axis/hooks/use-sub-axis.ts
+++ v3/src/components/axis/hooks/use-sub-axis.ts
@@ -22,7 +22,8 @@ export const useSubAxis = ({
                              enableAnimation
                            }: IUseSubAxis) => {
   const layout = useAxisLayoutContext(),
-    isNumeric = axisModel && isNumericAxisModel(axisModel)
+    isNumeric = axisModel && isNumericAxisModel(axisModel),
+    multiScaleChangeCount = layout.getAxisMultiScale(axisModel?.place ?? 'bottom')?.changeCount ?? 0
 
   const refreshSubAxis = useCallback(() => {
     const
@@ -145,7 +146,7 @@ export const useSubAxis = ({
   useEffect(() => {
     const disposer = reaction(
       () => layout.getComputedBounds(axisModel?.place ?? 'bottom'),
-      () => refreshSubAxis(/*myBounds*/)
+      () => refreshSubAxis()
     )
     return () => disposer()
   }, [layout, refreshSubAxis, axisModel?.place])
@@ -162,7 +163,6 @@ export const useSubAxis = ({
       })
       return () => disposer()
     }
-    // Note axisModelChanged as a dependent. Shouldn't be necessary.
   }, [isNumeric, axisModel, refreshSubAxis, layout])
 
   // update d3 scale and axis when layout/range changes
@@ -181,6 +181,6 @@ export const useSubAxis = ({
   // update on component refresh
   useEffect(() => {
     refreshSubAxis()
-  }, [refreshSubAxis])
+  }, [refreshSubAxis, multiScaleChangeCount])
 
 }
