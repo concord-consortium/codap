6ca514bcfc084ba22f2efae54864eb191c555618
Henry Morgan <hmorgan471@gmail.com>
2023-04-03 15:49:52 -0400

    i lied

--- v3/webpack.config.js
+++ v3/webpack.config.js
@@ -202,7 +202,7 @@ module.exports = (env, argv) => {
         publicPath: DEPLOY_PATH
       })] : []),
       new CleanWebpackPlugin(),
-      process.env.CODE_COVERAGE ? {} : new ForkTsCheckerWebpackPlugin(),
+      process.env.CODE_COVERAGE ? null : new ForkTsCheckerWebpackPlugin(),
     ]
   }
 }
