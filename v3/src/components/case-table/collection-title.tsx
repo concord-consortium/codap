a1b677be5afa574b0e0cedbf585b674d3b461983
Kirk Swenson <kswenson@concord.org>
2023-04-04 16:53:58 -0700

    refactor: code review changes

--- v3/src/components/case-table/collection-title.tsx
+++ v3/src/components/case-table/collection-title.tsx
@@ -1,7 +1,8 @@
-import React, { useEffect, useRef, useState, useCallback } from "react"
+import React, { useEffect, useRef, useState } from "react"
 import { useDndContext } from "@dnd-kit/core"
 import { Editable, EditableInput, EditablePreview } from "@chakra-ui/react"
 import throttle from "lodash/throttle"
+import {useResizeDetector} from "react-resize-detector"
 
 interface ICollectionTitleProps {
   setTitle: (title: string) => void,
@@ -13,68 +14,51 @@ interface ICollectionTitleProps {
 // Does not handle component resize at the moment
 export const CollectionTitle = function CollectionTitle({ setTitle, displayTitle, defaultName, caseCount }
   : ICollectionTitleProps) {
+  const tileRef = useRef<HTMLDivElement | null>(null)
+  const contentRef = useRef<HTMLDivElement | null>(null)
   const titleRef = useRef<HTMLDivElement>(null)
-  const refExists = titleRef.current != null
   const title = displayTitle || defaultName
-  const [tableScrollLeft, setTableScrollLeft] = useState(0)
-  const [titleBarStyle, setTitleBarStyle] = useState({ left: 0, right: 0 })
+  // used to trigger a render
+  const [ , setTableScrollLeft] = useState(0)
   const { active } = useDndContext()
   const dragging = !!active
   const [isEditing, setIsEditing] = useState(false)
-  const [caseTableRenderCount, setCaseTableRenderCount] = useState(0)
+
+  // re-render the component when either the tile or the title change size
+  useResizeDetector({ targetRef: tileRef })
+  useResizeDetector({ targetRef: titleRef })
 
   useEffect(() => {
-    if (!refExists) {
-      return
-    }
-    const ref = titleRef.current
+    // these parents exist for the lifetime of the component
+    tileRef.current = titleRef.current?.closest(".codap-component") ?? null
+    contentRef.current = titleRef.current?.closest(".case-table-content") ?? null
+
     const updateScrollPosition = throttle((e: Event) => {
       if (e.currentTarget != null) {
         setTableScrollLeft((e.currentTarget as HTMLDivElement).scrollLeft)
       }
-    }, 5)
-    ref.closest(".case-table-content")?.addEventListener("scroll", updateScrollPosition)
-    return () => {
-      ref.closest(".case-table-content")?.removeEventListener("scroll", updateScrollPosition)
-    }
-  }, [refExists])
+    }, 15)
 
-  const updateTitleScrollStyles = useCallback(() => {
-    if (!refExists) {
-      return
+    const contentElt = contentRef.current
+    contentElt?.addEventListener("scroll", updateScrollPosition)
+    return () => {
+      contentElt?.removeEventListener("scroll", updateScrollPosition)
     }
-    const tileRect = titleRef.current.closest(".codap-component")?.getBoundingClientRect()
-    const tileWidth = tileRect?.width ?? 580
-    const tileLeft = tileRect?.left ?? 0
-    const titleRect = titleRef.current.getBoundingClientRect()
-    const deltaLeft = titleRect.left - tileLeft
-    const deltaRight = titleRect.right - (tileLeft + tileWidth)
-    const style = { left: 0, right: 0 }
+  }, [])
+
+  const tileRect = tileRef.current?.getBoundingClientRect()
+  const titleRect = titleRef.current?.getBoundingClientRect()
+  const titleStyle: React.CSSProperties = { left: 0, right: 0 }
+  if (tileRect && titleRect) {
+    const deltaLeft = titleRect.left - tileRect.left
+    const deltaRight = titleRect.right - tileRect.right
     if (deltaLeft < 0) {
-      style.left = -deltaLeft + 6
+      titleStyle.left = -deltaLeft + 6
     }
     if (deltaRight > 0) {
-      style.right = deltaRight
+      titleStyle.right = deltaRight
     }
-    setTitleBarStyle(style)
-  }, [refExists, titleRef])
-
-  useEffect(() => {
-    updateTitleScrollStyles()
-  }, [tableScrollLeft, updateTitleScrollStyles])
-
-  // React-data-grid takes several renders to arrive at its full width,
-  // even though the mobx data relevant to the title are available before that.
-  // So, we hide the title until the data table is fully rendered, and then
-  // calculate the correct size. This is preferable to a setTimeout,
-  // but if there's a best-practice mobX solution, I'm open to it.
-  useEffect(() => {
-    if (caseTableRenderCount < 5) {
-      setCaseTableRenderCount(caseTableRenderCount + 1)
-      return
-    }
-    updateTitleScrollStyles()
-  }, [caseTableRenderCount, updateTitleScrollStyles])
+  }
 
   const handleChangeTitle = (nextValue?: string) => {
     if (nextValue) {
@@ -84,11 +68,11 @@ export const CollectionTitle = function CollectionTitle({ setTitle, displayTitle
 
   return (
     <div className="collection-title-wrapper" ref={titleRef}>
-      <div className="collection-title" style={{ left: titleBarStyle.left, right: titleBarStyle.right }}>
+      <div className="collection-title" style={titleStyle}>
         <Editable value={isEditing ? title : `${title} (${caseCount} cases)`}
             onEdit={() => setIsEditing(true)} onSubmit={() => setIsEditing(false)} onCancel={() => setIsEditing(false)}
             isPreviewFocusable={!dragging} submitOnBlur={true} onChange={handleChangeTitle}>
-          <EditablePreview paddingY={0} display={caseTableRenderCount >= 5 ? "block" : "none"} />
+          <EditablePreview paddingY={0} />
           <EditableInput value={title} paddingY={0} className="collection-title-input" />
         </Editable>
       </div>
