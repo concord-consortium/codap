1311e1de1a3236421d90bb2342130bedd64d780c
Henry Morgan <hmorgan471@gmail.com>
2023-04-06 13:40:11 -0400

    case table CollectionTitle improvements (#739)
    * move CollectionTitle to its own react-only component and make changes
    
    * refactor: code review changes
    
    * address review feedback and visual drag/drop bug from before
    
    * use a translated string for caseCount cases
    
    ---------
    
    Co-authored-by: Kirk Swenson <kswenson@concord.org>

--- v3/src/components/case-table/collection-table.tsx
+++ v3/src/components/case-table/collection-table.tsx
@@ -1,6 +1,5 @@
 import { observer } from "mobx-react-lite"
-import pluralize from "pluralize"
-import React, { useEffect, useRef, useState } from "react"
+import React, { useRef } from "react"
 import DataGrid, { DataGridHandle } from "react-data-grid"
 import { kChildMostTableCollectionId, TRow } from "./case-table-types"
 import { CollectionTableSpacer } from "./collection-table-spacer"
@@ -10,7 +9,7 @@ import { useRows } from "./use-rows"
 import { useSelectedRows } from "./use-selected-rows"
 import { useCollectionContext } from "../../hooks/use-collection-context"
 import { useDataSetContext } from "../../hooks/use-data-set-context"
-import { getCollectionAttrs } from "../../models/data/data-set-utils"
+import { CollectionTitle } from './collection-title'
 
 import styles from "./case-table-shared.scss"
 import "react-data-grid/lib/styles.css"
@@ -53,56 +52,3 @@ export const CollectionTable = observer(function CollectionTable() {
     </div>
   )
 })
-
-// Does not handle component resize at the moment
-function CollectionTitle() {
-  const data = useDataSetContext()
-  const collection = useCollectionContext()
-  const titleRef = useRef<HTMLDivElement>(null)
-  const refExists = titleRef.current != null
-  const defaultName = pluralize((collection.displayTitle || getCollectionAttrs(collection, data)[0]?.name) ?? '')
-  const caseCount = data?.getCasesForCollection(collection?.id).length ?? 0
-  const [tableScrollLeft, setTableScrollLeft] = useState(0)
-  const [titleBarStyle, setTitleBarStyle] = useState({ left: 0, right: 0 })
-
-  useEffect(() => {
-    if (!refExists) {
-      return
-    }
-    const ref = titleRef.current
-    // This could possibly use a throttle
-    const updateScrollPosition = (e: Event) => setTableScrollLeft((e.currentTarget as HTMLDivElement).scrollLeft)
-    ref.closest(".case-table-content")?.addEventListener("scroll", updateScrollPosition)
-    return () => {
-      ref.closest(".case-table-content")?.removeEventListener("scroll", updateScrollPosition)
-    }
-  }, [refExists])
-
-  useEffect(() => {
-    if (!refExists) {
-      return
-    }
-    const tileRect = titleRef.current?.closest(".codap-component")?.getBoundingClientRect()
-    const tileWidth = tileRect?.width ?? 580
-    const tileLeft = tileRect?.left ?? 0
-    const titleRect = titleRef.current.getBoundingClientRect()
-    const deltaLeft = titleRect.left - tileLeft
-    const deltaRight = titleRect.right - (tileLeft + tileWidth)
-    const style = { left: 0, right: 0 }
-    if (deltaLeft < 0) {
-      style.left = -deltaLeft + 6
-    }
-    if (deltaRight > 0) {
-      style.right = deltaRight
-    }
-    setTitleBarStyle(style)
-  }, [tableScrollLeft, refExists])
-
-  return (
-    <div className="collection-title-wrapper" ref={titleRef}>
-      <div className="collection-title" style={{ left: titleBarStyle.left, right: titleBarStyle.right }}>
-        {`${defaultName} (${caseCount} cases)`}
-      </div>
-    </div>
-  )
-}
