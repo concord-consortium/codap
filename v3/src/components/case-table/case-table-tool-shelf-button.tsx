82ea8e605899fa21351beff60123fb11ec399d7a
eireland <eireland@concord.org>
2023-04-21 12:48:29 -0700

    Adds data set names to the case table menu ui

--- v3/src/components/case-table/case-table-tool-shelf-button.tsx
+++ v3/src/components/case-table/case-table-tool-shelf-button.tsx
@@ -1,15 +1,37 @@
 import React from "react"
 import { Menu, MenuButton, MenuItem, MenuList, Tag} from "@chakra-ui/react"
 import t from "../../utilities/translation/translate"
-import { useDataSetContext } from "../../hooks/use-data-set-context"
+import { getSharedModelManager } from "../../models/tiles/tile-environment"
+import { appState } from "../../models/app-state"
+import { kSharedDataSetType, SharedDataSet } from "../../models/shared/shared-data-set"
+import { observer } from "mobx-react-lite"
 import TableIcon from "../../assets/icons/icon-table.svg"
 import TrashIcon from "../../assets/icons/icon-trash.svg"
 
 import "../tool-shelf/tool-shelf.scss"
 
-export const CaseTableToolShelfButton = () => {
-  const data = useDataSetContext()
+export const CaseTableToolShelfMenuList = observer(function CaseTableToolShelfMenuList() {
+  const document = appState.document
+  const manager = getSharedModelManager(document)
+  const datasets = manager?.getSharedModelsByType<typeof SharedDataSet>(kSharedDataSetType)
 
+  return (
+    <MenuList>
+      {datasets?.map(ds => {
+        const dataSetName = ds.dataSet.name
+        return (
+          <MenuItem key={dataSetName}>{dataSetName}
+            <TrashIcon className="tool-shelf-menu-trash-icon"/>
+          </MenuItem>
+        )
+      })}
+      <MenuItem>{t("DG.AppController.caseTableMenu.clipboardDataset")}</MenuItem>
+      <MenuItem>{t("DG.AppController.caseTableMenu.newDataSet")}</MenuItem>
+    </MenuList>
+  )
+})
+
+export const CaseTableToolShelfButton = () => {
   return (
     <Menu isLazy>
       <MenuButton className="tool-shelf-button menu table" title={`${t("DG.ToolButtonData.tableButton.toolTip")}`}
@@ -17,13 +39,7 @@ export const CaseTableToolShelfButton = () => {
         <TableIcon />
         <Tag className='tool-shelf-tool-label table'>{t("DG.ToolButtonData.tableButton.title")}</Tag>
       </MenuButton>
-      <MenuList>
-        <MenuItem>{data?.name}
-          <TrashIcon className="tool-shelf-menu-trash-icon"/>
-        </MenuItem>
-        <MenuItem>{t("DG.AppController.caseTableMenu.clipboardDataset")}</MenuItem>
-        <MenuItem>{t("DG.AppController.caseTableMenu.newDataSet")}</MenuItem>
-      </MenuList>
+      <CaseTableToolShelfMenuList />
     </Menu>
   )
 }
