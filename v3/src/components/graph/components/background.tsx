6ce01a1d602df0af923e300607b6d442c4fc2954
William Finzer <wfinzer@concord.org>
2023-05-01 09:09:41 -0700

    [#185057138] Feature: Plots split by categorical attributes on top and right have shaded cells
    * This fixes problems with the initial commit, especially those having to do with updating on change of attribute and also having to do with shading when there is a background color.

--- v3/src/components/graph/components/background.tsx
+++ v3/src/components/graph/components/background.tsx
@@ -1,7 +1,8 @@
 import {onAction} from "mobx-state-tree"
-import React, {forwardRef, MutableRefObject, useEffect, useRef} from "react"
+import React, {forwardRef, MutableRefObject, useCallback, useEffect, useRef} from "react"
 import {drag, select, color, range} from "d3"
 import RTreeLib from 'rtree'
+
 type RTree = ReturnType<typeof RTreeLib>
 import {CaseData} from "../d3-types"
 import {InternalizedData, rTreeRect} from "../graphing-types"
@@ -61,7 +62,56 @@ export const Background = forwardRef<SVGGElement, IProps>((props, ref) => {
     selectionTree = useRef<RTree | null>(null),
     previousMarqueeRect = useRef<rTreeRect>()
 
-  useEffect(() => {
+  const onDragStart = useCallback((event: { x: number; y: number; sourceEvent: { shiftKey: boolean } }) => {
+      const {computedBounds} = layout,
+        plotBounds = computedBounds.plot
+      appState.beginPerformance()
+      selectionTree.current = prepareTree(`.${instanceId}`, 'circle')
+      startX.current = event.x - plotBounds.left
+      startY.current = event.y - plotBounds.top
+      width.current = 0
+      height.current = 0
+      if (!event.sourceEvent.shiftKey) {
+        dataset.current?.setSelectedCases([])
+      }
+      marqueeState.setMarqueeRect({x: startX.current, y: startY.current, width: 0, height: 0})
+    }, [dataset, instanceId, layout, marqueeState]),
+
+    onDrag = useCallback((event: { dx: number; dy: number }) => {
+      if (event.dx !== 0 || event.dy !== 0) {
+        previousMarqueeRect.current = rectNormalize(
+          {x: startX.current, y: startY.current, w: width.current, h: height.current})
+        width.current = width.current + event.dx
+        height.current = height.current + event.dy
+        const marqueeRect = marqueeState.marqueeRect
+        marqueeState.setMarqueeRect({
+          x: marqueeRect.x, y: marqueeRect.y,
+          width: marqueeRect.width + event.dx,
+          height: marqueeRect.height + event.dy
+        })
+        const currentRect = rectNormalize({
+            x: startX.current, y: startY.current,
+            w: width.current,
+            h: height.current
+          }),
+          newSelection = getCasesForDelta(selectionTree.current, currentRect, previousMarqueeRect.current),
+          newDeselection = getCasesForDelta(selectionTree.current, previousMarqueeRect.current, currentRect)
+        newSelection.length && dataset.current?.selectCases(newSelection, true)
+        newDeselection.length && dataset.current?.selectCases(newDeselection, false)
+      }
+    }, [dataset, marqueeState]),
+
+    onDragEnd = useCallback(() => {
+      marqueeState.setMarqueeRect({x: 0, y: 0, width: 0, height: 0})
+      selectionTree.current = null
+      appState.endPerformance()
+    }, [marqueeState]),
+    dragBehavior = drag<SVGRectElement, number>()
+      .on("start", onDragStart)
+      .on("drag", onDrag)
+      .on("end", onDragEnd)
+
+  const refreshBackground = useCallback(() => {
     const bgColor = String(color(graphModel.plotBackgroundColor)),
       darkBgColor = String(color(graphModel.plotBackgroundColor)?.darker(0.2)),
       numRows = layout.getAxisMultiScale('left').repetitions,
@@ -69,93 +119,46 @@ export const Background = forwardRef<SVGGElement, IProps>((props, ref) => {
       cellWidth = plotWidth / numCols,
       cellHeight = plotHeight / numRows,
       row = (index: number) => Math.floor(index / numCols),
-      col = (index: number) => index % numCols
-    const onDragStart = (event: { x: number; y: number; sourceEvent: { shiftKey: boolean } }) => {
-      const {computedBounds} = layout,
-          plotBounds = computedBounds.plot
-        appState.beginPerformance()
-        selectionTree.current = prepareTree(`.${instanceId}`, 'circle')
-        startX.current = event.x - plotBounds.left
-        startY.current = event.y - plotBounds.top
-        width.current = 0
-        height.current = 0
-        if (!event.sourceEvent.shiftKey) {
-          dataset.current?.setSelectedCases([])
-        }
-        marqueeState.setMarqueeRect({x: startX.current, y: startY.current, width: 0, height: 0})
-      },
-
-      onDrag = (event: { dx: number; dy: number }) => {
-        if (event.dx !== 0 || event.dy !== 0) {
-          previousMarqueeRect.current = rectNormalize(
-            {x: startX.current, y: startY.current, w: width.current, h: height.current})
-          width.current = width.current + event.dx
-          height.current = height.current + event.dy
-          const marqueeRect = marqueeState.marqueeRect
-          marqueeState.setMarqueeRect({
-            x: marqueeRect.x, y: marqueeRect.y,
-            width: marqueeRect.width + event.dx,
-            height: marqueeRect.height + event.dy
-          })
-          const currentRect = rectNormalize({
-              x: startX.current, y: startY.current,
-              w: width.current,
-              h: height.current
-            }),
-            newSelection = getCasesForDelta(selectionTree.current, currentRect, previousMarqueeRect.current),
-            newDeselection = getCasesForDelta(selectionTree.current, previousMarqueeRect.current, currentRect)
-          newSelection.length && dataset.current?.selectCases(newSelection, true)
-          newDeselection.length && dataset.current?.selectCases(newDeselection, false)
-        }
-      },
-
-      onDragEnd = () => {
-        marqueeState.setMarqueeRect({x: 0, y: 0, width: 0, height: 0})
-        selectionTree.current = null
-        appState.endPerformance()
-      },
-      dragBehavior = drag<SVGRectElement, number>()
-        .on("start", onDragStart)
-        .on("drag", onDrag)
-        .on("end", onDragEnd),
+      col = (index: number) => index % numCols,
       groupElement = bgRef.current
+    select(groupElement)
+      .selectAll('rect')
+      .data(range(numRows * numCols))
+      .join('rect')
+      .attr('class', 'plot-cell-background')
+      .attr('transform', transform)
+      .attr('width', cellWidth)
+      .attr('height', cellHeight)
+      .attr('x', d => cellWidth * col(d))
+      .attr('y', d => cellHeight * row(d))
+      .style('fill', d => (row(d) + col(d)) % 2 === 0 ? bgColor : darkBgColor)
+      .style('fill-opacity', graphModel.isTransparent ? 0 : 1)
+      // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+      // @ts-ignore
+      .call(dragBehavior)
+  }, [bgRef, dragBehavior, graphModel.isTransparent, graphModel.plotBackgroundColor, layout,
+    plotHeight, plotWidth, transform])
+
+  useEffect(() => {
+    const groupElement = bgRef.current
     select(groupElement).on('click', (event) => {
       if (!event.shiftKey) {
         dataset.current?.selectAll(false)
       }
     })
-    select(groupElement)
-      .selectAll('rect')
-      .data(range(numRows * numCols))
-      .join(
-        (enter) =>
-          enter.append('rect')
-            .attr('class', 'graph-background')
-            .call(dragBehavior),
-        (update) =>
-          update
-            .attr('transform', transform)
-            .attr('width', cellWidth)
-            .attr('height', cellHeight)
-            .attr('x', d => cellWidth * col(d))
-            .attr('y', d => cellHeight * row(d))
-            .style('fill', d => d % 2 === 0 ? bgColor : darkBgColor)
-            .style('fill-opacity', graphModel.isTransparent ? 0 : 1)
-      )
-  }, [bgRef, instanceId, transform, dataset, plotHeight, plotWidth, graphModel, layout, marqueeState])
+    refreshBackground()
+  }, [dataset, bgRef, refreshBackground])
 
   // respond to point properties change
   useEffect(function respondToGraphPointVisualAction() {
     const disposer = onAction(graphModel, action => {
       if (['setPlotBackgroundColor', 'setIsTransparent'].includes(action.name)) {
-        select(bgRef.current).selectAll('rect')
-          .style('fill', graphModel.plotBackgroundColor)
-          .style('fill-opacity', graphModel.isTransparent ? 0 : 1)
+        refreshBackground()
       }
     }, true)
 
     return () => disposer()
-  }, [graphModel, bgRef])
+  }, [graphModel, bgRef, refreshBackground])
 
   return (
     <g ref={bgRef}/>
