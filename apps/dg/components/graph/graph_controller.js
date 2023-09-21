ad49f87a6fb2e8ef3cce12e1f89e5cb529b22731
eireland <eireland@concord.org>
2023-09-21 16:48:58 -0700

    adds missing semi-color

--- apps/dg/components/graph/graph_controller.js
+++ apps/dg/components/graph/graph_controller.js
@@ -330,9 +330,9 @@ DG.GraphController = DG.DataDisplayController.extend(
                             this.appendChild(iDesc.control);
                           }
                           else {
-                            iDesc.layout = { height: kRowHeight }
-                            iDesc.localize = true
-                            this.appendChild(SC.CheckboxView.create(iDesc))
+                            iDesc.layout = { height: kRowHeight };
+                            iDesc.localize = true;
+                            this.appendChild(SC.CheckboxView.create(iDesc));
                           }
                         }.bind(this));
                       }
