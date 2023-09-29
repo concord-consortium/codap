9c180ab9a5dad9b9dac0075f1f66bcf1c2473d63
Ethan McElroy <emcelroy@concord.org>
2023-09-29 14:31:02 -0400

    chore: add tests for updateMeasureValue

--- v3/src/components/graph/adornments/univariate-measures/mean/mean-adornment-model.test.ts
+++ v3/src/components/graph/adornments/univariate-measures/mean/mean-adornment-model.test.ts
@@ -19,6 +19,16 @@ describe("MeanModel", () => {
     adornment.addMeasure(10)
     expect(adornment.measures.size).toBe(1)
   })
+  it("can have an existing mean value in its measures map updated", () => {
+    const adornment = MeanAdornmentModel.create()
+    expect(adornment.measures.size).toBe(0)
+    adornment.addMeasure(10)
+    expect(adornment.measures.size).toBe(1)
+    expect(adornment.measures.get("{}")?.value).toBe(10)
+    adornment.updateMeasureValue(20, "{}")
+    expect(adornment.measures.get("{}")?.value).toBe(20)
+    expect(adornment.measures.size).toBe(1)
+  })
   it("can have an existing mean value removed from its measures map", () => {
     const adornment = MeanAdornmentModel.create()
     expect(adornment.measures.size).toBe(0)
