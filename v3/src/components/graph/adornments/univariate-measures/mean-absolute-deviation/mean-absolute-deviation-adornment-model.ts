b0acc1600ff77dada75e94321371d2a317cb3f5d
William Finzer <wfinzer@concord.org>
2023-10-13 16:39:03 -0700

    * Fixed conflicts with main

--- v3/src/components/graph/adornments/univariate-measures/mean-absolute-deviation/mean-absolute-deviation-adornment-model.ts
+++ v3/src/components/graph/adornments/univariate-measures/mean-absolute-deviation/mean-absolute-deviation-adornment-model.ts
@@ -1,11 +1,11 @@
 import { Instance, types } from "mobx-state-tree"
 import { mean } from "mathjs"
+import {IGraphDataConfigurationModel} from "../../../models/graph-data-configuration-model"
 import {
   UnivariateMeasureAdornmentModel, IUnivariateMeasureAdornmentModel
 } from "../univariate-measure-adornment-model"
 import { kMeanAbsoluteDeviationValueTitleKey,
          kMeanAbsoluteDeviationType } from "./mean-absolute-deviation-adornment-types"
-import { IDataConfigurationModel } from "../../../../data-display/models/data-configuration-model"
 
 export const MeanAbsoluteDeviationAdornmentModel = UnivariateMeasureAdornmentModel
   .named("MeanAbsoluteDeviationAdornmentModel")
@@ -17,12 +17,12 @@ export const MeanAbsoluteDeviationAdornmentModel = UnivariateMeasureAdornmentMod
     get hasRange() {
       return true
     },
-    computeMeasureRange(attrId: string, cellKey: Record<string, string>, dataConfig: IDataConfigurationModel) {
+    computeMeasureRange(attrId: string, cellKey: Record<string, string>, dataConfig: IGraphDataConfigurationModel) {
       const caseValues = self.getCaseValues(attrId, cellKey, dataConfig)
       const mad = caseValues.reduce((acc, val) => acc + Math.abs(val - mean(caseValues)), 0) / caseValues.length
       return mad
     },
-    computeMeasureValue(attrId: string, cellKey: Record<string, string>, dataConfig: IDataConfigurationModel) {
+    computeMeasureValue(attrId: string, cellKey: Record<string, string>, dataConfig: IGraphDataConfigurationModel) {
       const caseValues = self.getCaseValues(attrId, cellKey, dataConfig)
       if (caseValues.length === 0) return NaN
       return mean(caseValues)
