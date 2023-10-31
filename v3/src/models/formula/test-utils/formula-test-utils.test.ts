ef01317f8e350dd68389e514ba231338eb20287c
pjanik <janikpiotrek@gmail.com>
2023-10-31 17:48:20 +0100

    feat: add formula test utils (and their own tests) [PT-186374843]

---
+++ v3/src/models/formula/test-utils/formula-test-utils.test.ts
@@ -0,0 +1,42 @@
+import { evaluate, getFormulaTestEnv } from "./formula-test-utils"
+
+describe("getFormulaTestEnv", () => {
+  it("returns default environment with datasets and global values", () => {
+    const formulaTestEnv = getFormulaTestEnv()
+    const Mammals = formulaTestEnv.dataSetsByName.Mammals
+    const Cats = formulaTestEnv.dataSetsByName.Cats
+    expect(Mammals.name).toEqual("Mammals")
+    expect(Cats.name).toEqual("Cats")
+    expect(formulaTestEnv.dataSets?.get(Mammals.id)).toEqual(Mammals)
+    expect(formulaTestEnv.dataSets?.get(Cats.id)).toEqual(Cats)
+    expect(formulaTestEnv.globalValueManager?.getValueByName("v1")?.value).toEqual(0.5)
+    expect(formulaTestEnv.globalValueManager?.getValueByName("v2")?.value).toEqual(2)
+  })
+})
+
+describe("evaluate", () => {
+  it("evaluates basic formulas correctly", () => {
+    expect(evaluate("1 + 2")).toEqual(3)
+  })
+
+  it("can evaluate case-dependant formulas when case pointer is provided", () => {
+    expect(evaluate("LifeSpan", 0)).toEqual("70")
+    expect(evaluate("LifeSpan", 10)).toEqual("30")
+    expect(evaluate("LifeSpan", 26)).toEqual("25")
+  })
+
+  it("evaluates formulas with global values", () => {
+    expect(evaluate("v1")).toEqual(0.5)
+    expect(evaluate("v2")).toEqual(2)
+  })
+
+  it("sets Mammals dataset as a local dataset", () => {
+    expect(evaluate("LifeSpan", 0)).toEqual("70")
+    // TailLength is an attribute from Cats dataset.
+    expect(() => evaluate("TailLength", 0)).toThrow("Undefined symbol TailLength")
+  })
+
+  it("throws an error when user is trying to evaluate case-dependant formula without providing casePointer", () => {
+    expect(() => evaluate("LifeSpan")).toThrow()
+  })
+})
---
+++ v3/src/models/formula/test-utils/formula-test-utils.ts
@@ -0,0 +1,70 @@
+import testDoc from "./test-doc.json"
+import { getSharedDataSets } from "../../shared/shared-data-utils"
+import { createCodapDocument } from "../../codap/create-codap-document"
+import { getSharedModelManager, getGlobalValueManager } from "../../tiles/tile-environment"
+import { math } from "../functions/math"
+import { FormulaMathJsScope } from "../formula-mathjs-scope"
+import { displayToCanonical, getDisplayNameMap } from "../formula-utils"
+
+// Because formulas largely depend on the document context and data structures, it's not trivial to mock them in each
+// unit test. Therefore, these helpers are provided to make it easier to write tests for formulas.
+
+// They are based on test-doc.json, which has the following structure:
+// - Mammals (data set)
+// - Cats (data set)
+// - v1 slider / global value with a value of 0.5
+// - v2 slider / global value with a value of 2
+
+// While it may be opinionated and potentially more than most unit tests require, I believe that working
+// with real data is preferable to mocking it every time. Coming up with values that make sense for each particular
+// test file is a task in itself, and these helpers can significantly reduce that effort. Even the test expressions are
+// easier to validate when they are based on real data. Additionally, test-file.json can be opened in a browser, and
+// results can be easily compared or inspected. It's also possible to replicate the same document in V2 by dragging the
+// 'mammals.csv' and 'cats.csv' CSV files from the 'sample-data' directory into CODAP V2 workspace and adding two
+// sliders.
+
+export const getFormulaTestEnv = () => {
+  const doc = createCodapDocument(testDoc)
+  const dataSets = getSharedDataSets(doc).map(sharedDs => sharedDs.dataSet)
+
+  const mammals = dataSets.find(dataSet => dataSet.name === "Mammals")
+  if (!mammals) {
+    throw new Error("Mammals data set not found in testdoc.json")
+  }
+  const cats = dataSets.find(dataSet => dataSet.name === "Cats")
+  if (!cats) {
+    throw new Error("Cats data set not found in testdoc.json")
+  }
+  return {
+    dataSetsByName: {
+      Mammals: mammals,
+      Cats: cats,
+    },
+    globalValueManager: getGlobalValueManager(getSharedModelManager(doc)),
+    dataSets: new Map(dataSets.map(dataSet => [dataSet.id, dataSet])),
+  }
+}
+
+// When casePointer is provided, evaluation will support cases-dependant formulas.
+export const evaluate = (displayFormula: string, casePointer?: number) => {
+  const { dataSetsByName, dataSets, globalValueManager } = getFormulaTestEnv()
+  const localDataSet = dataSetsByName.Mammals
+  const caseIds = localDataSet.cases.map(c => c.__id__)
+  const scope = new FormulaMathJsScope({
+    localDataSet,
+    dataSets,
+    globalValueManager,
+    caseIds: casePointer !== undefined ? caseIds : undefined,
+    childMostCollectionCaseIds: caseIds
+  })
+  if (casePointer !== undefined) {
+    scope.setCasePointer(casePointer)
+  }
+  const displayNameMap = getDisplayNameMap({
+    localDataSet,
+    dataSets,
+    globalValueManager,
+  })
+  const formula = displayToCanonical(displayFormula, displayNameMap)
+  return math.evaluate(formula, scope)
+}
