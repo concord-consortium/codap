386b45aa52e7ff58f9bd17c43dc0156cb1471106
Ethan McElroy <emcelroy@concord.org>
2023-11-07 12:10:01 -0500

    chore: Use hasOwnProperty to find duplicates

--- v3/src/components/graph/adornments/adornment-utils.ts
+++ v3/src/components/graph/adornments/adornment-utils.ts
@@ -1,7 +1,7 @@
 export const updateCellKey = (cellKey: Record<string, string>, attrId: string, cat: string) => {
   const newCellKey = { ...cellKey }
   if (cat) {
-    const propertyAlreadyPresent = Object.keys(newCellKey).includes(attrId)
+    const propertyAlreadyPresent = Object.prototype.hasOwnProperty.call(newCellKey, attrId)
     if (propertyAlreadyPresent && newCellKey[attrId] !== cat) {
       // When the same attribute appears on multiple axes or splits, we avoid overwriting the existing key's
       // value by using the new key "__IMPOSSIBLE__" since it's impossible for a single case to have two
