87a6058b625a3aee7bd421531439d16e84b46145
pjanik <janikpiotrek@gmail.com>
2023-11-14 21:42:20 +0100

    fix: fix `equal` helper, add tests of the basic function helpers [PT-186479532]

---
+++ v3/src/models/formula/functions/function-utils.test.ts
@@ -0,0 +1,86 @@
+import { isValueNonEmpty, isNumber, isValueTruthy, equal, evaluateNode } from "./function-utils"
+import { parse } from "mathjs"
+
+describe("isValueNonEmpty", () => {
+  it("should return false for empty values", () => {
+    expect(isValueNonEmpty("")).toBe(false)
+    expect(isValueNonEmpty(null)).toBe(false)
+    expect(isValueNonEmpty(undefined)).toBe(false)
+  })
+
+  it("should return true for non-empty values", () => {
+    expect(isValueNonEmpty("non-empty")).toBe(true)
+    expect(isValueNonEmpty(0)).toBe(true)
+    expect(isValueNonEmpty(false)).toBe(true)
+  })
+})
+
+describe("isNumber", () => {
+  it("should return true for numbers", () => {
+    expect(isNumber(0)).toBe(true)
+    expect(isNumber("0")).toBe(true)
+    expect(isNumber(1.23)).toBe(true)
+    expect(isNumber("1.23")).toBe(true)
+  })
+
+  it("should return false for non-numbers", () => {
+    expect(isNumber("")).toBe(false)
+    expect(isNumber("abc")).toBe(false)
+    expect(isNumber(null)).toBe(false)
+    expect(isNumber(undefined)).toBe(false)
+  })
+})
+
+describe("isValueTruthy", () => {
+  it("should return true for truthy values", () => {
+    expect(isValueTruthy(1)).toBe(true)
+    expect(isValueTruthy("non-empty")).toBe(true)
+  })
+
+  it("should return false for falsy values", () => {
+    expect(isValueTruthy("")).toBe(false)
+    expect(isValueTruthy(null)).toBe(false)
+    expect(isValueTruthy(undefined)).toBe(false)
+    expect(isValueTruthy(false)).toBe(false)
+  })
+})
+
+describe("equal", () => {
+  it("should return true for equal values", () => {
+    expect(equal(1, 1)).toBe(true)
+    expect(equal("1", 1)).toBe(true)
+    expect(equal(1, "1")).toBe(true)
+    expect(equal("1", "1")).toBe(true)
+    expect(equal(true, "true")).toBe(true)
+    expect(equal("true", true)).toBe(true)
+  })
+
+  it("should return false for unequal values", () => {
+    expect(equal(1, 2)).toBe(false)
+    expect(equal("1", 2)).toBe(false)
+    expect(equal(1, "2")).toBe(false)
+    expect(equal("1", "2")).toBe(false)
+    expect(equal(true, "false")).toBe(false)
+    expect(equal("true", false)).toBe(false)
+    expect(equal(true, 1)).toBe(false)
+  })
+
+  it("should support array arguments", () => {
+    expect(equal([1, 2], [1, 2])).toEqual([true, true])
+    expect(equal([1, 2], [1, 3])).toEqual([true, false])
+    expect(equal([1, 2], [2, 1])).toEqual([false, false])
+
+    expect(equal([1, 2], 1)).toEqual([true, false])
+    expect(equal(1, [1, 2])).toEqual([true, false])
+
+    expect(equal([1, 2], "1")).toEqual([true, false])
+    expect(equal("1", [1, 2])).toEqual([true, false])
+  })
+})
+
+describe("evaluateNode", () => {
+  it("should evaluate math nodes correctly", () => {
+    const node = parse("1 + 2")
+    expect(evaluateNode(node)).toBe(3)
+  })
+})
