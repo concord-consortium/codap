f755cd928eb70605e5d55ebba6f1c8255750bc31
Kirk Swenson <kswenson@users.noreply.github.com>
2023-11-28 19:14:38 -0800

    fix: case table connecting lines (#1026)

--- v3/src/components/case-table/use-collection-table-model.ts
+++ v3/src/components/case-table/use-collection-table-model.ts
@@ -1,19 +1,8 @@
-import { useEffect, useRef } from "react"
 import { useCollectionContext } from "../../hooks/use-collection-context"
-import { CollectionTableModel } from "./collection-table-model"
 import { useCaseTableModel } from "./use-case-table-model"
 
 export function useCollectionTableModel(collectionId?: string) {
   const tableModel = useCaseTableModel()
   const collectionIdFromContext = useCollectionContext()
-  const collectionTableModel = useRef<CollectionTableModel | undefined>()
-
-  useEffect(() => {
-    const isMismatch = collectionTableModel.current?.collectionId !== collectionId
-    if (!collectionTableModel.current || isMismatch) {
-      collectionTableModel.current = tableModel?.getCollectionTableModel(collectionId ?? collectionIdFromContext)
-    }
-  })
-
-  return collectionTableModel.current
+  return tableModel?.getCollectionTableModel(collectionId ?? collectionIdFromContext)
 }
