26844da2dc06d4f1814ab3b0b10212be35533126
Ethan McElroy <emcelroy@concord.org>
2024-01-17 17:52:06 -0500

    chore: remove unused import

--- v3/src/components/graph/adornments/adornments.tsx
+++ v3/src/components/graph/adornments/adornments.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react"
+import React, { useEffect } from "react"
 import { clsx } from "clsx"
 import { observer } from "mobx-react-lite"
 import { mstAutorun } from "../../../utilities/mst-autorun"
