ce76d9a93911883a92b2ff66a083f33ad1e90032
Kirk Swenson <kswenson@users.noreply.github.com>
2024-02-06 22:24:19 -0800

    fix: CFM style tweaks; fix font configuration

--- v3/src/styles/menu-style.ts
+++ v3/src/styles/menu-style.ts
@@ -6,7 +6,7 @@ const helpers = createMultiStyleConfigHelpers(["menu", "list", "item"])
 export const Menu = helpers.defineMultiStyleConfig({
   baseStyle: {
     menu: {
-      font: `"Montserrat-Regular", sans-serif`,
+      font: `"Montserrat", sans-serif`,
       boxShadow: "md",
       rounded: "md",
       flexDirection: "column",
