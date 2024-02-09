e1ed4f4401ec064583ca1333ebd3160a114b8425
Teale Fristoe <fristoe@gmail.com>
2024-02-08 16:43:42 -0800

    Cleanup tile styling.

--- v3/src/components/web-view/web-view.tsx
+++ v3/src/components/web-view/web-view.tsx
@@ -9,8 +9,14 @@ export const WebViewComponent = ({ tile }: ITileBaseProps) => {
   if (!isWebViewModel(webViewModel)) return null
 
   return (
-    <div className="codap-web-view" data-testid="codap-web-view">
-      <iframe className="codap-web-view-iframe" src={webViewModel.url} />
+    <div className="codap-web-view-body" data-testid="codap-web-view">
+      <div className="codap-web-view-backdrop">
+        <div className="codap-web-view-url">URL</div>
+        <div className="codap-web-view-message">Check that URL yall.</div>
+      </div>
+      <div className="codap-web-view-iframe-wrapper">
+        <iframe className="codap-web-view-iframe" src={webViewModel.url} />
+      </div>
     </div>
   )
 }
