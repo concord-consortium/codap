1b0f2763aa6cc3c12dc2523618c29e450c350ac0
Teale Fristoe <fristoe@gmail.com>
2024-02-20 14:17:46 -0800

    Add cypress test.

---
+++ v3/cypress/e2e/plugin.spec.ts
@@ -0,0 +1,17 @@
+import { ToolbarElements as toolbar } from "../support/elements/toolbar-elements"
+import { WebViewTileElements } from "../support/elements/web-view-tile"
+
+context("codap plugins", () => {
+  beforeEach(function () {
+    const url = `${Cypress.config("index")}?mouseSensor`
+    cy.visit(url)
+  })
+  it('will update web view title in response to plugin request', ()=>{
+      const url='https://concord-consortium.github.io/codap-data-interactives/DataInteractiveAPITester/index.html?lang=en'
+      toolbar.getOptionsButton().click()
+      toolbar.getWebViewButton().click()
+      WebViewTileElements.enterUrl(url)
+      cy.wait(1000)
+      WebViewTileElements.getTitle().should("contain.text", "CODAP API Tester")
+  })
+})
