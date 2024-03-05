4a9f7996a1e23e7e6938bb7f9f4e6933b18920eb
Teale Fristoe <fristoe@gmail.com>
2024-03-04 16:04:02 -0800

    Add cypress test for a plugin getting an attribute.

--- v3/cypress/e2e/plugin.spec.ts
+++ v3/cypress/e2e/plugin.spec.ts
@@ -3,7 +3,7 @@ import { WebViewTileElements } from "../support/elements/web-view-tile"
 
 context("codap plugins", () => {
   beforeEach(function () {
-    const url = `${Cypress.config("index")}?mouseSensor`
+    const url = `${Cypress.config("index")}?mouseSensor&sample=mammals&dashboard`
     cy.visit(url)
   })
   it('will update web view title in response to plugin request', ()=>{
@@ -13,5 +13,37 @@ context("codap plugins", () => {
       WebViewTileElements.enterUrl(url)
       cy.wait(1000)
       WebViewTileElements.getTitle().should("contain.text", "CODAP API Tester")
+
+      cy.log("Handle get attribute request")
+      WebViewTileElements.getIFrame().find(`.di-message-area`).type(`{
+        "action": "get",
+        "resource": "dataContext[Mammals].collection[Mammals].attribute[Order]"
+      }`)
+      WebViewTileElements.getIFrame().find(`.di-send-button`).click()
+      WebViewTileElements.getIFrame().find(`.di-log-message`).contains(/.*"success":\s*true.*/).should("exist")
+      // WebViewTileElements.getIFrame().find(`.di-log-message`).eq(1).invoke("text").should("include", `{
+      //   "success": true,
+      //   "values": {
+      //     "name": "Order",
+      //     "type": "categorical",
+      //     "title": "",
+      //     "editable": true,
+      //     "hidden": false,
+      //     "renameable": true,
+      //     "deleteable": true,`)
+      // WebViewTileElements.getIFrame().find(`.di-log-message`).eq(1).should("have.text", `{
+      //   "success": true,
+      //   "values": {
+      //     "name": "Order",
+      //     "type": "categorical",
+      //     "title": "",
+      //     "editable": true,
+      //     "hidden": false,
+      //     "renameable": true,
+      //     "deleteable": true,
+      //     "guid": "ATTRZt2J9fkVbzJS",
+      //     "id": "ATTRZt2J9fkVbzJS"
+      //   }
+      // }`)
   })
 })
