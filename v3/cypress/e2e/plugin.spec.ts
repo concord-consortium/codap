b27fa104a85aae0e7ebeaaa3bdcdf36faa2eddd4
Teale Fristoe <fristoe@gmail.com>
2024-03-06 07:02:03 -0800

    Add cypress test.

--- v3/cypress/e2e/plugin.spec.ts
+++ v3/cypress/e2e/plugin.spec.ts
@@ -1,5 +1,6 @@
+import { TableTileElements as table } from "../support/elements/table-tile"
 import { ToolbarElements as toolbar } from "../support/elements/toolbar-elements"
-import { WebViewTileElements } from "../support/elements/web-view-tile"
+import { WebViewTileElements as webView } from "../support/elements/web-view-tile"
 
 context("codap plugins", () => {
   beforeEach(function () {
@@ -10,35 +11,60 @@ context("codap plugins", () => {
       const url='https://concord-consortium.github.io/codap-data-interactives/DataInteractiveAPITester/index.html?lang=en'
       toolbar.getOptionsButton().click()
       toolbar.getWebViewButton().click()
-      WebViewTileElements.enterUrl(url)
+      webView.enterUrl(url)
       cy.wait(1000)
-      WebViewTileElements.getTitle().should("contain.text", "CODAP API Tester")
+      webView.getTitle().should("contain.text", "CODAP API Tester")
 
       cy.log("Handle get attribute request")
       const cmd1 = `{
         "action": "get",
         "resource": "dataContext[Mammals].collection[Mammals].attribute[Order]"
       }`
-      WebViewTileElements.sendAPITesterCommand(cmd1)
-      WebViewTileElements.confirmAPITesterResponseContains(/.*"success":\s*true.*/)
-      WebViewTileElements.clearAPITesterResponses()
+      webView.sendAPITesterCommand(cmd1)
+      webView.confirmAPITesterResponseContains(/.*"success":\s*true.*/)
+      webView.clearAPITesterResponses()
 
       cy.log("Properly handles illegal actions")
       const cmd2 = `{
         "action": "fake",
-        "resource": "dataContext[Mammals].collection[Mammals].attribute[name]"
+        "resource": "dataContext[Mammals].collection[Mammals].attribute[Order]"
       }`
-      WebViewTileElements.sendAPITesterCommand(cmd2, cmd1)
-      WebViewTileElements.confirmAPITesterResponseContains(/.*"Unsupported action: fake\/attribute".*/)
-      WebViewTileElements.clearAPITesterResponses()
+      webView.sendAPITesterCommand(cmd2, cmd1)
+      webView.confirmAPITesterResponseContains(/.*"Unsupported action: fake\/attribute".*/)
+      webView.clearAPITesterResponses()
 
       cy.log("Finds the default dataset when no dataset is included")
       const cmd3 = `{
         "action": "get",
         "resource": "collection[Mammals].attribute[Order]"
       }`
-      WebViewTileElements.sendAPITesterCommand(cmd3, cmd2)
-      WebViewTileElements.confirmAPITesterResponseContains(/.*"success":\s*true.*/)
-      WebViewTileElements.clearAPITesterResponses()
+      webView.sendAPITesterCommand(cmd3, cmd2)
+      webView.confirmAPITesterResponseContains(/.*"success":\s*true.*/)
+      webView.clearAPITesterResponses()
+
+      cy.log("Handle update attribute request")
+      const cmd4 = `{
+        "action": "update",
+        "resource": "dataContext[Mammals].collection[Mammal].attribute[Order]",
+        "values": {
+          "title": "new title",
+          "name": "new name",
+          "description": "new description",
+          "unit": "new unit",
+          "formula": "new formula",
+          "editable": false,
+          "precision": 10
+        }
+      }`
+      webView.sendAPITesterCommand(cmd4, cmd3)
+      webView.confirmAPITesterResponseContains(/.*"description":\s*"new description".*/)
+      webView.confirmAPITesterResponseContains(/.*"editable":\s*false.*/)
+      webView.confirmAPITesterResponseContains(/.*"formula":\s*"new formula".*/)
+      webView.confirmAPITesterResponseContains(/.*"name":\s*"new name".*/)
+      webView.confirmAPITesterResponseContains(/.*"precision":\s*10.*/)
+      webView.confirmAPITesterResponseContains(/.*"title":\s*"new title".*/)
+      webView.confirmAPITesterResponseContains(/.*"unit":\s*"new unit".*/)
+      table.getColumnHeaders().contains("new name (new unit)").should("exist")
+      webView.clearAPITesterResponses()
   })
 })
