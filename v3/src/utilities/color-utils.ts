d064cce66b8740e11ec2ab6cf6d44fc43f280954
Kirk Swenson <kswenson@users.noreply.github.com>
2024-03-15 09:38:11 -0700

    fix: case table display of color swatches with out-of-range values

--- v3/src/utilities/color-utils.ts
+++ v3/src/utilities/color-utils.ts
@@ -1,3 +1,7 @@
+import { Colord, colord, getFormat as getColorFormat } from "colord"
+import namesPlugin from "colord/plugins/names"
+import type { Parsers } from "colord/types"
+
 /*
   The following list of 20 colors are maximally visually distinct from each other.
   See http://eleanormaclure.files.wordpress.com/2011/03/colour-coding.pdf
@@ -23,3 +27,38 @@ export const defaultPointColor = '#E6805B',
   defaultSelectedStrokeWidth = 2,
   defaultSelectedStrokeOpacity = 1,
   defaultBackgroundColor = '#FFFFFF'
+
+/*
+  The colord library maintains a single global list of parsers, so there's no built-in
+  way to maintain two different sets of parsers to accommodate the loose and strict
+  parsing required by CODAP. Therefore, we configure the colord library for the strict
+  set of colors and make use of the additional plugins to handle the loose formatting,
+  particularly the color names. To do so, we initialize the names plugin with a pointer
+  to our own array of parsers and then extract the parse function itself from the array.
+ */
+const parsers: Parsers = {
+  string: [],
+  object: [],
+}
+// Initialize the names plugin, passing a pointer to our own parser arrays.
+namesPlugin(Colord, parsers)
+// Extract the parsing function that the plugin installed into the parser arrays.
+const parseColorName = parsers.string[0][0]
+
+export interface ParseColorOptions {
+  colorNames?: boolean
+}
+
+/**
+ * parseColor
+ *
+ * @param str string to be parsed for its color
+ * @param options { colorNames?: boolean } whether or not to recognize color names when parsing
+ * @returns canonicalized color string or empty string
+ */
+export function parseColor(str: string, options?: ParseColorOptions) {
+  // if it's a valid color name, return it
+  if (options?.colorNames && parseColorName(str)) return str.toLowerCase()
+  // if it's a valid color string, return its hex equivalent
+  return getColorFormat(str) ? colord(str).toHex() : ""
+}
