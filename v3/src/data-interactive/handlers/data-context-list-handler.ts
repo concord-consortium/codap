2acbf4ff69248bb9600fa0082d938caa87f34763
Teale Fristoe <fristoe@gmail.com>
2024-03-18 10:27:56 -0700

    Remove unused handler functions from diDataContextListHandler.

--- v3/src/data-interactive/handlers/data-context-list-handler.ts
+++ v3/src/data-interactive/handlers/data-context-list-handler.ts
@@ -1,23 +1,20 @@
 import { t } from "../../utilities/translation/translate"
 import { registerDIHandler } from "../data-interactive-handler"
-import { DIHandler, DIResources, diNotImplementedYet } from "../data-interactive-types"
+import { DIHandler, DIResources } from "../data-interactive-types"
 
 export const diDataContextListHandler: DIHandler = {
   get(resources: DIResources) {
     if (!resources.dataContextList) {
       return { success: false, values: { error: t("V3.DI.Error.dataContextNotFound") } }
     }
-    
+
     return { success: true, values: resources.dataContextList.map(dataSet => ({
       name: dataSet.name,
       guid: Number(dataSet.id), // TODO: This is wrong and there is no current equivalent to guid in v3
       title: dataSet.name, // TODO: DataSets don't have titles in v3
       id: Number(dataSet.id) // TODO: This is wrong and there is no current equivalent to v2's id in v3
     }))}
-  },
-  create: diNotImplementedYet,
-  update: diNotImplementedYet,
-  delete: diNotImplementedYet
+  }
 }
 
 registerDIHandler("dataContextList", diDataContextListHandler)
