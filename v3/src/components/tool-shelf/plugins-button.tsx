c00dcfb4ab4720adaa97e8e5b0edbde4b509c7e9
Teale Fristoe <fristoe@gmail.com>
2024-03-21 09:35:00 -0700

    Make dropdown selection create plugin.

--- v3/src/components/tool-shelf/plugins-button.tsx
+++ v3/src/components/tool-shelf/plugins-button.tsx
@@ -1,9 +1,22 @@
 import React from "react"
 import { Menu, MenuButton, MenuItem, MenuList, Tag } from "@chakra-ui/react"
 import PluginsIcon from '../../assets/icons/icon-plug.svg'
+import { useDocumentContent } from "../../hooks/use-document-content"
 import { t } from "../../utilities/translation/translate"
+import { kWebViewTileType } from "../web-view/web-view-defs"
+import { IWebViewModel } from "../web-view/web-view-model"
 
 export function PluginsButton() {
+  const documentContent = useDocumentContent()
+
+  function handleTileButtonClick(url: string) {
+    documentContent?.applyUndoableAction(() => {
+      // TODO v2 eliminates the undo history when you add a plugin
+      const tile = documentContent?.createOrShowTile?.(kWebViewTileType)
+      if (tile) (tile.content as IWebViewModel).setUrl(url)
+    }, "", "")
+  }
+
   return (
     <Menu isLazy>
       <MenuButton
@@ -15,8 +28,11 @@ export function PluginsButton() {
         <Tag className="tool-shelf-tool-label">{t("DG.ToolButtonData.pluginMenu.title")}</Tag>
       </MenuButton>
       <MenuList>
-        <MenuItem data-testid="tool-shelf-plugins-option" onClick={() => console.log(`selected`)}>
-          Sampler
+        <MenuItem
+          data-testid="tool-shelf-plugins-option"
+          onClick={() => handleTileButtonClick("https://www.wikipedia.org")}
+        >
+          <PluginsIcon />Sampler
         </MenuItem>
       </MenuList>
     </Menu>
