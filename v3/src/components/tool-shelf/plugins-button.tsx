c94bd78c495f79893242ecae7db97099734a6b98
Teale Fristoe <fristoe@gmail.com>
2024-03-21 08:59:59 -0700

    Make plugins button a dropdown menu.

--- v3/src/components/tool-shelf/plugins-button.tsx
+++ v3/src/components/tool-shelf/plugins-button.tsx
@@ -1,15 +1,24 @@
 import React from "react"
-import PluginIcon from '../../assets/icons/icon-plug.svg'
+import { Menu, MenuButton, MenuItem, MenuList, Tag } from "@chakra-ui/react"
+import PluginsIcon from '../../assets/icons/icon-plug.svg'
 import { t } from "../../utilities/translation/translate"
-import { ToolShelfButton } from "./tool-shelf-button"
 
 export function PluginsButton() {
   return (
-    <ToolShelfButton
-      hint={t("DG.ToolButtonData.pluginMenu.toolTip")}
-      icon={<PluginIcon />}
-      label={t("DG.ToolButtonData.pluginMenu.title")}
-      onClick={() => console.log(`... Clicked plugin button`)}
-    />
+    <Menu isLazy>
+      <MenuButton
+        className="tool-shelf-button menu plugins"
+        title={t("DG.ToolButtonData.optionMenu.toolTip")}
+        data-testid="tool-shelf-button-plugins"
+      >
+        <PluginsIcon />
+        <Tag className="tool-shelf-tool-label">{t("DG.ToolButtonData.pluginMenu.title")}</Tag>
+      </MenuButton>
+      <MenuList>
+        <MenuItem data-testid="tool-shelf-plugins-option" onClick={() => console.log(`selected`)}>
+          Sampler
+        </MenuItem>
+      </MenuList>
+    </Menu>
   )
 }
