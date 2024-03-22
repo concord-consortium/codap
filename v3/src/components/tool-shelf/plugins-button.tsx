ab86e4a9704a072dce44d0c44b48cc4bb227c108
Teale Fristoe <fristoe@gmail.com>
2024-03-21 18:00:12 -0700

    Use plugin data to populate dropdown options.

--- v3/src/components/tool-shelf/plugins-button.tsx
+++ v3/src/components/tool-shelf/plugins-button.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import { Menu, MenuButton, MenuItem, MenuList, Tag } from "@chakra-ui/react"
 import PluginsIcon from '../../assets/icons/icon-plug.svg'
 import { useDocumentContent } from "../../hooks/use-document-content"
@@ -6,14 +6,41 @@ import { t } from "../../utilities/translation/translate"
 import { kWebViewTileType } from "../web-view/web-view-defs"
 import { IWebViewModel } from "../web-view/web-view-model"
 
+const rootPluginUrl = "https://codap-resources.s3.amazonaws.com/plugins"
+const pluginDataUrl = `${rootPluginUrl}/published-plugins.json`
+
+interface PluginData {
+  aegis?: string,
+  categories: string[],
+  description: string,
+  "description-string": string,
+  height: number,
+  icon: string,
+  isStandard: string, // All have "true" for some reason
+  path: string,
+  title: string,
+  "title-string": string,
+  visible: boolean | string, // Most have "true" or "false" for some reason, but a couple have true
+  width: number
+}
+
 export function PluginsButton() {
   const documentContent = useDocumentContent()
+  const [pluginData, setPluginData] = useState<PluginData[]>([])
 
-  function handleTileButtonClick(url: string) {
+  useEffect(() => {
+    fetch(pluginDataUrl)
+      .then(response => response.json())
+      .then(json => setPluginData(json))
+  }, [])
+
+  function handlePluginSelectionClick(pd: PluginData) {
+    const url = `${rootPluginUrl}${pd.path}`
     documentContent?.applyUndoableAction(() => {
       // TODO v2 eliminates the undo history when you add a plugin
       const tile = documentContent?.createOrShowTile?.(kWebViewTileType)
       if (tile) (tile.content as IWebViewModel).setUrl(url)
+      // TODO Set the tile's height and width
     }, "", "")
   }
 
@@ -28,12 +55,16 @@ export function PluginsButton() {
         <Tag className="tool-shelf-tool-label">{t("DG.ToolButtonData.pluginMenu.title")}</Tag>
       </MenuButton>
       <MenuList>
-        <MenuItem
-          data-testid="tool-shelf-plugins-option"
-          onClick={() => handleTileButtonClick("https://www.wikipedia.org")}
-        >
-          <PluginsIcon />Sampler
-        </MenuItem>
+        {pluginData.map(pd => (
+          <MenuItem
+            data-testid="tool-shelf-plugins-option"
+            key={pd.title}
+            onClick={() => handlePluginSelectionClick(pd)}
+          >
+            <img src={`${rootPluginUrl}${pd.icon}`} />
+            {pd.title}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   )
