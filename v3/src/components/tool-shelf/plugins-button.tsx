a0eb06cc9ac7acb65471337292095add2ecf28fe
Teale Fristoe <fristoe@gmail.com>
2024-03-25 14:10:47 -0700

    Improve PluginData typing.

--- v3/src/components/tool-shelf/plugins-button.tsx
+++ v3/src/components/tool-shelf/plugins-button.tsx
@@ -16,14 +16,14 @@ interface PluginData {
   aegis?: string,
   categories: string[],
   description: string,
-  "description-string": string,
+  "description-string"?: string,
   height: number,
   icon: string,
-  isStandard: string, // All have "true" for some reason
+  isStandard: "true" | "false", // All have "true" for some reason
   path: string,
   title: string,
-  "title-string": string,
-  visible: boolean | string, // Most have "true" or "false" for some reason, but a couple have true
+  "title-string"?: string,
+  visible: boolean | "true" | "false", // Most have "true" or "false" for some reason, but a couple have true
   width: number
 }
 
