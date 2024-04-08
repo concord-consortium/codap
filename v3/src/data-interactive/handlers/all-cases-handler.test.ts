3051d4a25772840d0b8fcc561120e3553cb4bcf6
Teale Fristoe <fristoe@gmail.com>
2024-04-08 15:34:13 -0700

    Fix type issues.

--- v3/src/data-interactive/handlers/all-cases-handler.test.ts
+++ v3/src/data-interactive/handlers/all-cases-handler.test.ts
@@ -1,6 +1,6 @@
 import { CollectionModel, ICollectionModel } from "../../models/data/collection"
 import { DataSet, IDataSet, toCanonical } from "../../models/data/data-set"
-import { DIAllCases, DIHandlerFnResult } from "../data-interactive-types"
+import { DIAllCases } from "../data-interactive-types"
 import { diAllCasesHandler } from "./all-cases-handler"
 
 describe("DataInteractive AllCasesHandler", () => {
@@ -31,32 +31,36 @@ describe("DataInteractive AllCasesHandler", () => {
 
   it("delete works as expected", () => {
     setupDataset()
-    expect(dataset.cases.length).toBe(cases.length)
+    expect(dataset!.cases.length).toBe(cases.length)
     expect(handler.delete?.({ dataContext: dataset })?.success).toBe(true)
-    expect(dataset.cases.length).toBe(0)
+    expect(dataset!.cases.length).toBe(0)
   })
   it("get works as expected", () => {
     setupDataset()
 
-    const getCase = (result: DIHandlerFnResult, index: number) =>
-      (result.values! as DIAllCases).cases![index].case!
+    interface GetAllCasesResult {
+      success: boolean,
+      values: DIAllCases
+    }
+    const getCase = (result: GetAllCasesResult, index: number) =>
+      result.values.cases![index].case!
     const checkCase = (c: any, attribute: string, value: string, children: number, parent?: string) => {
       expect(c.values[attribute]).toBe(value)
       expect(c.children.length).toBe(children)
       expect(c.parent).toBe(parent)
     }
 
-    const result1 = handler.get?.({ dataContext: dataset, collection: c1 })
+    const result1 = handler.get?.({ dataContext: dataset, collection: c1 }) as GetAllCasesResult
     expect(result1.success).toBe(true)
-    expect(result1.values.cases.length).toBe(2)
+    expect(result1.values.cases?.length).toBe(2)
     const c1c1 = getCase(result1, 0)
     checkCase(c1c1, "a1", "a", 2)
     const c1c2 = getCase(result1, 1)
     checkCase(c1c2, "a1", "b", 2)
 
-    const result2 = handler.get?.({ dataContext: dataset, collection: c2 })
+    const result2 = handler.get?.({ dataContext: dataset, collection: c2 }) as GetAllCasesResult
     expect(result2.success).toBe(true)
-    expect(result2.values.cases.length).toBe(4)
+    expect(result2.values.cases?.length).toBe(4)
     const c2c1 = getCase(result2, 0)
     checkCase(c2c1, "a2", "x", 2, c1c1.id)
     const c2c2 = getCase(result2, 1)
@@ -66,9 +70,9 @@ describe("DataInteractive AllCasesHandler", () => {
     const c2c4 = getCase(result2, 3)
     checkCase(c2c4, "a2", "z", 1, c1c2.id)
 
-    const result3 = handler.get?.({ dataContext: dataset, collection: dataset?.ungrouped })
+    const result3 = handler.get?.({ dataContext: dataset, collection: dataset?.ungrouped }) as GetAllCasesResult
     expect(result3.success).toBe(true)
-    expect(result3.values.cases.length).toBe(6)
+    expect(result3.values.cases?.length).toBe(6)
     checkCase(getCase(result3, 0), "a3", "1", 0, c2c1.id)
     checkCase(getCase(result3, 1), "a3", "5", 0, c2c1.id)
     checkCase(getCase(result3, 2), "a3", "3", 0, c2c2.id)
