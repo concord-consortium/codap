8882e3d5bb64123874356ef4441d6cad7a0cac77
eireland <eireland@concord.org>
2024-04-10 10:13:10 -0700

    Build 0720

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -128,7 +128,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0719',
+    BUILD_NUM: '0720',
 
     IS_DG_BUILD: isDGBuild.property(),
 
