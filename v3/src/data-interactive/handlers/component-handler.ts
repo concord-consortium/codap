fb777791972acb1ac07ef39a2922c135df29fa52
Teale Fristoe <fristoe@gmail.com>
2024-04-19 14:46:08 -0700

    Handle delete component requests.

--- v3/src/data-interactive/handlers/component-handler.ts
+++ v3/src/data-interactive/handlers/component-handler.ts
@@ -2,10 +2,19 @@ import { t } from "../../utilities/translation/translate"
 import { registerDIHandler } from "../data-interactive-handler"
 import { DIHandler, DINotification, DIResources, DIValues } from "../data-interactive-types"
 import { uiState } from "../../models/ui-state"
+import { appState } from "../../models/app-state"
 
 const componentNotFoundResult = { success: false , values: { error: t("V3.DI.Error.componentNotFound") } } as const
 
 export const diComponentHandler: DIHandler = {
+  delete(resources: DIResources) {
+    const { component } = resources
+    if (!component) return componentNotFoundResult
+
+    appState.document.deleteTile(component.id)
+
+    return { success: true }
+  },
   notify(resources: DIResources, values?: DIValues) {
     const { component } = resources
     if (!component) return componentNotFoundResult
@@ -15,6 +24,7 @@ export const diComponentHandler: DIHandler = {
     const { request } = values as DINotification
     if (request === "select") {
       uiState.setFocusedTile(component.id)
+    // } else if (request === "autoScale") { // TODO Handle autoScale requests
     }
 
     return { success: true }
