578685301d30a454323a13d58dbfab64611c9448
Kirk Swenson <kswenson@users.noreply.github.com>
2024-04-22 17:36:15 -0700

    chore: add cypress test

---
+++ v3/cypress/e2e/case-card.spec.ts
@@ -0,0 +1,23 @@
+import { TableTileElements as table } from "../support/elements/table-tile"
+
+beforeEach(() => {
+  // cy.scrollTo() doesn't work as expected with `scroll-behavior: smooth`
+  const queryParams = "?sample=mammals&scrollBehavior=auto"
+  const url = `${Cypress.config("index")}${queryParams}`
+  cy.visit(url)
+  cy.wait(2000)
+})
+
+context("case card", () => {
+  describe("case card", () => {
+    it("can switch from case table to case card view and back", () => {
+      const tableHeaderLeftSelector = ".codap-component.codap-case-table .component-title-bar .header-left"
+      cy.get(tableHeaderLeftSelector).click()
+      cy.get(`${tableHeaderLeftSelector} .card-table-toggle-message`).click()
+      cy.wait(500)
+      const cardHeaderLeftSelector = ".codap-component.codap-case-card .component-title-bar .header-left"
+      cy.get(cardHeaderLeftSelector).click()
+      cy.get(`${cardHeaderLeftSelector} .card-table-toggle-message`).click()
+    })
+  })
+})
