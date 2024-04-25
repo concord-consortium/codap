3037a32f3bfaa2e40d81ffe4ed86dcf42468b691
Teale Fristoe <fristoe@gmail.com>
2024-04-25 14:28:12 -0700

    Include hidden information in get componentList.

--- v3/src/data-interactive/handlers/component-list-handler.ts
+++ v3/src/data-interactive/handlers/component-list-handler.ts
@@ -1,13 +1,15 @@
 import { isWebViewModel } from "../../components/web-view/web-view-model"
 import { appState } from "../../models/app-state"
+import { isFreeTileLayout } from "../../models/document/free-tile-row"
 import { kComponentTypeV3ToV2Map, kV2GameType, kV2WebViewType } from "../data-interactive-component-types"
 import { registerDIHandler } from "../data-interactive-handler"
 import { DIComponentInfo, DIHandler, DIResources } from "../data-interactive-types"
 
 export const diComponentListHandler: DIHandler = {
   get(_resources: DIResources) {
+    const { document } = appState
     const values: DIComponentInfo[] = []
-    appState.document.content?.tileMap.forEach(tile => {
+    document.content?.tileMap.forEach(tile => {
       // TODO Should we add names to tiles?
       // TODO Tiles sometimes show titles different than tile.title. Should we return those?
       const { content, id, title } = tile
@@ -16,7 +18,9 @@ export const diComponentListHandler: DIHandler = {
           ? kV2GameType
           : kV2WebViewType
         : kComponentTypeV3ToV2Map[content.type]
-      values.push({ id, title, type })
+      const tileLayout = document.content?.getTileLayoutById(id)
+      const hidden = isFreeTileLayout(tileLayout) ? !!tileLayout.isHidden : false
+      values.push({ hidden, id, title, type })
     })
 
     return { success: true, values }
