3da8338ad26bd5d0e632a8e2e26b99335e1ad1f5
Teale Fristoe <fristoe@gmail.com>
2024-05-08 18:27:20 -0700

    Handle get collection requests.

---
+++ v3/src/data-interactive/handlers/collection-handler.ts
@@ -0,0 +1,25 @@
+import { isCollectionModel } from "../../models/data/collection"
+import { t } from "../../utilities/translation/translate"
+import { registerDIHandler } from "../data-interactive-handler"
+import { DIHandler, DIResources } from "../data-interactive-types"
+import { convertCollectionToV2, convertUngroupedCollectionToV2 } from "../data-interactive-type-utils"
+
+const collectionNotFoundResult = { success: false, values: { error: t("V3.DI.Error.collectionNotFound") } } as const
+
+export const diCollectionHandler: DIHandler = {
+  get(resources: DIResources) {
+    const { collection, dataContext } = resources
+    if (!dataContext) return { success: false, values: { error: t("V3.DI.Error.dataContextNotFound") } }
+    if (!collection) return collectionNotFoundResult
+
+    const v2Collection = isCollectionModel(collection)
+      ? convertCollectionToV2(collection)
+      : convertUngroupedCollectionToV2(dataContext)
+    return {
+      success: true,
+      values: v2Collection
+    }
+  }
+}
+
+registerDIHandler("collection", diCollectionHandler)
