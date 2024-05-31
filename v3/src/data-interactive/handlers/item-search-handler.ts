e42a24e675c4cb5d2be4a8aa040642f6df4a5f3f
Teale Fristoe <fristoe@gmail.com>
2024-05-30 17:12:39 -0700

    Handle delete itemSearch requests.

--- v3/src/data-interactive/handlers/item-search-handler.ts
+++ v3/src/data-interactive/handlers/item-search-handler.ts
@@ -1,17 +1,29 @@
+import { toV2Id } from "../../utilities/codap-utils"
 import { registerDIHandler } from "../data-interactive-handler"
 import { getV2ItemResult } from "../data-interactive-type-utils"
-import { DIHandler, DIResources, diNotImplementedYet } from "../data-interactive-types"
+import { DIHandler, DIResources } from "../data-interactive-types"
 import { couldNotParseQueryResult, dataContextNotFoundResult } from "./di-results"
 
 export const diItemSearchHandler: DIHandler = {
-  delete: diNotImplementedYet,
+  delete(resources: DIResources) {
+    const { dataContext, itemSearch } = resources
+    if (!dataContext) return dataContextNotFoundResult
+    if (!itemSearch) return couldNotParseQueryResult
+
+    const itemIds = itemSearch.map(aCase => aCase.__id__)
+    dataContext.applyModelChange(() => {
+      dataContext.removeCases(itemIds)
+    })
+
+    return { success: true, values: itemIds.map(itemId => toV2Id(itemId)) }
+  },
 
   get(resources: DIResources) {
     const { dataContext, itemSearch } = resources
     if (!dataContext) return dataContextNotFoundResult
     if (!itemSearch) return couldNotParseQueryResult
 
-    const values = itemSearch?.map(aCase => getV2ItemResult(dataContext, aCase.__id__))
+    const values = itemSearch.map(aCase => getV2ItemResult(dataContext, aCase.__id__))
     return { success: true, values }
   }
 }
