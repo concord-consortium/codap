968a956ecc69b2e3b4d97516373ad2f821ab0e61
Teale Fristoe <fristoe@gmail.com>
2024-06-01 18:07:36 -0700

    Handle create component webView.

--- v3/src/data-interactive/data-interactive-component-types.ts
+++ v3/src/data-interactive/data-interactive-component-types.ts
@@ -46,6 +46,9 @@ export interface V2Component {
   title?: string
   type: string
 }
+export interface V2Calculator extends V2Component {
+  type: "calculator"
+}
 export interface V2CaseTable extends V2Component {
   dataContext?: string
   horizontalScrollOffset?: number
@@ -57,9 +60,44 @@ export interface V2Graph extends V2Component {
   enableNumberToggle?: boolean
   legendAttributeName?: string
   numberToggleLastMode?: boolean
+  type: "graph"
   xAttributeName?: string
   yAttributeName?: string
   y2AttributeName?: string
 }
+export interface V2GuidePage {
+  itemTitle: string
+  url: string
+}
+export interface V2Guide extends V2Component {
+  currentItemIndex?: number
+  isVisible?: boolean
+  items?: V2GuidePage[]
+  type: "guideView"
+}
+export interface V2Map extends V2Component {
+  center?: [number, number]
+  dataContext?: string
+  legendAttributeName?: string
+  type: "map"
+  zoom?: number
+}
+export interface V2Slider extends V2Component {
+  animationDirection?: number
+  animationMode?: number
+  globalValueName?: string
+  lowerBound?: number
+  type: "slider"
+  upperBound?: number
+}
+export interface V2Text extends V2Component {
+  text?: string
+  type: "text"
+}
+export interface V2WebView extends V2Component {
+  type: "webView"
+  URL?: string
+}
 
-export type V2SpecificComponent = V2CaseTable | V2Graph
+export type V2SpecificComponent = V2Calculator | V2CaseTable | V2Graph | V2Guide | V2Map | V2Slider | V2Text |
+  V2WebView
--- v3/src/data-interactive/handlers/component-handler.ts
+++ v3/src/data-interactive/handlers/component-handler.ts
@@ -1,6 +1,10 @@
 import { createOrShowTableForDataset } from "../../components/case-table/case-table-utils"
+import { GraphAttrRole } from "../../components/data-display/data-display-types"
+import { IGraphContentModel } from "../../components/graph/models/graph-content-model"
+import { IWebViewModel } from "../../components/web-view/web-view-model"
 import { appState } from "../../models/app-state"
 import { kSharedCaseMetadataType, SharedCaseMetadata } from "../../models/shared/shared-case-metadata"
+import { ISharedDataSet } from "../../models/shared/shared-data-set"
 import { getSharedDataSets } from "../../models/shared/shared-data-utils"
 import { getSharedModelManager } from "../../models/tiles/tile-environment"
 import { uiState } from "../../models/ui-state"
@@ -9,12 +13,10 @@ import { t } from "../../utilities/translation/translate"
 import { registerDIHandler } from "../data-interactive-handler"
 import { DIHandler, DINotification, diNotImplementedYet, DIResources, DIValues } from "../data-interactive-types"
 import {
-  kComponentTypeV2ToV3Map, kV2CaseTableType, kV2GraphType, V2CaseTable, V2Component, V2Graph
+  kComponentTypeV2ToV3Map, kV2CaseTableType, kV2GraphType, kV2WebViewType, V2CaseTable, V2Component, V2Graph,
+  V2WebView
 } from "../data-interactive-component-types"
 import { componentNotFoundResult, dataContextNotFoundResult, valuesRequiredResult } from "./di-results"
-import { IGraphContentModel } from "../../components/graph/models/graph-content-model"
-import { ISharedDataSet } from "../../models/shared/shared-data-set"
-import { GraphAttrRole } from "../../components/data-display/data-display-types"
 
 export const diComponentHandler: DIHandler = {
   create(_resources: DIResources, values?: DIValues) {
@@ -40,91 +42,98 @@ export const diComponentHandler: DIHandler = {
       success: false as const,
       values: { error: t("V3.DI.Error.fieldRequired", { vars: ["Create", type, "dataContext"] }) }
     }
-    if (type === kV2CaseTableType) {
-      const { dataContext } = values as V2CaseTable
-      if (!dataContext) return dataContextRequiredResult
-      const sharedDataSet = getSharedDataSet(dataContext)
-      if (!sharedDataSet) return dataContextNotFoundResult
-      const dataSet = getDataSet(dataContext, sharedDataSet)
-      if (!dataSet) return dataContextNotFoundResult
+    return document.applyModelChange(() => {
+      if (type === kV2CaseTableType) {
+        const { dataContext } = values as V2CaseTable
+        if (!dataContext) return dataContextRequiredResult
+        const sharedDataSet = getSharedDataSet(dataContext)
+        if (!sharedDataSet) return dataContextNotFoundResult
+        const dataSet = getDataSet(dataContext, sharedDataSet)
+        if (!dataSet) return dataContextNotFoundResult
 
-      const caseMetadata = getCaseMetadata(dataSet.id)
-      if (!caseMetadata) {
-        return { success: false, values: { error: t("V3.DI.Error.caseMetadataNotFound", { vars: [dataContext] }) } }
-      }
-
-      const tile = createOrShowTableForDataset(sharedDataSet)
-      if (!tile) return { success: false, values: { error: "Unable to create tile." } }
-
-      // TODO Handle more options, like isIndexHidden
-      return {
-        success: true,
-        values: {
-          id: toV2Id(tile.id),
-          title: tile.title,
-          type
+        const caseMetadata = getCaseMetadata(dataSet.id)
+        if (!caseMetadata) {
+          return { success: false, values: { error: t("V3.DI.Error.caseMetadataNotFound", { vars: [dataContext] }) } }
         }
-      }
-    } else if (kComponentTypeV2ToV3Map[type]) {
-      const tile = document.content?.createOrShowTile(kComponentTypeV2ToV3Map[type], { cannotClose, ...dimensions })
-      if (!tile) return { success: false, values: { error: "Unable to create tile." } }
 
-      if (title) {
-        tile.setTitle(title)
-      } else if (name) {
-        tile.setTitle(name)
-      }
+        const tile = createOrShowTableForDataset(sharedDataSet)
+        if (!tile) return { success: false, values: { error: "Unable to create tile." } }
 
-      // TODO Handle position
+        // TODO Handle more options, like isIndexHidden
+        return {
+          success: true,
+          values: {
+            id: toV2Id(tile.id),
+            title: tile.title,
+            type
+          }
+        }
+      } else if (kComponentTypeV2ToV3Map[type]) {
+        const tile = document.content?.createOrShowTile(kComponentTypeV2ToV3Map[type], { cannotClose, ...dimensions })
+        if (!tile) return { success: false, values: { error: "Unable to create tile." } }
 
-      if (type === kV2GraphType) {
-        const graphTile = tile.content as IGraphContentModel
-        const { dataContext, legendAttributeName, xAttributeName, yAttributeName, y2AttributeName } = values as V2Graph
-        if (dataContext) {
-          const dataSet = getDataSet(dataContext)
-          if (dataSet) {
-            const caseMetadata = getCaseMetadata(dataSet.id)
-            graphTile.layers.forEach(layer => layer.dataConfiguration.setDataset(dataSet, caseMetadata))
+        if (title) {
+          tile.setTitle(title)
+        } else if (name) {
+          tile.setTitle(name)
+        }
 
-            const setAttribute = (role: GraphAttrRole, attributeName?: string) => {
-              if (attributeName) {
-                const attribute = dataSet.getAttributeByName(attributeName)
-                if (attribute) {
-                  graphTile.setAttributeID(role, dataSet.id, attribute.id)
+        // TODO Handle position
+
+        if (type === kV2GraphType) {
+          const graphTile = tile.content as IGraphContentModel
+          const {
+            dataContext, legendAttributeName, xAttributeName, yAttributeName, y2AttributeName
+          } = values as V2Graph
+          if (dataContext) {
+            const dataSet = getDataSet(dataContext)
+            if (dataSet) {
+              const caseMetadata = getCaseMetadata(dataSet.id)
+              graphTile.layers.forEach(layer => layer.dataConfiguration.setDataset(dataSet, caseMetadata))
+
+              const setAttribute = (role: GraphAttrRole, attributeName?: string) => {
+                if (attributeName) {
+                  const attribute = dataSet.getAttributeByName(attributeName)
+                  if (attribute) {
+                    graphTile.setAttributeID(role, dataSet.id, attribute.id)
+                  }
                 }
               }
-            }
-            // TODO Figure out how to do this without setTimeout
-            setTimeout(() => {
-              setAttribute("legend", legendAttributeName)
-              setAttribute("x", xAttributeName)
-              setAttribute("y", yAttributeName)
-              setAttribute("yPlus", y2AttributeName)
-            })
+              // TODO Figure out how to do this without setTimeout
+              setTimeout(() => {
+                setAttribute("legend", legendAttributeName)
+                setAttribute("x", xAttributeName)
+                setAttribute("y", yAttributeName)
+                setAttribute("yPlus", y2AttributeName)
+              })
 
-            // TODO Handle enableNumberToggle and numberToggleLastMode
+              // TODO Handle enableNumberToggle and numberToggleLastMode
+            }
+          }
+        } else if (type === kV2WebViewType) {
+          const webViewTile = tile.content as IWebViewModel
+          const { URL } = values as V2WebView
+          if (URL) webViewTile.setUrl(URL)
+        }
+
+        return {
+          success: true,
+          values: {
+            id: toV2Id(tile.id),
+            title: tile.title,
+            type
           }
         }
       }
 
-      return {
-        success: true,
-        values: {
-          id: toV2Id(tile.id),
-          title: tile.title,
-          type
-        }
-      }
-    }
-
-    // TODO Handle other types:
-    // map
-    // slider
-    // calculator
-    // text
-    // webView
-    // guide
-    return { success: false, values: { error: `Unsupported component type ${type}` } }
+      // TODO Handle other types:
+      // map
+      // slider
+      // calculator
+      // text
+      // guide
+      return { success: false, values: { error: `Unsupported component type ${type}` } }
+    })
   },
   
   delete(resources: DIResources) {
