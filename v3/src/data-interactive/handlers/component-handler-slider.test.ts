917329bc5ba1558afd89bafc1049d9667ff83ed2
Teale Fristoe <fristoe@gmail.com>
2024-06-06 11:43:59 -0700

    Add test for create component slider requests.

---
+++ v3/src/data-interactive/handlers/component-handler-slider.test.ts
@@ -0,0 +1,61 @@
+import { appState } from "../../models/app-state"
+import { toV3Id } from "../../utilities/codap-utils"
+import { DIComponentInfo } from "../data-interactive-types"
+import { diComponentHandler } from "./component-handler"
+import { getSharedModelManager } from "../../models/tiles/tile-environment"
+import { getGlobalValueManager } from "../../models/tiles/tile-environment"
+import { kSliderIdPrefix } from "../../components/slider/slider-registration"
+import { ISliderModel, isSliderModel } from "../../components/slider/slider-model"
+import { AnimationDirections, AnimationModes } from "../../components/slider/slider-types"
+
+
+describe("DataInteractive ComponentHandler Slider", () => {
+  const documentContent = appState.document.content!
+  const globalManager = getGlobalValueManager(getSharedModelManager(documentContent))
+  globalManager?.addValueSnapshot({ name: "global", value: 10 })
+  const handler = diComponentHandler
+
+  it("create slider works", async () => {
+    // Create slider with no value specified
+    expect(documentContent.tileMap.size).toBe(0)
+    const newValueResult = handler.create!({}, { type: "slider" })!
+    expect(newValueResult.success).toBe(true)
+    expect(documentContent.tileMap.size).toBe(1)
+    const newValueResultValues = newValueResult.values as DIComponentInfo
+    const newValueTile = documentContent.tileMap.get(toV3Id(kSliderIdPrefix, newValueResultValues.id!))!
+    expect(newValueTile).toBeDefined()
+    expect(isSliderModel(newValueTile.content)).toBe(true)
+
+    // Delete slider tile
+    const deleteResult = handler.delete!({ component: newValueTile })!
+    expect(deleteResult.success).toBe(true)
+    expect(documentContent.tileMap.size).toBe(0)
+
+    // Create slider with value and other options specified
+    const oldValueResult = handler.create!({}, {
+      type: "slider", globalValueName: "global", lowerBound: -100, upperBound: 100, animationDirection: 0, animationMode: 1
+    })
+    expect(oldValueResult.success).toBe(true)
+    expect(documentContent.tileMap.size).toBe(1)
+    const oldValueResultValues = oldValueResult.values as DIComponentInfo
+    const oldValueTile = documentContent.tileMap.get(toV3Id(kSliderIdPrefix, oldValueResultValues.id!))!
+    expect(oldValueTile).toBeDefined()
+    expect(isSliderModel(oldValueTile.content)).toBe(true)
+    const oldValueContent = oldValueTile.content as ISliderModel
+    expect(oldValueContent.globalValue.name).toBe("global")
+    expect(oldValueContent.axis.min).toBe(-100)
+    expect(oldValueContent.axis.max).toBe(100)
+    expect(oldValueContent.animationDirection).toBe(AnimationDirections[0])
+    expect(oldValueContent.animationMode).toBe(AnimationModes[1])
+
+    // Cannot create slider with duplicate global value
+    const duplicateResult = handler.create!({}, { type: "slider", globalValueName: "global" })
+    expect(duplicateResult.success).toBe(false)
+    expect(documentContent.tileMap.size).toBe(1)
+
+    // Cannot create slider with global value that hasn't been defined
+    const noValueResult = handler.create!({}, { type: "slider", globalValueName: "noGlobal" })
+    expect(noValueResult.success).toBe(false)
+    expect(documentContent.tileMap.size).toBe(1)
+  })
+})
