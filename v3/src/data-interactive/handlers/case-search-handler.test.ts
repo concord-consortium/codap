48013849a97da997153eacd92126a2bc54a88b8b
Teale Fristoe <fristoe@gmail.com>
2024-06-14 11:03:14 -0700

    Update test.

--- v3/src/data-interactive/handlers/case-search-handler.test.ts
+++ v3/src/data-interactive/handlers/case-search-handler.test.ts
@@ -9,7 +9,7 @@ describe("DataInteractive CaseSearchHandler", () => {
 
   it("get works", () => {
     const { dataset: dataContext, c2: collection } = setupTestDataset()
-    const cases = dataContext.getGroupsForCollection(collection.id)!
+    const cases = dataContext.getGroupsForCollection(collection.id)!.map(c => c.pseudoCase)
     const caseSearch = [cases[0], cases[2], cases[3]]
 
     expect(handler.get?.({ dataContext, collection }).success).toBe(false)
@@ -20,8 +20,8 @@ describe("DataInteractive CaseSearchHandler", () => {
     expect(result.success).toBe(true)
     const values = result.values as DIFullCase[]
     caseSearch.forEach((item, index) => {
-      expect(values[index].id).toBe(toV2Id(item.pseudoCase.__id__))
-      const itemIndex = dataContext.caseIndexFromID(item.childCaseIds[0])!
+      expect(values[index].id).toBe(toV2Id(item.__id__))
+      const itemIndex = dataContext.caseIndexFromID(dataContext.pseudoCaseMap.get(item.__id__)!.childCaseIds[0])!
       collection.attributes.forEach(attribute => {
         expect(attribute && values[index].values?.[attribute.name]).toBe(attribute?.value(itemIndex))
       })
