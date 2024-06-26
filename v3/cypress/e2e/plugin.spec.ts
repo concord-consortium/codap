b34500ba01d93d0ce07db1ebc274532fcfcc9667
Teale Fristoe <fristoe@gmail.com>
2024-06-26 10:29:15 -0700

    Fix basic plugin request cypress tests.

--- v3/cypress/e2e/plugin.spec.ts
+++ v3/cypress/e2e/plugin.spec.ts
@@ -27,7 +27,7 @@ context("codap plugins", () => {
     cy.log("Handle get attribute request")
     const cmd1 = `{
       "action": "get",
-      "resource": "dataContext[Mammals].collection[Mammals].attribute[Order]"
+      "resource": "dataContext[Mammals].collection[Cases].attribute[Order]"
     }`
     webView.sendAPITesterCommand(cmd1)
     webView.confirmAPITesterResponseContains(/"success":\s*true/)
@@ -36,7 +36,7 @@ context("codap plugins", () => {
     cy.log("Properly handles illegal actions")
     const cmd2 = `{
       "action": "fake",
-      "resource": "dataContext[Mammals].collection[Mammals].attribute[Order]"
+      "resource": "dataContext[Mammals].collection[Cases].attribute[Order]"
     }`
     webView.sendAPITesterCommand(cmd2, cmd1)
     webView.confirmAPITesterResponseContains(/"Unsupported action: fake\/attribute"/)
@@ -45,7 +45,7 @@ context("codap plugins", () => {
     cy.log("Handle update attribute hidden")
     const cmd3 = `{
       "action": "update",
-      "resource": "dataContext[Mammals].collection[Mammals].attribute[Order]",
+      "resource": "dataContext[Mammals].collection[Cases].attribute[Order]",
       "values": {
         "hidden": true
       }
@@ -58,7 +58,7 @@ context("codap plugins", () => {
     cy.log("Handle create attribute")
     const cmd4 =`{
       "action": "create",
-      "resource": "dataContext[Mammals].collection[Mammals].attribute",
+      "resource": "dataContext[Mammals].collection[Cases].attribute",
       "values": [
         {
           "name": "Heartrate"
@@ -73,7 +73,7 @@ context("codap plugins", () => {
     cy.log("handle delete attribute")
     const cmd5 = `{
       "action": "delete",
-      "resource": "dataContext[Mammals].collection[Mammals].attribute[Heartrate]"
+      "resource": "dataContext[Mammals].collection[Cases].attribute[Heartrate]"
     }`
     table.getAttributeHeader().contains("Heartrate").should("exist")
     webView.sendAPITesterCommand(cmd5, cmd4)
@@ -83,7 +83,7 @@ context("codap plugins", () => {
     cy.log("Finds the default dataset when no dataset is included")
     const cmd6 = `{
       "action": "get",
-      "resource": "collection[Mammals].attribute[Order]"
+      "resource": "collection[Cases].attribute[Order]"
     }`
     webView.sendAPITesterCommand(cmd6, cmd5)
     webView.confirmAPITesterResponseContains(/"success":\s*true/)
