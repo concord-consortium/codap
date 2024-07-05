cfa688c1c10848d5c66db421b8f4e2c32e7fc1d4
eireland <eireland@concord.org>
2024-07-05 14:52:54 -0700

    adds testids to menu items

--- v3/src/components/graph/components/camera-menu-list.tsx
+++ v3/src/components/graph/components/camera-menu-list.tsx
@@ -26,17 +26,25 @@ export const CameraMenuList = () => {
     <MenuList data-testid="graph-camera-menu-list">
       {hasBackgroundImage
         ? <>
-            <MenuItem onClick={handleRemoveBackgroundImage}>{t("DG.DataDisplayMenu.removeBackgroundImage")}</MenuItem>
+            <MenuItem onClick={handleRemoveBackgroundImage} data-testid="remove-background-image">
+              {t("DG.DataDisplayMenu.removeBackgroundImage")}
+            </MenuItem>
             {imageLocked
-              ? <MenuItem onClick={handleUnlockImage}>{t("DG.DataDisplayMenu.unlockImageFromAxes")}</MenuItem>
-              : <MenuItem onClick={handleLockImage}>{t("DG.DataDisplayMenu.lockImageToAxes")}</MenuItem>
+              ? <MenuItem onClick={handleUnlockImage} data-testid="unlock-image">
+                  {t("DG.DataDisplayMenu.unlockImageFromAxes")}
+                </MenuItem>
+              : <MenuItem onClick={handleLockImage} data-testid="lock-image">
+                  {t("DG.DataDisplayMenu.lockImageToAxes")}
+                </MenuItem>
             }
           </>
-        : <MenuItem onClick={handleAddBackgroundImage}>{t("DG.DataDisplayMenu.addBackgroundImage")}</MenuItem>
+        : <MenuItem onClick={handleAddBackgroundImage} data-testid="add-background-image">
+            {t("DG.DataDisplayMenu.addBackgroundImage")}
+          </MenuItem>
       }
-      <MenuItem>{t("DG.DataDisplayMenu.copyAsImage")}</MenuItem>
-      <MenuItem>{t("DG.DataDisplayMenu.exportPngImage")}</MenuItem>
-      <MenuItem>{t("DG.DataDisplayMenu.exportSvgImage")}</MenuItem>
+      <MenuItem data-testid="open-in-draw-tool">{t("DG.DataDisplayMenu.copyAsImage")}</MenuItem>
+      <MenuItem data-testid="export-png-image">{t("DG.DataDisplayMenu.exportPngImage")}</MenuItem>
+      <MenuItem data-testid="export-svg-image">{t("DG.DataDisplayMenu.exportSvgImage")}</MenuItem>
     </MenuList>
   )
 }
