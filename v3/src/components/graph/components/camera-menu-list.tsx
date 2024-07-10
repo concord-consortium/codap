e84440948baaf583f5df010ded84703b649bf328
eireland <eireland@concord.org>
2024-07-10 15:58:23 -0700

    Adds notifications for when graph camera menu items are clicked

--- v3/src/components/graph/components/camera-menu-list.tsx
+++ v3/src/components/graph/components/camera-menu-list.tsx
@@ -1,25 +1,51 @@
 import React, { useState } from "react"
-import { MenuItem, MenuList } from "@chakra-ui/react"
+import { MenuItem, MenuList, useToast } from "@chakra-ui/react"
 import { t } from "../../../utilities/translation/translate"
 
 export const CameraMenuList = () => {
   const [hasBackgroundImage, setHasBackgroundImage] = useState(false)
   const [imageLocked, setImageLocked] = useState(false)
+  const toast = useToast()
+  const handleMenuItemClick = (menuItem: string) => {
+    toast({
+      title: 'Menu item clicked',
+      description: `You clicked on ${menuItem}`,
+      status: 'success',
+      duration: 5000,
+      isClosable: true,
+    })
+  }
 
   const handleAddBackgroundImage = () => {
     setHasBackgroundImage(true)
+    handleMenuItemClick("Add Background Image clicked")
   }
 
   const handleRemoveBackgroundImage = () => {
     setHasBackgroundImage(false)
+    handleMenuItemClick("Remove Background Image clicked")
   }
 
   const handleUnlockImage = () => {
     setImageLocked(false)
+    handleMenuItemClick("Unlock Image clicked")
   }
 
   const handleLockImage = () => {
     setImageLocked(true)
+    handleMenuItemClick("Lock Image clicked")
+  }
+
+  const handleCopyImage = () => {
+    handleMenuItemClick("Copy Image clicked")
+  }
+
+  const handleExportPNG = () => {
+    handleMenuItemClick("Export PNG Image clicked")
+  }
+
+  const handleExportSVG = () => {
+    handleMenuItemClick("Export SVG Image clicked")
   }
 
   return (
@@ -42,9 +68,15 @@ export const CameraMenuList = () => {
             {t("DG.DataDisplayMenu.addBackgroundImage")}
           </MenuItem>
       }
-      <MenuItem data-testid="open-in-draw-tool">{t("DG.DataDisplayMenu.copyAsImage")}</MenuItem>
-      <MenuItem data-testid="export-png-image">{t("DG.DataDisplayMenu.exportPngImage")}</MenuItem>
-      <MenuItem data-testid="export-svg-image">{t("DG.DataDisplayMenu.exportSvgImage")}</MenuItem>
+      <MenuItem data-testid="open-in-draw-tool" onClick={handleCopyImage}>
+        {t("DG.DataDisplayMenu.copyAsImage")}
+      </MenuItem>
+      <MenuItem data-testid="export-png-image" onClick={handleExportPNG}>
+        {t("DG.DataDisplayMenu.exportPngImage")}
+      </MenuItem>
+      <MenuItem data-testid="export-svg-image" onClick={handleExportSVG}>
+        {t("DG.DataDisplayMenu.exportSvgImage")}
+      </MenuItem>
     </MenuList>
   )
 }
