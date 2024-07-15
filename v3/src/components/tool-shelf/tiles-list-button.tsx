4f7e6f373544808e3bd1a671236f40e2984048d5
eireland <eireland@concord.org>
2024-07-15 13:55:01 -0700

    Fixes bug where all tile show Web Page when there is no data context
    PR Fixes

--- v3/src/components/tool-shelf/tiles-list-button.tsx
+++ v3/src/components/tool-shelf/tiles-list-button.tsx
@@ -4,15 +4,21 @@ import { Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react"
 import OptionsIcon from "../../assets/icons/icon-options.svg"
 import { useDocumentContent } from "../../hooks/use-document-content"
 import { uiState } from "../../models/ui-state"
-import { IFreeTileLayout } from "../../models/document/free-tile-row"
+import { isFreeTileLayout } from "../../models/document/free-tile-row"
 import { t } from "../../utilities/translation/translate"
 import { kRightButtonBackground, ToolShelfButtonTag } from "./tool-shelf-button"
-import GraphIcon from "../../assets/icons/icon-graph.svg"
-import TableIcon from "../../assets/icons/icon-table.svg"
-import SliderIcon from "../../assets/icons/icon-slider.svg"
-import MapIcon from "../../assets/icons/icon-map.svg"
-import CalculatorIcon from "../../assets/icons/icon-calc.svg"
-import GuideIcon from "../../assets/icons/icon-guide.svg"
+import { getTileComponentIcon } from "../../models/tiles/tile-component-info"
+import { kSliderTileType } from "../slider/slider-defs"
+import { ITileModel } from "../../models/tiles/tile-model"
+import { isSliderModel } from "../slider/slider-model"
+import { kCalculatorTileType } from "../calculator/calculator-defs"
+import { isCalculatorModel } from "../calculator/calculator-model"
+import { kCaseTableTileType } from "../case-table/case-table-defs"
+import { isCaseTableModel } from "../case-table/case-table-model"
+import { kGraphTileType } from "../graph/graph-defs"
+import { isGraphContentModel } from "../graph/models/graph-content-model"
+import { kMapTileType } from "../map/map-defs"
+import { isMapContentModel } from "../map/models/map-content-model"
 import WebViewIcon from "../../assets/icons/icon-media-tool.svg"
 
 import "./tool-shelf.scss"
@@ -20,25 +26,40 @@ import "./tool-shelf.scss"
 export const TilesListShelfButton = observer(function TilesListShelfButton() {
   const documentContent = useDocumentContent()
   const tilesArr: any[] = []
-
-  documentContent?.tileMap.forEach((tile) => {
-    tilesArr.push(tile)
-  })
-
-  const tileTypeIconMap = [{type: "Graph", icon: <GraphIcon className="tile-list-icon"/>},
-                            {type: "CaseTable", icon: <TableIcon className="tile-list-icon"/>},
-                            {type: "CodapSlider", icon: <SliderIcon className="tile-list-icon"/>},
-                            {type: "Map", icon: <MapIcon className="tile-list-icon"/>},
-                            {type: "Calculator", icon: <CalculatorIcon className="tile-list-icon"/>},
-                            {type: "Guide", icon: <GuideIcon className="tile-list-icon"/>},
-                            {type: "CodapWebView", icon: <WebViewIcon className="tile-list-icon"/>}
-                          ]
+  if (documentContent?.tileMap) {
+    tilesArr.push(...Array.from(documentContent.tileMap.values()))
+  }
 
   const handleSelectTile = (tileId: string) => {
     uiState.setFocusedTile(tileId)
     const tileRow = documentContent?.findRowContainingTile(tileId)
-    const tileLayout = tileRow?.getTileLayout(tileId) as IFreeTileLayout
-    tileLayout.setMinimized(false)
+    const tileLayout = tileRow?.getTileLayout(tileId)
+    isFreeTileLayout(tileLayout) && tileLayout.setMinimized(false)
+  }
+
+  const getTitle = (tile: ITileModel) => {
+    const { title } = tile || {}
+    const tileName = () => {
+      let model
+      switch (tile.content.type) {
+        case kSliderTileType:
+          model = isSliderModel(tile.content) ? tile.content : undefined
+          return model?.name || t("DG.DocumentController.sliderTitle")
+        case kCalculatorTileType:
+          model = isCalculatorModel(tile.content) ? tile.content : undefined
+          return model?.name || t("DG.DocumentController.calculatorTitle")
+        case kCaseTableTileType:
+          model = isCaseTableModel(tile.content) ? tile.content : undefined
+          return model?.data?.title || t("DG.DocumentController.caseTableTitle")
+        case kGraphTileType:
+          model = isGraphContentModel(tile.content) ? tile.content : undefined
+          return tile.title || t("DG.DocumentController.graphTitle")
+        case kMapTileType:
+          model = isMapContentModel(tile.content) ? tile.content : undefined
+          return tile.title || t("DG.DocumentController.mapTitle")
+      }
+    }
+    return title || tileName() || t("DG.WebView.defaultTitle")
   }
 
   return (
@@ -57,13 +78,15 @@ export const TilesListShelfButton = observer(function TilesListShelfButton() {
           />
         </MenuButton>
         <MenuList>
-          {tilesArr.map((tile) => {
-            console.log("tile", tile.title)
+          {tilesArr?.map((tile) => {
+            const Icon = getTileComponentIcon(tile?.content.type)
+            const title = getTitle(tile)
             return (
-            <MenuItem key={tile.id} data-testid={`tiles-list-item ${tile.id}`} onClick={()=>handleSelectTile(tile.id)}>
-                {tileTypeIconMap.find((iconMap) => iconMap.type === tile.content.type)?.icon}
-                {tile.title ?? t("DG.WebView.defaultTitle")}
-            </MenuItem>
+              <MenuItem key={tile?.id} data-testid={`tiles-list-item ${tile.id}`}
+                  onClick={()=>handleSelectTile(tile.id)}>
+                {(Icon && <Icon className="tile-list-icon"/>) || <WebViewIcon className="tile-list-icon" />}
+                {title}
+              </MenuItem>
             )
           })}
         </MenuList>
