3a59eefe75e0464199b17c1cb1a9016c66afd6c4
pjanik <janikpiotrek@gmail.com>
2024-07-22 21:44:14 +0200

    feat: implement most of date functions, extract createDate/ and convertToDate, add tests [PT-187799270]

--- v3/src/models/formula/functions/function-utils.test.ts
+++ v3/src/models/formula/functions/function-utils.test.ts
@@ -1,35 +1,5 @@
-import { isValueNonEmpty, isNumber, isValueTruthy, equal, evaluateNode } from "./function-utils"
+import { isValueTruthy, equal, evaluateNode } from "./function-utils"
 import { parse } from "mathjs"
-
-describe("isValueNonEmpty", () => {
-  it("should return false for empty values", () => {
-    expect(isValueNonEmpty("")).toBe(false)
-    expect(isValueNonEmpty(null)).toBe(false)
-    expect(isValueNonEmpty(undefined)).toBe(false)
-  })
-
-  it("should return true for non-empty values", () => {
-    expect(isValueNonEmpty("non-empty")).toBe(true)
-    expect(isValueNonEmpty(0)).toBe(true)
-    expect(isValueNonEmpty(false)).toBe(true)
-  })
-})
-
-describe("isNumber", () => {
-  it("should return true for numbers", () => {
-    expect(isNumber(0)).toBe(true)
-    expect(isNumber("0")).toBe(true)
-    expect(isNumber(1.23)).toBe(true)
-    expect(isNumber("1.23")).toBe(true)
-  })
-
-  it("should return false for non-numbers", () => {
-    expect(isNumber("")).toBe(false)
-    expect(isNumber("abc")).toBe(false)
-    expect(isNumber(null)).toBe(false)
-    expect(isNumber(undefined)).toBe(false)
-  })
-})
 
 describe("isValueTruthy", () => {
   it("should return true for truthy values", () => {
