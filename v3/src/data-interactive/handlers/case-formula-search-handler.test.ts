bb32978600b1fa7a8742a2998f572532d3e610a1
Teale Fristoe <fristoe@gmail.com>
2024-07-24 10:00:12 -0700

    Add case-formula-search-handler test.

--- v3/src/data-interactive/handlers/case-formula-search-handler.test.ts
+++ v3/src/data-interactive/handlers/case-formula-search-handler.test.ts
@@ -1,25 +1,25 @@
 import { toV2Id } from "../../utilities/codap-utils"
 import { DIFullCase } from "../data-interactive-types"
-import { diCaseSearchHandler } from "./case-search-handler"
+import { diCaseFormulaSearchHandler } from "./case-formula-search-handler"
 import { setupTestDataset } from "./handler-test-utils"
 
 
-describe("DataInteractive CaseSearchHandler", () => {
-  const handler = diCaseSearchHandler
+describe("DataInteractive CaseFormulaSearchHandler", () => {
+  const handler = diCaseFormulaSearchHandler
 
   it("get works", () => {
     const { dataset: dataContext, c2: collection } = setupTestDataset()
     const cases = dataContext.getGroupsForCollection(collection.id).map(c => c.groupedCase)
-    const caseSearch = [cases[0], cases[2], cases[3]]
+    const caseFormulaSearch = [cases[0], cases[2], cases[3]]
 
     expect(handler.get?.({ dataContext, collection }).success).toBe(false)
-    expect(handler.get?.({ dataContext, caseSearch }).success).toBe(false)
-    expect(handler.get?.({ collection, caseSearch }).success).toBe(false)
+    expect(handler.get?.({ dataContext, caseFormulaSearch }).success).toBe(false)
+    expect(handler.get?.({ collection, caseFormulaSearch }).success).toBe(false)
 
-    const result = handler.get!({ dataContext, collection, caseSearch })
+    const result = handler.get!({ dataContext, collection, caseFormulaSearch })
     expect(result.success).toBe(true)
     const values = result.values as DIFullCase[]
-    caseSearch.forEach((item, index) => {
+    caseFormulaSearch.forEach((item, index) => {
       expect(values[index].id).toBe(toV2Id(item.__id__))
       const itemIndex = dataContext.getItemIndex(dataContext.caseInfoMap.get(item.__id__)!.childItemIds[0])!
       collection.attributes.forEach(attribute => {
