98a5c3da71c5776b16d17f1bd503248bbba8b301
Teale Fristoe <fristoe@gmail.com>
2024-08-01 16:51:25 -0700

    Remove outdated references to cid.

--- v3/src/models/data/v2-model.test.ts
+++ v3/src/models/data/v2-model.test.ts
@@ -67,7 +67,6 @@ describe("V2Model", () => {
     expect(isV2ModelSnapshot(m)).toBe(true)
     m = {
       id: 1,
-      cid: "cid",
       name: "name",
       title: "title"
     } as V2ModelStorage
@@ -92,7 +91,6 @@ describe("V2Model", () => {
     v2m = {
       id: 1,
       guid: 1,
-      cid: "cid",
       name: "name",
       title: "title"
     }
