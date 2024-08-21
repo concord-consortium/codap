b7e0b7a1e5602e469afb2a20bf380394408520ae
Teale Fristoe <fristoe@gmail.com>
2024-08-21 16:14:08 -0700

    Include correct title in get dataContextList requests.

---
+++ v3/src/data-interactive/handlers/data-context-list-handler.test.ts
@@ -0,0 +1,26 @@
+import { DIDataContext } from "../data-interactive-types"
+import { diDataContextListHandler } from "./data-context-list-handler"
+import { setupTestDataset } from "./handler-test-utils"
+import { toV2Id } from "../../utilities/codap-utils"
+
+describe("DataInteractive DataContextHandler", () => {
+  const handler = diDataContextListHandler
+
+  it("get works as expected", () => {
+    const { dataset } = setupTestDataset()
+    const title = "dataset title"
+    dataset.setTitle(title)
+
+    expect(handler.get?.({}).success).toBe(false)
+
+    const result = handler.get?.({ dataContextList: [dataset] })!
+    expect(result.success).toBe(true)
+    const values = result.values as DIDataContext[]
+    expect(values.length).toBe(1)
+    const diDataContext = values[0]
+    expect(diDataContext.name).toBe(dataset.name)
+    expect(diDataContext.title).toBe(dataset._title)
+    expect(diDataContext.guid).toBe(toV2Id(dataset.id))
+    expect(diDataContext.id).toBe(toV2Id(dataset.id))
+  })
+})
