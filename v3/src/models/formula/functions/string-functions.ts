fc82c5f52f6a61cf30149cd6a470ba70c6fb1f60
Teale Fristoe <fristoe@gmail.com>
2024-08-23 17:19:58 -0700

    Implement wordListMatches function.

--- v3/src/models/formula/functions/string-functions.ts
+++ v3/src/models/formula/functions/string-functions.ts
@@ -1,7 +1,10 @@
-import { t } from "../../../utilities/translation/translate"
-import { valueToString } from "../../../utilities/data-utils"
-import { FValue } from "../formula-types"
 import escapeStringRegexp from "escape-string-regexp"
+import { MathNode } from "mathjs"
+import { valueToString } from "../../../utilities/data-utils"
+import { t } from "../../../utilities/translation/translate"
+import { CurrentScope, DisplayNameMap, FValue, ILookupDependency, LookupStringConstantArg } from "../formula-types"
+import { isConstantStringNode } from "../utils/mathjs-utils"
+import { evaluateNode, getRootScope } from "./function-utils"
 
 export const stringFunctions = {
   // beginsWith(text, prefix) Returns true if text begins with prefix, otherwise false.
@@ -229,9 +232,51 @@ export const stringFunctions = {
   // ends with "/" it is treated as a regular expression and searched for with case sensitivity.
   wordListMatches: {
     numOfRequiredArguments: 3,
-    evaluate: (...args: FValue[]) => {
-      // todo: Implement this function
-      return "NYI"
+    evaluateRaw: (args: MathNode[], _mathjs: any, currentScope: CurrentScope) => {
+      const scope = getRootScope(currentScope)
+      const functionName = "wordListMatches"
+      const numOfReqArgs = stringFunctions.wordListMatches.numOfRequiredArguments
+      if (args.length !== numOfReqArgs) {
+        throw new Error(t("DG.Formula.FuncArgsErrorPlural.description", { vars: [ functionName, numOfReqArgs ] }))
+      }
+      [1, 2].forEach(i => {
+        if (!isConstantStringNode(args[i])) {
+          throw new Error(t("V3.formula.error.stringConstantArg", { vars: [ functionName, i + 1 ] }))
+        }
+      })
+      const [_textRefArg, dataSetTitleArg, wordAttributeNameArg, ratingAttributeNameArg] =
+        args as LookupStringConstantArg[]
+      const text = evaluateNode(args[0], scope) as string
+      const dataSetTitle = dataSetTitleArg?.value || ""
+      const dataSet = scope.getDataSetByTitle(dataSetTitle)
+      if (!dataSet) {
+        throw new Error(t("DG.Formula.LookupDataSetError.description", { vars: [ dataSetTitle ] }))
+      }
+
+      const wordAttributeName = wordAttributeNameArg?.value
+      const wordAttribute = wordAttributeName ? dataSet.getAttributeByName(wordAttributeName) : undefined
+      if (!wordAttribute) {
+        throw new Error(t("DG.Formula.LookupAttrError.description",
+          { vars: [ wordAttributeName, dataSet.title || "" ] }))
+      }
+
+      const ratingAttributeName = ratingAttributeNameArg?.value
+      const ratingAttribute = ratingAttributeName ? dataSet.getAttributeByName(ratingAttributeName) : undefined
+
+      const wordRatingMap: Record<string, number> = {}
+      wordAttribute.strValues.forEach((word, index) => wordRatingMap[word] = ratingAttribute?.numeric(index) ?? 1)
+
+      let result = 0
+      wordAttribute.strValues.forEach(word => {
+        const isRegEx = word.startsWith("/") && word.endsWith("/")
+        const pattern = isRegEx ? word.substring(1, word.length - 1) : `\\b${word}\\b`
+        const flags = isRegEx ? "g" : "gi"
+        const regEx = new RegExp(pattern, flags)
+        const match = text.match(regEx)
+        result += wordRatingMap[word] * (match ? match.length : 0)
+      })
+
+      return result
     }
   },
 }
