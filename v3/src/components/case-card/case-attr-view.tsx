034768b26a975bf374203b9deffe35f725dd596d
lublagg <lvblagg@gmail.com>
2024-08-29 12:53:46 -0400

    Checkpoint: can edit string values.

--- v3/src/components/case-card/case-attr-view.tsx
+++ v3/src/components/case-card/case-attr-view.tsx
@@ -57,6 +57,53 @@ export const CaseAttrView = observer(function CaseAttrView (props: ICaseAttrView
     setIsEditing(false)
   }
 
+  const handleClick = () => {
+    setIsEditing(!isEditing)
+  }
+
+  const handleBlur = (newValue: string) => {
+    const caseToUpdate: ICase = {__id__: caseId, [attrId]: newValue}
+    const undoStringKey = "DG.Undo.caseTable.editCellValue"
+    const redoStringKey = "DG.Redo.caseTable.editCellValue"
+    // let oldCaseIds = new Set(collection?.caseIds ?? [])
+    // let updatedCaseIds: string[] = []
+
+    data?.applyModelChange(
+      () => {
+        setCaseValuesWithCustomUndoRedo(data, [caseToUpdate])
+        // if (collection?.id === data.childCollection.id) {
+        //   // The child collection's case ids are persistent, so we can just use the casesToUpdate to
+        //   // determine which case ids to use in the updateCasesNotification
+        //   updatedCaseIds = casesToUpdate.map(aCase => aCase.__id__)
+        // } else {
+        //   // Other collections have cases whose ids change when values change due to updated case grouping,
+        //   // so we have to check which case ids were not present before updating to determine which case ids
+        //   // to use in the updateCasesNotification
+        //   collection?.caseIds.forEach(caseId => {
+        //     if (!oldCaseIds.has(caseId)) updatedCaseIds.push(caseId)
+        //   })
+        // }
+        // oldCaseIds = new Set(collection?.caseIds ?? [])
+      },
+      {
+        notify: () => {
+          const notifications: any = []
+          // if (updatedCaseIds.length > 0) {
+          //   const updatedCases = updatedCaseIds.map(caseId => data.caseInfoMap.get(caseId))
+          //     .filter(caseGroup => !!caseGroup)
+          //     .map(caseGroup => caseGroup.groupedCase)
+          //   notifications.push(updateCasesNotification(data, updatedCases))
+          // }
+          // if (newCaseIds.length > 0) notifications.push(createCasesNotification(newCaseIds, data))
+          return notifications
+        },
+        undoStringKey,
+        redoStringKey
+      }
+    )
+    setIsEditing(false)
+  }
+
   return (
     <tr className="case-card-attr" data-testid="case-card-attr">
       <td className="case-card-attr-name" data-testid="case-card-attr-name">
