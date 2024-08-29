24594e71ea24a94b712af7a4a35b73cc172cdb84
eireland <eireland@concord.org>
2024-08-29 08:55:40 -0500

    Initial plugin logging. Tested with Sampler

--- v3/src/data-interactive/handlers/log-message-handler.ts
+++ v3/src/data-interactive/handlers/log-message-handler.ts
@@ -1,8 +1,24 @@
+import { LoggableObject, logMessageWithReplacement } from "../../lib/log-message"
 import { registerDIHandler } from "../data-interactive-handler"
-import { DIHandler, diNotImplementedYet } from "../data-interactive-types"
+import { DIHandler, DILogMessage, DIResources, DIValues,  } from "../data-interactive-types"
 
 export const diLogMessageHandler: DIHandler = {
-  notify: diNotImplementedYet
+  notify(resources: DIResources, values?: DIValues) {
+    const webviewModel = resources.interactiveFrame?.content
+    const {formatStr, replaceArgs} = values as DILogMessage
+    const message = formatStr ?? ""
+    const argsArray = replaceArgs != null
+      ? Array.isArray(replaceArgs) ? replaceArgs : [replaceArgs]
+      : undefined
+    const args: LoggableObject = {}
+    argsArray?.forEach((value: any, index: number) => {
+      args[index.toString()] = value
+    })
+    webviewModel?.applyModelChange(()=>{},
+      { log: logMessageWithReplacement(message, args)}
+    )
+    return {success: true}
+  }
 }
 
 registerDIHandler("logMessage", diLogMessageHandler)
