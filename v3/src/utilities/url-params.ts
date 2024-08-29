521ca375274ea62d701c05d2bc0ed72c82f81481
Kirk Swenson <kswenson@users.noreply.github.com>
2024-08-28 19:10:15 -0700

    feat: support `url` url parameter (#1440)

--- v3/src/utilities/url-params.ts
+++ v3/src/utilities/url-params.ts
@@ -20,11 +20,96 @@ export function removeSearchParams(paramsToRemove: string[]) {
   window.history.pushState({path: newUrl}, "", newUrl)
 }
 
-export let urlParams = queryString.parse(getSearchParams())
+export interface UrlParams {
+  /*
+   * For testing -- when present brings up a default set of tiles (table, graph, calculator, slider, text).
+   * value: ignored
+   */
+  dashboard?: string | null
+  /*
+   * [v2] Loads the specified url as a data interactive/plugin tile.
+   * value: url string
+   */
+  di?: string | null
+  /*
+   * [v2] When present enables the gaussian fit feature of the normal curve adornment.
+   * value: ignored
+   */
+  gaussianFit?: string | null
+  /*
+   * [v2] When present enables the informal confidence interval on the box plot adornment.
+   * value: ignored
+   */
+  ICI?: string | null
+  /*
+   * [v2] Specifies the default locale, overriding the browser default.
+   * value: locale string, e.g. `en-US` or `es`
+   */
+  lang?: string | null
+  /*
+   * [v2] Alternate means to specify default locale.
+   * v2 comment: "Allow override of lang query param"(?)
+   * value: locale string, e.g. `en-US` or `es`
+   */
+  "lang-override"?: string | null
+  /*
+   * Specifies the type of tile container. Defaults to "free", which is the standard free layout used by CODAP.
+   * Limited/provisional support for "mosaic" layout, in which tiles are connected and cannot overlap.
+   * Experimental flag for possible future development direction.
+   * value: "free" (default) | "mosaic"
+   */
+  layout?: string | null
+  /*
+   * [v3] Specifies a url that can be used to add additional items to the Plugins menu.
+   * See useRemotePluginsConfig() hook for more details, including format of remote document.
+   * value: url string
+   */
+  morePlugins?: string | null
+  /*
+   * For testing -- when present adds the DnDKit MouseSensor in addition to the PointerSensor for drag/drop.
+   * The MouseSensor has proven more amenable for testing drag/drop in cypress tests.
+   * value: ignored
+   */
+  mouseSensor?: string | null
+  /*
+   * For testing -- when present disables animation, which can be useful in automated tests.
+   * value: ignored
+   */
+  noComponentAnimation?: string | null
+  /*
+   * For testing -- when present disables data tips, which can be useful in automated tests.
+   * value: ignored
+   */
+  noDataTips?: string | null
+  /*
+   * For testing -- specifies a built-in sample document to be loaded on startup.
+   * Useful for automated tests; often combined with `dashboard`.
+   * value: "abalone" | "cats" | "coasters" | "colors" | "four" | "mammals"
+   */
+  sample?: string | null
+  /*
+   * Primarily for testing -- specifies whether scrolling should be "smooth" (default) or not.
+   * Disable smooth scrolling by setting it to "auto", which can be useful in automated tests.
+   * value: "auto" | "smooth" (default)
+   */
+  scrollBehavior?: string | null
+  /*
+   * For testing -- when present along with `dashboard`, only a single table will be created by default.
+   * value: ignored
+   */
+  tableOnly?: string | null
+  /*
+   * [v2] Specifies the url of a document to load on application launch.
+   * value: url string
+   */
+  url?: string | null
+}
+
+export let urlParams: UrlParams = queryString.parse(getSearchParams())
 
 export const setUrlParams = (search: string) => urlParams = queryString.parse(search)
 
 // remove developer-convenience url params
 export function removeDevUrlParams() {
-  removeSearchParams(["sample", "dashboard"])
+  removeSearchParams(["dashboard", "sample", "tableOnly"])
 }
