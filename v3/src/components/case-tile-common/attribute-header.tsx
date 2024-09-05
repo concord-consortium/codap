0d3f0b234c7e7efe8726044a461e00cf69313152
lublagg <lvblagg@gmail.com>
2024-09-05 11:45:48 -0400

    Add drag overlay to case card and mouse styling to attribute header.

--- v3/src/components/case-tile-common/attribute-header.tsx
+++ v3/src/components/case-tile-common/attribute-header.tsx
@@ -70,6 +70,11 @@ export const AttributeHeader = observer(function ColumnHeader({
 
   useEffect(() => {
     onCloseMenuRef.current?.()
+    if (dragging) {
+      document.body.style.cursor = "grabbing"
+    } else {
+      document.body.style.cursor = "default"
+    }
   }, [dragging])
 
   useEffect(() => {
