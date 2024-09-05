716732840279113c1de7fed81ecf7c8ab0e1c94d
William Finzer <wfinzer@concord.org>
2024-09-05 16:24:51 -0700

    [#188200067] Bug fix: **Bar Chart** Compresses to Top When Hiding Unselected Cases in Mammals Dataset
    * The problem was that when all cases were hidden, the Count axis min and max got set to NaN. We fix by not allowing this to happen. On encountering NaN, we leave the min or max alone.
    * With an intact Count axis, the bar chart is able to correctly compute the placement of the bars.

--- v3/src/components/axis/models/axis-model.ts
+++ v3/src/components/axis/models/axis-model.ts
@@ -112,8 +112,8 @@ export const BaseNumericAxisModel = AxisModel
       } else if ((min < 0) && (Math.abs(max) < Math.abs(min / snapFactor))) {
         max = 0
       }
-      self.min = min
-      self.max = max
+      if (isFinite(min)) self.min = min
+      if (isFinite(max)) self.max = max
       self.dynamicMin = undefined
       self.dynamicMax = undefined
     },
