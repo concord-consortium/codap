2e056deed75cb35056206fe6ba3a710e7594421f
Ethan McElroy <emcelroy@concord.org>
2024-09-10 16:59:51 -0400

    feat: case card drag within level (PT-188001450) (#1469)
    * Checkpoint: can edit string values.
    
    * Checkpoint: edit attribute names in case card.
    
    * checkpoint: move attribute menu, hide attributes, style tweaks
    
    * chore: refactor ColumnHeader => AttributeHeader
    
    * chore: update default card width, input and table style adjustments
    
    * chore: add attributes and cases, refactor, add tests
    
    * chore: code review suggestions
    
    * Add drag overlay to case card and mouse styling to attribute header.
    
    * chore: use pure CSS for cursor styling
    
    * Add drag overlay to case card and mouse styling to attribute header.
    
    * Checkpoint: dragging attributes within collection.
    
    * feat: add drop target divider at top of attribute list
    
    * fix: width of dragged case card attribute
    
    * refactor: common header divider
    
    * chore: refactor to eliminate case-table/case-card-specific references in common code
    
    * fix: remove unnecessary call to createCasesNotification
    
    * chore: replace code with new shared `applyCaseValueChanges`
    
    * Feat: user can drag between levels to create new hierarchy (PT-188001450)
    
    * Revert "Feat: user can drag between levels to create new hierarchy (PT-188001450)"
    
    This reverts commit cf096af7ea6db73930c5f252a84a150519dd2ebe.
    
    ---------
    
    Co-authored-by: lublagg <lvblagg@gmail.com>
    Co-authored-by: Kirk Swenson <kswenson@users.noreply.github.com>

--- v3/src/components/case-card/case-attrs-view.tsx
+++ v3/src/components/case-card/case-attrs-view.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useCallback, useRef, useState } from "react"
 import { observer } from "mobx-react-lite"
 import { IGroupedCase } from "../../models/data/data-set-types"
 import { CaseAttrView } from "./case-attr-view"
@@ -6,6 +6,9 @@ import { IValueType } from "../../models/data/attribute"
 import { ICollectionModel } from "../../models/data/collection"
 import { useCaseCardModel } from "./use-case-card-model"
 import { getSharedCaseMetadataFromDataset } from "../../models/shared/shared-data-utils"
+import { AttributeHeader } from "../case-tile-common/attribute-header"
+import { AttributeHeaderDivider } from "../case-tile-common/attribute-header-divider"
+import { kIndexColumnKey } from "../case-tile-common/case-tile-types"
 
 import "./case-attrs-view.scss"
 
@@ -14,15 +17,45 @@ interface ICaseAttrsViewProps {
   collection?: ICollectionModel
 }
 
+function getDividerBounds(containerBounds: DOMRect, cellBounds: DOMRect) {
+  const kCardCellWidthOffset = 5
+  const kCardCellHeight = 25
+  return {
+    top: cellBounds.bottom - containerBounds.top + kCardCellHeight,
+    left: cellBounds.left - containerBounds.left,
+    width: containerBounds.width - cellBounds.left - containerBounds.left - kCardCellWidthOffset,
+    height: 6
+  }
+}
+
 export const CaseAttrsView = observer(function CaseAttrsView({caseItem, collection}: ICaseAttrsViewProps) {
   const data = useCaseCardModel()?.data
+  const contentRef = useRef<HTMLDivElement | null>(null)
+  const [, setCellElt] = useState<HTMLElement | null>(null)
   const values: IValueType[] = collection?.attributes.map(attr => {
     return attr?.id && data?.getValue(caseItem.__id__, attr.id)
   }) ?? []
 
+  const handleSetHeaderContentElt = useCallback((contentElt: HTMLDivElement | null) => {
+    contentRef.current = contentElt
+    const _cellElt: HTMLElement | null = contentRef.current?.closest(".case-card-attr") ?? null
+    setCellElt(_cellElt)
+    return _cellElt
+  }, [])
+
   return (
     <table className="case-card-attrs fadeIn" data-testid="case-card-attrs">
       <tbody>
+        <tr className="case-card-attr index-row">
+          <td colSpan={2}>
+            <AttributeHeader
+              attributeId={kIndexColumnKey}
+              getDividerBounds={getDividerBounds}
+              HeaderDivider={AttributeHeaderDivider}
+              onSetHeaderContentElt={handleSetHeaderContentElt}
+            />
+          </td>
+        </tr>
         {collection?.attributes.map((attr, index: number) => {
             const metadata = data && getSharedCaseMetadataFromDataset(data)
             if (!attr || metadata?.isHidden(attr.id)) return null
@@ -35,6 +68,8 @@ export const CaseAttrsView = observer(function CaseAttrsView({caseItem, collecti
                 name={attr.name}
                 value={values[index]}
                 unit={attr.units}
+                getDividerBounds={getDividerBounds}
+                onSetContentElt={handleSetHeaderContentElt}
               />
             )
           })
