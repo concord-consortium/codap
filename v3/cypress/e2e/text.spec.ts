9ea7f6a05d54314b010e5f94de8a8bf5386db7d4
nstclair-cc <20171905+nstclair-cc@users.noreply.github.com>
2024-09-30 17:34:28 -0700

    skip flaky text tile test

--- v3/cypress/e2e/text.spec.ts
+++ v3/cypress/e2e/text.spec.ts
@@ -11,26 +11,26 @@ context("Text tile", () => {
       cy.visit(url)
       cy.wait(1000) // Ensuring the page and components are fully loaded.
   })
-  it("updates text title with undo/redo", () => {
+  // this test has become flaky, skipping for now
+  // PT-#188358092
+  it.skip("updates text title with undo/redo", () => {
     const newTextTileName = "My Text"
     c.getComponentTitle(kTextTileTestId).should("have.text", textDefaultTitle)
     c.changeComponentTitle(kTextTileTestId, newTextTileName)
     c.getComponentTitle(kTextTileTestId).should("have.text", newTextTileName)
 
-    // this part of the test has become flaky, commenting out for now.
+    cy.log("Check update text title with undo/redo")
+    // Undo title change
+    cy.wait(100)
+    toolbar.getUndoTool().click()
+    cy.wait(100)
+    c.getComponentTitle(kTextTileTestId).should("have.text", textDefaultTitle)
 
-    // cy.log("Check update text title with undo/redo")
-    // // Undo title change
-    // cy.wait(100)
-    // toolbar.getUndoTool().click()
-    // cy.wait(100)
-    // c.getComponentTitle(kTextTileTestId).should("have.text", textDefaultTitle)
-
-    // // Redo title change
-    // toolbar.getRedoTool().should("be.enabled")
-    // toolbar.getRedoTool().click()
-    // cy.wait(100)
-    // c.getComponentTitle(kTextTileTestId).should("have.text", newTextTileName)
+    // Redo title change
+    toolbar.getRedoTool().should("be.enabled")
+    toolbar.getRedoTool().click()
+    cy.wait(100)
+    c.getComponentTitle(kTextTileTestId).should("have.text", newTextTileName)
   })
   it("close text tile from close button with undo/redo", () => {
     c.closeComponent(kTextTileTestId)
