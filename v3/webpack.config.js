718dad938771327ff50fea5c71293eb4756587f8
Scott Cytacki <scytacki@concord.org>
2024-10-09 12:28:11 -0400

    webpack cache is making no difference for Cypress
    I think the issue is because Cypress's internal caching is overwriting the webpack cache.

--- v3/webpack.config.js
+++ v3/webpack.config.js
@@ -73,12 +73,10 @@ module.exports = (env, argv) => {
       path: path.resolve(__dirname, 'dist'),
       filename: 'assets/index.[contenthash].js',
     },
-    infrastructureLogging: { debug: true },
     cache: {
       buildDependencies: {
         config: [__filename],
       },
-      profile: true,
       type: 'filesystem',
     },
     performance: { hints: false },
