a74ac430efac94fe395723230d2cf9dabe9cb648
Teale Fristoe <fristoe@gmail.com>
2024-10-09 13:17:22 -0700

    Finish renaming webViewId variables.

--- v3/src/models/document/create-document-model.ts
+++ v3/src/models/document/create-document-model.ts
@@ -62,8 +62,8 @@ export const createDocumentModel = (snapshot?: IDocumentModelSnapshot) => {
     }
 
     // configure notifications
-    fullEnvironment.notify = function(message: DIMessage, callback: iframePhone.ListenerCallback, webViewId?: string) {
-      document.content?.broadcastMessage(message, callback, webViewId)
+    fullEnvironment.notify = function(message: DIMessage, callback: iframePhone.ListenerCallback, targetTileId?: string) {
+      document.content?.broadcastMessage(message, callback, targetTileId)
     }
 
     return document
--- v3/src/models/tiles/tile-environment.ts
+++ v3/src/models/tiles/tile-environment.ts
@@ -12,7 +12,7 @@ export interface ITileEnvironment {
   sharedModelManager?: ISharedModelManager
   formulaManager?: FormulaManager
   log?: (message: ILogMessage) => void
-  notify?: (message: DIMessage, callback: iframePhone.ListenerCallback, webViewId?: string) => void
+  notify?: (message: DIMessage, callback: iframePhone.ListenerCallback, targetTileId?: string) => void
 }
 
 export function getTileEnvironment(node?: IAnyStateTreeNode) {
