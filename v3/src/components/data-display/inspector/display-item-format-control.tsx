06ddf284f40f3411b2671371167f63e5cbe3d245
eireland <eireland@concord.org>
2024-10-17 22:15:07 -0700

    Adds color swatch palette and color picker input to categorical legend brush menu

--- v3/src/components/data-display/inspector/display-item-format-control.tsx
+++ v3/src/components/data-display/inspector/display-item-format-control.tsx
@@ -1,12 +1,13 @@
-import React, {ReactElement, useRef} from "react"
+import React, {ReactElement, useCallback, useRef, useState} from "react"
 import {observer} from "mobx-react-lite"
-import {
-  Checkbox, Flex, FormControl, FormLabel, Input, Slider, SliderThumb, SliderTrack
+import {Button, ButtonGroup, Checkbox, Flex, FocusLock, FormControl, FormLabel, Input, Popover, PopoverBody,
+  PopoverContent, PopoverTrigger, Portal, Slider, SliderThumb, SliderTrack, Spacer,
 } from "@chakra-ui/react"
 import {IDataConfigurationModel} from "../models/data-configuration-model"
 import {IDisplayItemDescriptionModel} from "../models/display-item-description-model"
-import {missingColor} from "../../../utilities/color-utils"
+import {missingColor, kellyColors} from "../../../utilities/color-utils"
 import {t} from "../../../utilities/translation/translate"
+import { ColorPicker } from "../../case-tile-common/color-picker"
 
 import "./inspector-panel.scss"
 
@@ -29,15 +30,19 @@ export const DisplayItemFormatControl = observer(function PointFormatControl(pro
   const attrType = dataConfiguration?.dataset?.attrFromID(legendAttrID ?? "")?.type
   const categoriesRef = useRef<string[] | undefined>()
   categoriesRef.current = dataConfiguration?.categoryArrayForAttrRole('legend')
+  const [openPopover, setOpenPopover] = useState<string | null>(null);
 
-  const handlePointColorChange = (color: string) => {
-    displayItemDescription.applyModelChange(() => {
-      displayItemDescription.setPointColor(color)
-    },  {
-      undoStringKey: "DG.Undo.graph.changePointColor",
-      redoStringKey: "DG.Redo.graph.changePointColor",
-      log: "Changed point color"
-    })
+  // const handlePointColorChange = (color: string) => {
+  //   displayItemDescription.applyModelChange(() => {
+  //     displayItemDescription.setPointColor(color)
+  //   },  {
+  //     undoStringKey: "DG.Undo.graph.changePointColor",
+  //     redoStringKey: "DG.Redo.graph.changePointColor",
+  //     log: "Changed point color"
+  //   })
+  // }
+  const handleSwatchClick = (cat: string) => {
+    setOpenPopover(openPopover === cat ? null : cat)
   }
 
   const catPointColorSettingArr: ReactElement[] = []
@@ -45,9 +50,17 @@ export const DisplayItemFormatControl = observer(function PointFormatControl(pro
     catPointColorSettingArr.push(
       <Flex direction="row" key={cat} className="palette-row color-picker-row cat-color-picker">
         <FormLabel className="form-label color-picker">{cat}</FormLabel>
-        <Input type="color" className="color-picker-thumb categorical"
-               value={dataConfiguration?.getLegendColorForCategory(cat) || missingColor}
-               onChange={e => handlePointColorChange(e.target.value)}/>
+        <Popover isLazy={true} isOpen={openPopover === cat} closeOnBlur={false}>
+          <PopoverTrigger>
+            <button className="color-picker-thumb" onClick={()=>handleSwatchClick(cat)}>
+              <div className="color-picker-thumb-swatch"
+                    style={{backgroundColor: dataConfiguration.getLegendColorForCategory(cat) || missingColor}}/>
+            </button>
+          </PopoverTrigger>
+          <Portal>
+            <PaletteColorPicker dataConfiguration={dataConfiguration} cat={cat}/>
+          </Portal>
+        </Popover>
       </Flex>
     )
   })
@@ -181,3 +194,83 @@ export const DisplayItemFormatControl = observer(function PointFormatControl(pro
     </Flex>
   )
 })
+
+interface ColorPickerIProps {
+  dataConfiguration: IDataConfigurationModel
+  cat: string
+}
+
+export const PaletteColorPicker = (props: ColorPickerIProps) => {
+  const dataConfiguration = props.dataConfiguration
+  const [showColorPicker, setShowColorPicker] = useState(false)
+  const [inputValue, setInputValue] = useState(missingColor)
+  const nonStandardColorSelected = false
+
+  const updateValue = useCallback((value: string) => {
+    setInputValue(value)
+    console.log("ColorPicker: updateValue", value)
+    // dataConfiguration.applyModelChange(() => {
+    //   // CategorySet.setColorForCategory(props.cat, value)
+    // })
+  }, [])
+
+  const rejectValue = useCallback(() => {
+    setShowColorPicker(false)
+  }, [])
+
+  const acceptValue = useCallback(() => {
+    setShowColorPicker(false)
+    console.log("ColorPicker: acceptValue", inputValue)
+  }, [])
+
+  const handleShowColorPicker = (evt: React.MouseEvent) => {
+    evt.preventDefault()
+    evt.stopPropagation()
+    console.log("handleShowColorPicker showColorPicker", showColorPicker)
+    setShowColorPicker(!showColorPicker)
+  }
+
+  const baseColors = ["#000000", "#a9a9a9", "#d3d3d3", "#FFFFFF"]
+  const swatchColors = [...baseColors, ...kellyColors.slice(0, 12)]
+
+  return (
+    <PopoverContent className="color-picker-palette-container">
+      <FocusLock persistentFocus={true}>
+        <PopoverBody className="color-picker-palette">
+          <div className="color-swatch-palette">
+            <div className="color-swatch-grid">
+              {swatchColors.map((color, index) => (
+                <div className="color-swatch-cell" style={{ backgroundColor: color }} key={index} />
+              ))}
+              {nonStandardColorSelected &&
+                <div className="color-swatch-row">
+                  <div className="color-swatch-cell" style={{backgroundColor: "#FFFF00"}}/>
+                </div>}
+            </div>
+            <div className="color-swatch-footer">
+              <Button onClick={handleShowColorPicker}>
+                {showColorPicker ? "Less" : "More"}
+              </Button>
+            </div>
+          </div>
+          {showColorPicker &&
+            <div className="color-picker">
+              <ColorPicker color={inputValue} onChange={updateValue} />
+              <Flex>
+                <Spacer/>
+                <ButtonGroup>
+                  <Button className="cancel-button" size="xs" fontWeight="normal" onClick={rejectValue}>
+                    {t("V3.CaseTable.colorPalette.cancel")}
+                  </Button>
+                  <Button className="set-color-button" size="xs" fontWeight="normal" onClick={acceptValue}>
+                    {t("V3.CaseTable.colorPalette.setColor")}
+                  </Button>
+                </ButtonGroup>
+              </Flex>
+            </div>
+          }
+        </PopoverBody>
+      </FocusLock>
+    </PopoverContent>
+  )
+}
