7e5d780c95fc828bdaa8637d9027c36c084fca21
Evangeline Ireland <eireland@concord.org>
2024-10-22 08:12:05 -0700

    182079220 function descriptions (#1551)
    * Adds function list json file
    
    * WIP adding operand and function buttons to formula editor modal
    
    * Adds insert value and insert function buttons to formula editor.
    
    Click on either button opens a list of values or formulas that can be selected to be added to the formula.
    
    Insert function button lists are mostly styled as in V2 (minus hover and click formatting)
    
    Insert Value list is missing globals and boundaries
    
    * Refactored formula functions and values menu into their own components.
    
    * Implements closing the menu lists when user clicks outside the menu
    
    * Adds hover and active color to insert buttons, menu Items, and function category headers
    
    * Adds the values or functions at the current cursor or selection position
    
    * Fixes function menu positioning depending on space available
    
    Fixes font style in function menu
    
    * Adds scrollable list of operands when user clicks on Insert Values button
    
    * Adds data-testid attributes to select components
    
    Adds cypress tests to insert value and insert function components
    
    Fixes broken cypress test for entering a formula
    
    * Add back afunction removed by merge of main
    
    * chore: code review tweaks
    
    * chore: eliminate !important
    
    * Fixes broken positioning and scrolling logic for values menu and functions menu
    
    ---------
    
    Co-authored-by: Kirk Swenson <kswenson@users.noreply.github.com>

---
+++ v3/src/components/common/formula-insert-values-menu.tsx
@@ -0,0 +1,190 @@
+import { TriangleDownIcon, TriangleUpIcon } from "@chakra-ui/icons"
+import {Divider, Flex, List, ListItem,} from "@chakra-ui/react"
+import React, { useEffect, useRef, useState } from "react"
+import { useDataSetContext } from "../../hooks/use-data-set-context"
+import { getGlobalValueManager, getSharedModelManager } from "../../models/tiles/tile-environment"
+import { useFormulaEditorContext } from "./formula-editor-context"
+
+import "./formula-insert-menus.scss"
+
+const kMenuGap = 3
+const kMaxHeight = 470
+
+interface IProps {
+  setShowValuesMenu: (show: boolean) => void
+}
+
+export const InsertValuesMenu = ({setShowValuesMenu}: IProps) => {
+  const dataSet = useDataSetContext()
+  const { editorApi } = useFormulaEditorContext()
+  const collections = dataSet?.collections
+  const attributeNamesInCollection = collections?.map(collection =>
+    collection.attributes
+      .map(attr => attr?.name)
+      .filter(name => name !== undefined)
+  )
+  const attributeNames = dataSet?.attributes.map(attr => attr.name)
+  const globalManager = dataSet && getGlobalValueManager(getSharedModelManager(dataSet))
+  const globals = globalManager
+                    ? Array.from(globalManager.globals.values()).map(global => ({ label: global.name }))
+                    : []
+  // TODO_Boundaries
+  const remoteBoundaryData = [ "CR_Cantones", "CR_Provincias", "DE_state_boundaries",
+     "IT_region_boundaries", "JP_Prefectures", "US_congressional_boundaries", "US_county_boundaries",
+     "US_puma_boundaries", "US_state_boundaries", "country_boundaries" ]
+  const constants = ["e", "false", "true", "π"]
+  const scrollableContainerRef = useRef<HTMLUListElement>(null)
+  const [listContainerStyle, setListContainerStyle] = useState({})
+  const [, setScrollPosition] = useState(0)
+
+  let maxItemLength = 0
+
+  const insertValueToFormula = (value: string) => {
+    editorApi?.insertVariableString(value)
+    setShowValuesMenu(false)
+  }
+
+  const getListContainerStyle = () => {
+    // calculate the top of the list container based on the height of the list. The list should be
+    // nearly centered on the button that opens it.
+    // The list should not extend beyond the top or bottom of the window.
+    const listEl = document.querySelector(".formula-operand-list-container") as HTMLElement
+    const button = document.querySelector(".formula-editor-button.insert-value")
+
+    attributeNames?.forEach((attrName) => {
+      if (attrName.length > maxItemLength) {
+        maxItemLength = attrName.length
+      }
+    })
+    remoteBoundaryData.forEach((boundary) => {
+      if (boundary.length > maxItemLength) {
+        maxItemLength = boundary.length
+      }
+    })
+
+    globals.forEach((global) => {
+      if (global.label.length > maxItemLength) {
+        maxItemLength = global.label.length
+      }
+    })
+
+    let top = 0
+    if (button && listEl) {
+      const buttonRect = button.getBoundingClientRect()
+      const buttonBottom = buttonRect.bottom || 0
+      const listHeight = listEl.offsetHeight || 0
+      const spaceBelow = window.innerHeight - buttonBottom
+
+      if (spaceBelow >= listHeight) {
+        top = buttonRect.height + kMenuGap
+      } else {
+        top = spaceBelow - listHeight
+      }
+      return { top, height: kMaxHeight, width: 40 + 10 * maxItemLength }
+    }
+    return {}
+  }
+
+  const handleScroll = (direction: "up" | "down") => {
+    const container = scrollableContainerRef.current
+    if (container) {
+      if (direction === "up") {
+        container.scrollTo({ top: 0, behavior: "smooth" })
+      } else {
+        container.scrollTo({ top: container.scrollHeight, behavior: "smooth" })
+      }
+    }
+  }
+
+  useEffect(() => {
+    setListContainerStyle(getListContainerStyle())
+
+    const handleScrollPosition = () => {
+      if (scrollableContainerRef.current) {
+        setScrollPosition(scrollableContainerRef.current.scrollTop)
+      }
+    }
+
+    const container = scrollableContainerRef.current
+    container?.addEventListener("scroll", handleScrollPosition)
+
+    return () => {
+      container?.removeEventListener("scroll", handleScrollPosition)
+    }
+  }, [])
+
+  const isScrollable = scrollableContainerRef.current && scrollableContainerRef.current.scrollHeight > kMaxHeight
+  const canScrollUp = scrollableContainerRef.current && scrollableContainerRef.current.scrollTop > 0
+  const canScrollDown = scrollableContainerRef.current &&
+          (scrollableContainerRef.current.scrollHeight - scrollableContainerRef.current.scrollTop + 20 > kMaxHeight)
+
+  return (
+    <Flex className="formula-operand-list-container" data-testid="formula-value-list"
+        style={listContainerStyle} >
+      { isScrollable && canScrollUp &&
+      <div className="scroll-arrow" onPointerOver={()=>handleScroll("up")}>
+        <TriangleUpIcon />
+      </div>
+      }
+      <List className="formula-operand-scrollable-container"  ref={scrollableContainerRef}>
+        <List className="formula-operand-list">
+          { collections?.map((collection, index) => {
+            return (
+              <React.Fragment key={collection.id}>
+                <List className="formula-operand-subset">
+                  { attributeNamesInCollection?.[index]?.map((attrName) => {
+                    return (
+                      <ListItem className="formula-operand-list-item" key={attrName}
+                          onClick={() => insertValueToFormula(attrName)} data-testid="formula-value-item">
+                        {attrName}
+                      </ListItem>
+                    )
+                  })}
+                </List>
+                <Divider className="list-divider"/>
+              </React.Fragment>
+            )
+          })}
+        </List>
+        <List className="formula-operand-subset">
+          <ListItem className="formula-operand-list-item" onClick={() => insertValueToFormula("caseIndex")}>
+            caseIndex
+          </ListItem>
+        </List>
+        <Divider className="list-divider"/>
+        <List className="formula-operand-subset">
+          { remoteBoundaryData.map((boundary) => {
+            return (
+              <ListItem key={boundary} className="formula-operand-list-item"
+                    onClick={() => insertValueToFormula(boundary)}>
+                {boundary}
+              </ListItem>
+            )
+          })}
+          { globals.map((global) => {
+            return (
+              <ListItem key={global.label} className="formula-operand-list-item"
+                    onClick={() => insertValueToFormula(global.label)}>
+                {global.label}
+              </ListItem>
+            )
+          })}
+        </List>
+        <Divider className="list-divider"/>
+        <List className="formula-operand-subset">
+          {constants.map(constant => (
+            <ListItem className="formula-operand-list-item" key={constant}
+                      onClick={() => insertValueToFormula(constant)}>
+              {constant}
+            </ListItem>
+          ))}
+        </List>
+      </List>
+      { isScrollable && canScrollDown &&
+        <div className="scroll-arrow" onPointerOver={()=>handleScroll("down")}>
+          <TriangleDownIcon />
+        </div>
+      }
+    </Flex>
+  )
+}
