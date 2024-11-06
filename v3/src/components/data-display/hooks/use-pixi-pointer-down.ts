b37b25fb42cddb0902ab3a2cc68d84406bb8064f
Kirk Swenson <kswenson@users.noreply.github.com>
2024-11-06 10:10:19 -0800

    fix: deselect on click on PIXI canvas

--- v3/src/components/data-display/hooks/use-pixi-pointer-down.ts
+++ v3/src/components/data-display/hooks/use-pixi-pointer-down.ts
@@ -2,18 +2,19 @@ import { useEffect } from "react"
 import { useTileModelContext } from "../../../hooks/use-tile-model-context"
 import { PixiPoints } from "../pixi/pixi-points"
 
-export function usePixiPointerDown(pixiPointsArray: PixiPoints[], onPointerDown: (event: PointerEvent) => void) {
+type OnPointerDownCallback = (event: PointerEvent, pixiPoints: PixiPoints) => void
+
+export function usePixiPointerDown(pixiPointsArray: PixiPoints[], onPointerDown: OnPointerDownCallback) {
   const { isTileSelected } = useTileModelContext()
 
   useEffect(() => {
     const handlePointerDownCapture = (event: PointerEvent) => {
       // Browser events are dispatched directly to the PIXI canvas.
       // Re-dispatched events are dispatched to elements behind the PIXI canvas.
-      const canvases: Array<EventTarget | null> = pixiPointsArray.map(pixiPoints => pixiPoints.canvas)
-      const isBrowserEventOnPixiCanvas = canvases.includes(event.target)
+      const pixiPointsIndex = pixiPointsArray.findIndex(pixiPoints => event.target === pixiPoints.canvas)
       // first click selects tile; deselection only occurs once the tile is already selected
-      if (isBrowserEventOnPixiCanvas && isTileSelected()) {
-        onPointerDown(event)
+      if (pixiPointsIndex >= 0 && isTileSelected()) {
+        onPointerDown(event, pixiPointsArray[pixiPointsIndex])
       }
     }
     window.addEventListener("pointerdown", handlePointerDownCapture, { capture: true })
