610f355971ad969e591d7537657f980baaa17832
Kirk Swenson <kswenson@users.noreply.github.com>
2024-11-12 14:40:42 -0800

    fix: crash in `usePixiPointerDown` due to undefined PixiPoints (#1612)

--- v3/src/components/data-display/hooks/use-pixi-pointer-down-deselect.ts
+++ v3/src/components/data-display/hooks/use-pixi-pointer-down-deselect.ts
@@ -1,9 +1,9 @@
 import { selectAllCases } from "../../../models/data/data-set-utils"
 import { IDataDisplayContentModel } from "../models/data-display-content-model"
-import { PixiPoints } from "../pixi/pixi-points"
+import { PixiPoints, PixiPointsArray } from "../pixi/pixi-points"
 import { usePixiPointerDown } from "./use-pixi-pointer-down"
 
-export function usePixiPointerDownDeselect(pixiPointsArray: PixiPoints[], model?: IDataDisplayContentModel) {
+export function usePixiPointerDownDeselect(pixiPointsArray: PixiPointsArray, model?: IDataDisplayContentModel) {
   usePixiPointerDown(pixiPointsArray, (event, pixiPoints: PixiPoints) => {
     if (!event.shiftKey && !event.metaKey && !event.ctrlKey) {
       pixiPoints.requestAnimationFrame("deselectAll", () => {
