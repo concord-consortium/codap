a795d36280af7d03c5bd6f2bca213444f931115e
eireland <eireland@concord.org>
2024-11-15 10:19:29 -0800

    clean up

--- v3/cypress/support/helpers/formula-helper.ts
+++ v3/cypress/support/helpers/formula-helper.ts
@@ -13,46 +13,6 @@ export const FormulaHelper = {
     cfm.openLocalDoc(file)
     cy.wait(1000)
   },
-  // addNewAttribute(collectionIndex = 1) {
-  //   table.addNewAttribute(collectionIndex)
-  // },
-  // renameAttribute(currentAttributeName: string, newAttributeName: string, collectionIndex = 1) {
-  //   table.renameAttribute(currentAttributeName, newAttributeName, collectionIndex)
-  // },
-  // deleteAttribute(attributeName: string, collectionIndex = 1) {
-  //   table.deleteAttribute(attributeName, collectionIndex)
-  // },
-  // addFormula(attributeName: string, formula: string, collectionIndex = 1) {
-  //   table.addFormula(attributeName, formula, collectionIndex)
-  // },
-  // verifyValues(attributeName: string, values: Array<any>, collectionIndex = 1) {
-  //   table.verifyFormulaValues(attributeName, values, collectionIndex)
-  // },
-  // checkFormulaExists(attributeName: string, formula: string, collectionIndex = 1) {
-  //   table.checkFormulaExists(attributeName, formula, collectionIndex)
-  // },
-  // editFormula(attributeName: string, formula: string, collectionIndex = 1) {
-  //   table.editFormula(attributeName, formula, collectionIndex)
-  // },
-  // createNewDataset() {
-  //   table.createNewTableFromToolShelf()
-  // },
-  // insertCases(rowIndex: number, numOfCases: number) {
-  //   table.openIndexMenuForRow(rowIndex)
-  //   table.insertCases(numOfCases, "after")
-  // },
-  // changeSliderVariableName(sliderVariableName: string) {
-  //   slider.changeVariableName(sliderVariableName)
-  // },
-  // changeSliderValue(sliderValue: string) {
-  //   slider.changeVariableValue(sliderValue)
-  // },
-  // addSlider() {
-  //   c.getIconFromToolShelf("slider").click()
-  // },
-  // deleteSlider() {
-  //   c.closeComponent("slider")
-  // },
   clearFormulaInput() {
     cy.get("[data-testid=formula-editor-input] .cm-content").should("be.visible").and("have.focus")
     cy.get("[data-testid=formula-editor-input] .cm-content").realPress([metaCtrlKey, "A"])
