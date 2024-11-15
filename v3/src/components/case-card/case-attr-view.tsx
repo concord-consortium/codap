7a3dd8eef93e4db85b05d5c38b125d56dba14360
eireland <eireland@concord.org>
2024-11-15 10:18:37 -0800

    Initial test of making case card respect attribute precision. -- Switch case card to use the same renderAttributeValue code as the table.

--- v3/src/components/case-card/case-attr-view.tsx
+++ v3/src/components/case-card/case-attr-view.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react"
 import { observer } from "mobx-react-lite"
-import { Editable, EditablePreview, EditableInput } from "@chakra-ui/react"
+import { Editable, EditablePreview, EditableInput, Tooltip } from "@chakra-ui/react"
 import { clsx } from "clsx"
 import { IValueType } from "../../models/data/attribute-types"
 import { ICollectionModel } from "../../models/data/collection"
@@ -11,6 +11,7 @@ import { AttributeHeaderDivider } from "../case-tile-common/attribute-header-div
 import { GetDividerBoundsFn } from "../case-tile-common/case-tile-types"
 import { applyCaseValueChanges } from "../case-tile-common/case-tile-utils"
 import { useCaseCardModel } from "./use-case-card-model"
+import { renderAttributeValue } from "../case-tile-common/attribute-format-utils"
 
 import "./case-attr-view.scss"
 
@@ -19,30 +20,32 @@ interface ICaseAttrViewProps {
   collection: ICollectionModel
   attrId: string
   name: string
-  value: IValueType
+  cellValue: IValueType
   unit?: string
   getDividerBounds?: GetDividerBoundsFn
   onSetContentElt?: (contentElt: HTMLDivElement | null) => HTMLElement | null
 }
 
 export const CaseAttrView = observer(function CaseAttrView (props: ICaseAttrViewProps) {
-  const { caseId, collection, attrId, unit, value, getDividerBounds, onSetContentElt } = props
+  const { caseId, collection, attrId, unit, cellValue, getDividerBounds, onSetContentElt } = props
   const cardModel = useCaseCardModel()
   const data = cardModel?.data
   const attr = collection.getAttribute(attrId)
   const isCollectionSummarized = !!cardModel?.summarizedCollections.includes(collection.id)
-  const displayValue = value ? String(value) : ""
-  const showUnitWithValue = isFiniteNumber(Number(value)) && unit
+  const strValue = cellValue ? String(cellValue) : ""
+  const numValue = isFiniteNumber(Number(cellValue)) ? Number(cellValue) : undefined
+  const showUnitWithValue = isFiniteNumber(Number(cellValue)) && unit
   const [isEditing, setIsEditing] = useState(false)
-  const [editingValue, setEditingValue] = useState(displayValue)
+  // const [editingValue, setEditingValue] = useState(strValue)
+  const { value, content } = renderAttributeValue(strValue, numValue, attr)
 
-  const handleChangeValue = (newValue: string) => {
-    setEditingValue(newValue)
-  }
+  // const handleChangeValue = (newValue: string) => {
+  //   setEditingValue(newValue)
+  // }
 
   const handleCancel = (_previousName?: string) => {
     setIsEditing(false)
-    setEditingValue(displayValue)
+    // setEditingValue(strValue)
   }
 
   const handleSubmit = (newValue?: string) => {
@@ -52,12 +55,12 @@ export const CaseAttrView = observer(function CaseAttrView (props: ICaseAttrView
 
       if (data) {
         applyCaseValueChanges(data, casesToUpdate)
-        return
+
       }
 
-      setEditingValue(newValue)
+      // setEditingValue(newValue)
     } else {
-      setEditingValue(displayValue)
+      // setEditingValue(strValue)
     }
   }
 
@@ -65,31 +68,35 @@ export const CaseAttrView = observer(function CaseAttrView (props: ICaseAttrView
     if (isCollectionSummarized) {
       return (
         <div className={clsx("case-card-attr-value-text", "static-summary", {"formula-attr-value": attr?.hasFormula})}>
-          {displayValue}
+          {/* {displayValue} */}
+          {content}
         </div>
       )
     }
 
     return (
-      <Editable
-        className={clsx("case-card-attr-value-text", {"formula-attr-value": attr?.hasFormula})}
-        isPreviewFocusable={true}
-        isDisabled={attr?.hasFormula}
-        onCancel={handleCancel}
-        onChange={handleChangeValue}
-        onEdit={() => setIsEditing(true)}
-        onSubmit={handleSubmit}
-        submitOnBlur={true}
-        value={isEditing ? editingValue : `${displayValue}${showUnitWithValue ? ` ${unit}` : ""}`}
-      >
-        <EditablePreview paddingY={0} />
-        <EditableInput
-          className="case-card-attr-value-text-editor"
-          data-testid="case-card-attr-value-text-editor"
-          paddingY={0}
-          value={editingValue}
-        />
-      </Editable>
+      <Tooltip label={value} h="20px" fontSize="12px" color="white" data-testid="case-table-data-tip"
+        openDelay={1000} placement="bottom" bottom="10px" left="15px">
+        {content}
+      </Tooltip>      // <Editable
+      //   className={clsx("case-card-attr-value-text", {"formula-attr-value": attr?.hasFormula})}
+      //   isPreviewFocusable={true}
+      //   isDisabled={attr?.hasFormula}
+      //   onCancel={handleCancel}
+      //   onChange={handleChangeValue}
+      //   onEdit={() => setIsEditing(true)}
+      //   onSubmit={handleSubmit}
+      //   submitOnBlur={true}
+      //   value={isEditing ? editingValue : `${strValue}${showUnitWithValue ? ` ${unit}` : ""}`}
+      // >
+      //   <EditablePreview paddingY={0} />
+      //   <EditableInput
+      //     className="case-card-attr-value-text-editor"
+      //     data-testid="case-card-attr-value-text-editor"
+      //     paddingY={0}
+      //     value={editingValue}
+      //   />
+      // </Editable>
     )
   }
 
