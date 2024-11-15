dc370b988e70e95dd53760e7d47f65e4e8403a56
Evangeline Ireland <eireland@concord.org>
2024-11-15 11:34:12 -0800

    188497528 case tile filter formula editor (#1617)
    * Changes graph filter formula modal to use the new formula editor modal
    
    * Adds filter formula to map inspector panel
    
    Adds new formula editor modal to the map inspector panel
    
    Note that filtering isn't implemented
    
    * Update edit filter string
    
    * Updates cypress test because of the formula editor change
    
    * Adds a custom hook for inspector panel formula filter text.
    
    * Changes case table/card filter formula editor to use the new formula editor modal
    
    * refactored cypress elements to fix circula logic and keep selectors and function in their elements.
    
    * Fix linting errors in cypress tests
    
    * clean up

--- v3/cypress/e2e/formula/formula-hierarchical-tables.spec.ts
+++ v3/cypress/e2e/formula/formula-hierarchical-tables.spec.ts
@@ -1,55 +1,56 @@
 import { FormulaHelper as fh } from "../../support/helpers/formula-helper"
+import { TableTileElements as table } from "../../support/elements/table-tile"
 
 context("Formula Engine", () => {
   describe("Hierarchical Case Tables Formula Tests", () => {
     it("Check aggregate and non-aggregate formulae referencing a parent attribute in child collection", () => {
       fh.visitURL("")
       fh.importFile("cypress/fixtures/hierarchical-four.codap3")
-      fh.addNewAttribute(2)
-      fh.renameAttribute("newAttr", "Formula", 2)
-      fh.addFormula("Formula", "count(b)", 2)
-      fh.verifyValues("Formula", [
+      table.addNewAttribute(2)
+      table.renameAttribute("newAttr", "Formula", 2)
+      table.addFormula("Formula", "count(b)", 2)
+      table.verifyFormulaValues("Formula", [
         "❌ invalid reference to parent attribute 'b' within aggregate function",
         "❌ invalid reference to parent attribute 'b' within aggregate function",
         "❌ invalid reference to parent attribute 'b' within aggregate function",
         "❌ invalid reference to parent attribute 'b' within aggregate function",
         "❌ invalid reference to parent attribute 'b' within aggregate function"
       ], 2)
-      fh.editFormula("Formula", "b+1", 2)
-      fh.verifyValues("Formula", [2, 2, 2, 1, 1], 2)
+      table.editFormula("Formula", "b+1", 2)
+      table.verifyFormulaValues("Formula", [2, 2, 2, 1, 1], 2)
     })
     it("Check aggregate and non-aggregate formulae referencing a child attribute in child collection", () => {
       fh.visitURL("")
       fh.importFile("cypress/fixtures/hierarchical-four.codap3")
-      fh.addNewAttribute(2)
-      fh.renameAttribute("newAttr", "Formula", 2)
-      fh.addFormula("Formula", "count(a)", 2)
-      fh.verifyValues("Formula", [3, 3, 3, 1, 1], 2)
-      fh.editFormula("Formula", "a+1", 2)
-      fh.verifyValues("Formula", [2, 3, 4, 4, 1], 2)
+      table.addNewAttribute(2)
+      table.renameAttribute("newAttr", "Formula", 2)
+      table.addFormula("Formula", "count(a)", 2)
+      table.verifyFormulaValues("Formula", [3, 3, 3, 1, 1], 2)
+      table.editFormula("Formula", "a+1", 2)
+      table.verifyFormulaValues("Formula", [2, 3, 4, 4, 1], 2)
     })
     it("Check aggregate and non-aggregate formulae referencing a parent attribute in parent collection", () => {
       fh.visitURL("")
       fh.importFile("cypress/fixtures/hierarchical-four.codap3")
-      fh.addNewAttribute(1)
-      fh.renameAttribute("newAttr", "Formula", 1)
-      fh.addFormula("Formula", "count(b)", 1)
-      fh.verifyValues("Formula", [1, 1], 1)
-      fh.editFormula("Formula", "b+1", 1)
-      fh.verifyValues("Formula", [2, 1], 1)
+      table.addNewAttribute(1)
+      table.renameAttribute("newAttr", "Formula", 1)
+      table.addFormula("Formula", "count(b)", 1)
+      table.verifyFormulaValues("Formula", [1, 1], 1)
+      table.editFormula("Formula", "b+1", 1)
+      table.verifyFormulaValues("Formula", [2, 1], 1)
     })
     it("Check aggregate and non-aggregate formulae referencing a child attribute in parent collection", () => {
       fh.visitURL("")
       fh.importFile("cypress/fixtures/hierarchical-four.codap3")
-      fh.addNewAttribute(1)
-      fh.renameAttribute("newAttr", "Formula", 1)
-      fh.addFormula("Formula", "a+1", 1)
-      fh.verifyValues("Formula", [
+      table.addNewAttribute(1)
+      table.renameAttribute("newAttr", "Formula", 1)
+      table.addFormula("Formula", "a+1", 1)
+      table.verifyFormulaValues("Formula", [
         "❌ invalid reference to child attribute 'a'",
         "❌ invalid reference to child attribute 'a'"
       ], 1)
-      fh.editFormula("Formula", "count(a)", 1)
-      fh.verifyValues("Formula", [3, 1], 1)
+      table.editFormula("Formula", "count(a)", 1)
+      table.verifyFormulaValues("Formula", [3, 1], 1)
     })
   })
 })
--- v3/cypress/support/helpers/formula-helper.ts
+++ v3/cypress/support/helpers/formula-helper.ts
@@ -1,6 +1,3 @@
-import { ComponentElements as c } from "../elements/component-elements"
-import { TableTileElements as table } from "../elements/table-tile"
-import { SliderTileElements as slider } from "../elements/slider-tile"
 import { CfmElements as cfm } from "../elements/cfm"
 
 const isMac = navigator.platform.toLowerCase().includes("mac")
@@ -15,46 +12,6 @@ export const FormulaHelper = {
   importFile(file: string) {
     cfm.openLocalDoc(file)
     cy.wait(1000)
-  },
-  addNewAttribute(collectionIndex = 1) {
-    table.addNewAttribute(collectionIndex)
-  },
-  renameAttribute(currentAttributeName: string, newAttributeName: string, collectionIndex = 1) {
-    table.renameAttribute(currentAttributeName, newAttributeName, collectionIndex)
-  },
-  deleteAttribute(attributeName: string, collectionIndex = 1) {
-    table.deleteAttribute(attributeName, collectionIndex)
-  },
-  addFormula(attributeName: string, formula: string, collectionIndex = 1) {
-    table.addFormula(attributeName, formula, collectionIndex)
-  },
-  verifyValues(attributeName: string, values: Array<any>, collectionIndex = 1) {
-    table.verifyFormulaValues(attributeName, values, collectionIndex)
-  },
-  checkFormulaExists(attributeName: string, formula: string, collectionIndex = 1) {
-    table.checkFormulaExists(attributeName, formula, collectionIndex)
-  },
-  editFormula(attributeName: string, formula: string, collectionIndex = 1) {
-    table.editFormula(attributeName, formula, collectionIndex)
-  },
-  createNewDataset() {
-    table.createNewTableFromToolShelf()
-  },
-  insertCases(rowIndex: number, numOfCases: number) {
-    table.openIndexMenuForRow(rowIndex)
-    table.insertCases(numOfCases, "after")
-  },
-  changeSliderVariableName(sliderVariableName: string) {
-    slider.changeVariableName(sliderVariableName)
-  },
-  changeSliderValue(sliderValue: string) {
-    slider.changeVariableValue(sliderValue)
-  },
-  addSlider() {
-    c.getIconFromToolShelf("slider").click()
-  },
-  deleteSlider() {
-    c.closeComponent("slider")
   },
   clearFormulaInput() {
     cy.get("[data-testid=formula-editor-input] .cm-content").should("be.visible").and("have.focus")
