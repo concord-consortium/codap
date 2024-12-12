e026e5039d425028b2c2fb8fb4b3ef658e20310b
Paul Kim <paulkim3151@gmail.com>
2024-12-12 14:50:08 +0900

    feat: getEmptyContent 이벤트 처리 추가

--- apps/dg/main.js
+++ apps/dg/main.js
@@ -757,6 +757,12 @@ DG.main = function main() {
           case "closedFile":
             cfmShowUserEntryView();
             break;
+          
+          case 'getEmptyContent':
+            resolveDocument(undefined).then(function(emptyContent) {
+              event.callback(emptyContent);
+            });
+            break;
 
           case 'getContent':
             DG.currDocumentController().captureCurrentDocumentState(true)
