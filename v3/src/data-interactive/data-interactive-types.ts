7588b0f3ffa9d7be7d7e21eb06917d184c2bd46f
Ethan McElroy <emcelroy@concord.org>
2024-12-17 20:51:43 -0500

    chore: add data display handler

--- v3/src/data-interactive/data-interactive-types.ts
+++ v3/src/data-interactive/data-interactive-types.ts
@@ -165,6 +165,9 @@ export interface DILogMessage {
 export interface DIUrl {
   URL: string
 }
+export interface DIDataDisplay {
+  exportDataUri?: string
+}
 
 export interface DIResources {
   attribute?: IAttribute
@@ -179,6 +182,7 @@ export interface DIResources {
   component?: DIComponent
   dataContext?: IDataSet
   dataContextList?: IDataSet[]
+  dataDisplay?: DIDataDisplay
   error?: string
   global?: IGlobalValue
   imageSnapshot?: DIComponent
@@ -199,7 +203,8 @@ export type DIValues = DISingleValues | DISingleValues[] | number | string[]
 
 // types returned as outputs by the API
 export type DIResultAttributes = { attrs: ICodapV2Attribute[] }
-export type DIResultSingleValues = DICase | DIComponentInfo |  DIGetCaseResult | DIGlobal | DIInteractiveFrame
+export type DIResultSingleValues = DICase | DIComponentInfo |  DIGetCaseResult | DIGlobal | DIInteractiveFrame |
+  DIDataDisplay
 export type DIResultValues = DIResultSingleValues | DIResultSingleValues[] |
   DIAllCases | DIDeleteCollectionResult | DIUpdateItemResult | DIResultAttributes | number | number[]
 
@@ -273,6 +278,7 @@ export interface DIResourceSelector {
   component?: string
   dataContext?: string
   dataContextList?: string
+  dataDisplay?: string
   global?: string
   interactiveFrame?: string
   item?: string
--- v3/src/register-tile-types.ts
+++ v3/src/register-tile-types.ts
@@ -10,6 +10,7 @@ import "./components/calculator/calculator-registration"
 import "./components/text/text-registration"
 import "./components/map/map-registration"
 import "./components/web-view/web-view-registration"
+import "./components/data-display/components/data-display-registration"
 
 const gTileRegistration: Record<string, () => void> = {
   // "Table": () => Promise.all([
