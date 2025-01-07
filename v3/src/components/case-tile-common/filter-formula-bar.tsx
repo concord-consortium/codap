10e323f69433ea0a4a992cab89044a346e10f232
eireland <eireland@concord.org>
2025-01-07 13:59:10 -0800

    Adds ability to open the edit formula modal when user clicks on formual bar
    Adds label text to language files.

--- v3/src/components/case-tile-common/filter-formula-bar.tsx
+++ v3/src/components/case-tile-common/filter-formula-bar.tsx
@@ -1,15 +1,55 @@
-import React from "react"
+import React, { useState } from "react"
 import { useDataSetContext } from "../../hooks/use-data-set-context"
+import { EditFormulaModal } from "../common/edit-formula-modal"
+import { useDisclosure } from "@chakra-ui/react"
+import { t } from "../../utilities/translation/translate"
 
 import "./filter-formula-bar.scss"
 
 export const FilterFormulaBar = () => {
   const data = useDataSetContext()
+  const formulaModal = useDisclosure()
+  const [modalOpen, setModalOpen] = useState(false)
+
+  const handleOpenEditFormulaModal = () => {
+    setModalOpen(true)
+    formulaModal.onOpen()
+  }
+
+  const handleSubmitEditFormula = (formula: string) => {
+    data?.applyModelChange(() => data.setFilterFormula(formula),
+      { undoStringKey: "DG.Undo.caseTable.changeFilterFormula",
+        redoStringKey: "DG.Redo.caseTable.changeFilterFormula",
+      }
+    )
+    handleCloseModal()
+  }
+
+  const handleCloseModal = () => {
+    setModalOpen(false)
+    formulaModal.onClose()
+  }
+
   if (!data) return null
+  const filterFormula = data.filterFormula?.display
+
   return (
-    <div className="filter-formula-container">
-      <span className="filter-formula-label">Show cases only if:</span>
-      <span className="filter-formula-value">{data.filterFormula?.display}</span>
-    </div>
+    <>
+      <div className="filter-formula-container">
+        <span className="filter-formula-label">{t("DG.CaseTable.formulaFilterBar.label")}:</span>
+        <span className="filter-formula-value" onClick={handleOpenEditFormulaModal}>
+          {filterFormula}
+        </span>
+      </div>
+      {modalOpen &&
+        <EditFormulaModal
+          applyFormula={handleSubmitEditFormula}
+          isOpen={formulaModal.isOpen}
+          onClose={handleCloseModal}
+          titleLabel={t("DG.PlottedFunction.namePrompt")}
+          value={filterFormula}
+        />
+      }
+    </>
   )
 }
