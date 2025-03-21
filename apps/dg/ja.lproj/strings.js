3bc22f5972eef57ecfb85a83e15119bfae59dd5e
eireland <eireland@concord.org>
2025-03-21 16:31:03 -0300

    update translation strings

--- apps/dg/ja.lproj/strings.js
+++ apps/dg/ja.lproj/strings.js
@@ -1137,9 +1137,9 @@ SC.stringsFor("ja", {
               "DG.plugin.Sonify.description": "Listen to your data",
               "DG.plugin.NHANES.title": "NHANES Portal",
               "DG.plugin.NHANES.description": "Get data from the National Health and Nutrition Examination Survey.",
-              "DG.plugin.NASA-data.title": "NASA Earth, Air and Water",
+              "DG.plugin.NASA-data.title": "NASA: Earth, Air, and Water",
               "DG.plugin.NASA-data.description": "Get global data from NASA",
-              "DG.plugin.DayLength.title": "DayLength",
+              "DG.plugin.DayLength.title": "Day Length",
               "DG.plugin.DayLength.description": "Get day lengths for any location on Earth plus an astronomical simulation",
               "DG.plugin.Simmer.name": "Simmer",
               "DG.plugin.Simmer.description": "Create block programmed simulations that generate data."
