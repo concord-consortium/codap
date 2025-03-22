349e268a7d96adf24237c739d410649f15b9be45
Ethan McElroy <emcelroy@concord.org>
2025-03-22 10:02:48 -0400

    feat: count adornment get returns counts per region (CODAP-68)
    [CODAP-68](https://concord-consortium.atlassian.net/browse/CODAP-68)

--- v3/src/components/graph/adornments/count/count-adornment-handler.ts
+++ v3/src/components/graph/adornments/count/count-adornment-handler.ts
@@ -2,9 +2,29 @@ import { DIAdornmentHandler } from "../../../../data-interactive/handlers/adornm
 import { IGraphContentModel } from "../../models/graph-content-model"
 import { IAdornmentModel } from "../adornment-models"
 import { isCountAdornment } from "./count-adornment-model"
-import { percentString } from "../../utilities/graph-utils"
 import { AdornmentData, adornmentMismatchResult, cellKeyToCategories } from "../utilities/adornment-handler-utils"
 import { kCountType } from "./count-adornment-types"
+import { IGraphDataConfigurationModel } from "../../models/graph-data-configuration-model"
+import { scaleLinear } from "d3"
+
+const createScale = (dataConfig: IGraphDataConfigurationModel) => {
+  const primaryRole = dataConfig?.primaryRole ?? "x"
+  const attrType = dataConfig?.attributeType(primaryRole)
+  const attrId = dataConfig?.attributeID(primaryRole)
+  if (!attrId) return null
+
+  const cases = dataConfig?.dataset?.getCasesForAttributes([attrId]) || []
+  const values = cases.map(c => dataConfig?.dataset?.getNumeric(c.__id__, attrId)).filter(v => v !== undefined)
+  if (values.length === 0) return null
+
+  const minValue = Math.min(...values)
+  const maxValue = Math.max(...values)
+
+  if (attrType === "numeric") {
+    return scaleLinear().domain([minValue, maxValue]).range([0, 1])
+  }
+  return null
+}
 
 export const countAdornmentHandler: DIAdornmentHandler = {
   get(adornment: IAdornmentModel, graphContent: IGraphContentModel) {
@@ -12,19 +32,43 @@ export const countAdornmentHandler: DIAdornmentHandler = {
 
     const { percentType, showCount, showPercent } = adornment
     const dataConfig = graphContent.dataConfiguration
-    const cellKeys = dataConfig?.getAllCellKeys()
+    const cellKeys = dataConfig.getAllCellKeys()
     const data: AdornmentData[] = []
+    const scale = createScale(dataConfig)
 
     for (const cellKey of cellKeys) {
-      const subPlotCases = dataConfig.subPlotCases(cellKey)
       const dataItem: AdornmentData = {}
+      if (!scale) return { success: false, values: { error: "Failed to create scale" } }
+
+      const subPlotRegionBoundaries =
+        graphContent.adornmentsStore?.subPlotRegionBoundaries(JSON.stringify(cellKey), scale)
+
+      const regionCounts = adornment.computeRegionCounts({
+        cellKey,
+        dataConfig,
+        inclusiveMax: false,
+        plotHeight: 0,
+        plotWidth: 0,
+        scale,
+        subPlotRegionBoundaries,
+        isBinnedDotPlot: false,
+        showCount,
+        showPercent
+      })
+
+      const regionCountValues = regionCounts.map(c => c.count)
+      const regionPercentValues = regionCounts.map(c => c.percent)
       
       if (showCount) {
-        dataItem.count = subPlotCases.length
+        dataItem.count = regionCountValues.length > 1
+          ? regionCountValues.filter((value): value is number => value !== undefined)
+          : regionCountValues[0]
       }
 
       if (showPercent) {
-        dataItem.percent = percentString(adornment.percentValue(subPlotCases.length, cellKey, dataConfig))
+        dataItem.percent = regionPercentValues.length > 1
+          ? regionPercentValues.filter((value): value is string => value !== undefined)
+          : regionPercentValues[0]
       }
     
       if (Object.keys(cellKey).length > 0) {
