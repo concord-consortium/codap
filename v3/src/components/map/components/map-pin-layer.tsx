b200bbb1543080538897532e9a1d00cf62f40d77
Teale Fristoe <fristoe@gmail.com>
2025-04-02 07:40:43 -0700

    Click pins to select them.

--- v3/src/components/map/components/map-pin-layer.tsx
+++ v3/src/components/map/components/map-pin-layer.tsx
@@ -2,8 +2,10 @@ import clsx from "clsx"
 import { observer } from "mobx-react-lite"
 import React, { useEffect, useRef, useState } from "react"
 import { useMap } from "react-leaflet"
+import { IDataSet } from "../../../models/data/data-set"
 import { isSetCaseValuesAction } from "../../../models/data/data-set-actions"
 import { ICaseCreation } from "../../../models/data/data-set-types"
+import { insertCasesWithCustomUndoRedo } from "../../../models/data/data-set-undo"
 import { onAnyAction } from "../../../utilities/mst-utils"
 import { useDataDisplayLayout } from "../../data-display/hooks/use-data-display-layout"
 import PlacedLocationMarker from "../assets/placed-location-marker.svg"
@@ -12,6 +14,7 @@ import { kPinColors } from "../map-types"
 import { IMapPinLayerModel } from "../models/map-pin-layer-model"
 import { pinAttributesFromDataSet } from "../utilities/map-utils"
 import "./map-pin-layer.scss"
+import { selectCases, setSelectedCases } from "../../../models/data/data-set-utils"
 
 // FIX-ME: Can we get these numbers dynamically?
 const mapPinHeight = 35
@@ -19,14 +22,29 @@ const mapPinWidth = 25
 
 interface IMapPinProps {
   color?: string
+  dataset: IDataSet
+  id: string
   key?: string
   selected?: boolean
   x: number
   y: number
 }
-function MapPin({ color="#0068EA", selected, x, y }: IMapPinProps) {
+function MapPin({ color="#0068EA", dataset, id, selected, x, y }: IMapPinProps) {
+  const handleClick = (e: React.MouseEvent) => {
+    e.stopPropagation()
+    if (e.shiftKey) {
+      selectCases([id], dataset, !selected)
+    } else {
+      setSelectedCases([id], dataset)
+    }
+  }
+
   return (
-    <button className={clsx("map-pin", { "selected-pin": selected })} style={{ left: x, top: y }}>
+    <button
+      className={clsx("map-pin", { "selected-pin": selected })}
+      onClick={handleClick}
+      style={{ left: x, top: y }}
+    >
       <PlacedLocationMarker color={color} />
     </button>
   )
@@ -68,12 +86,12 @@ export const MapPinLayer = observer(function MapPinLayer({ mapLayerModel }: IMap
       mapLayerModel.setAddMode(false)
 
       const layerBB = layerRef.current?.getBoundingClientRect()
-      if (!layerBB) return
+      if (!layerBB || !dataset) return
       const { lat, lng } = map.containerPointToLatLng([e.clientX - layerBB.x, e.clientY - layerBB.y])
-      const color = kPinColors[(dataset?.items.length ?? 0) % kPinColors.length]
+      const color = kPinColors[(dataset.items.length ?? 0) % kPinColors.length]
       const newItem: ICaseCreation = { [pinLatId]: lat, [pinLongId]: lng }
       if (colorId) newItem[colorId] = color
-      dataset?.addCases([newItem])
+      insertCasesWithCustomUndoRedo(dataset, [newItem])
     }
   }
 
@@ -84,7 +102,7 @@ export const MapPinLayer = observer(function MapPinLayer({ mapLayerModel }: IMap
       onClick={handleClick}
       ref={layerRef}
     >
-      {renderPins && dataset?.items.map(({ __id__ }) => {
+      {renderPins && dataset?.items.map(({ __id__ }, index) => {
         const lat = Number(dataset.getValue(__id__, pinLatId))
         const long = Number(dataset.getValue(__id__, pinLongId))
         if (!isFinite(lat) || !isFinite(long)) return null
@@ -93,10 +111,12 @@ export const MapPinLayer = observer(function MapPinLayer({ mapLayerModel }: IMap
         const pinX = x - mapPinWidth / 2
         const pinY = y - mapPinHeight
         
-        const color = colorId ? dataset.getStrValue(__id__, colorId) : undefined
+        const color = colorId ? dataset.getStrValue(__id__, colorId) : kPinColors[index % kPinColors.length]
         return (
           <MapPin
             color={color}
+            dataset={dataset}
+            id={__id__}
             key={`pin-${__id__}`}
             selected={dataset?.isCaseSelected(__id__)}
             x={pinX}
