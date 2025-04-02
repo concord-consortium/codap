3f45436f30a44c9d1527a6f38cdae940c7e14682
Teale Fristoe <fristoe@gmail.com>
2025-04-02 15:32:45 -0700

    More PR review feedback.

--- v3/src/components/map/components/map-interior.tsx
+++ v3/src/components/map/components/map-interior.tsx
@@ -3,14 +3,14 @@ import React, {useEffect} from "react"
 import {PixiPoints} from "../../data-display/pixi/pixi-points"
 import {useMapModelContext} from "../hooks/use-map-model-context"
 import {useMapModel} from "../hooks/use-map-model"
-import {kMapPointLayerType, kMapPolygonLayerType} from "../map-types"
+import {kMapPinLayerType, kMapPointLayerType, kMapPolygonLayerType} from "../map-types"
 import {isMapPointLayerModel} from "../models/map-point-layer-model"
 import {MapPointLayer} from "./map-point-layer"
 import {isMapPolygonLayerModel} from "../models/map-polygon-layer-model"
 import {MapPolygonLayer} from "./map-polygon-layer"
 import { DataConfigurationContext } from "../../data-display/hooks/use-data-configuration-context"
 import { useTileModelContext } from "../../../hooks/use-tile-model-context"
-import { isMapPinLayerModel } from "../models/map-pin-layer-model"
+import { IMapPinLayerModel, isMapPinLayerModel } from "../models/map-pin-layer-model"
 import { MapPinLayer } from "./map-pin-layer"
 import { PinControls } from "./pin-controls"
 
@@ -21,7 +21,7 @@ interface IProps {
 export const MapInterior = observer(function MapInterior({setPixiPointsLayer}: IProps) {
   const mapModel = useMapModelContext()
   const { transitionComplete: tileTransitionComplete } = useTileModelContext()
-  const pinLayer = mapModel?.layers.find((layer) => isMapPinLayerModel(layer))
+  let pinLayer: IMapPinLayerModel | undefined
 
   useMapModel()
 
@@ -55,18 +55,20 @@ export const MapInterior = observer(function MapInterior({setPixiPointsLayer}: I
           mapLayerModel={layerModel}
         />
       }
+      else if (isMapPinLayerModel(layerModel)) {
+        pinLayer = layerModel
+        return <MapPinLayer
+          key={`${kMapPinLayerType}-${index}`}
+          mapLayerModel={layerModel}
+        />
+      }
     })
   }
 
   return (
     <>
       {renderMapLayerComponents()}
-      {pinLayer && (
-        <>
-          <MapPinLayer mapLayerModel={pinLayer} />
-          <PinControls mapLayerModel={pinLayer} />
-        </>
-      )}
+      {pinLayer && <PinControls mapLayerModel={pinLayer} />}
     </>
   )
 })
