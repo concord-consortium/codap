6dc9c1837587d15286608dbd01b1c897f71d1372
Ethan McElroy <emcelroy@concord.org>
2025-04-04 13:09:41 -0400

    chore: add component tests, reinstate/update existing tests

--- v3/src/components/graph/adornments/region-of-interest/region-of-interest-adornment-model.test.ts
+++ v3/src/components/graph/adornments/region-of-interest/region-of-interest-adornment-model.test.ts
@@ -1,41 +1,22 @@
-// import { RegionOfInterestAdornmentModel, RoiUnitValueModel }
-//   from "./region-of-interest-adornment-model"
+import { ISecondaryAxisRange, RegionOfInterestAdornmentModel } from "./region-of-interest-adornment-model"
 
-// describe("RegionOfInterestAdornmentModel", () => {
-//   it("should create an instance", () => {
-//     const model = RegionOfInterestAdornmentModel.create()
-//     expect(model).toBeTruthy()
-//   })
+describe("RegionOfInterestAdornmentModel", () => {
+  it("should create an instance", () => {
+    const model = RegionOfInterestAdornmentModel.create()
+    expect(model).toBeTruthy()
+  })
 
-//   it("should set height", () => {
-//     const heightProp = RoiUnitValueModel.create({ unit: "coordinate", value: 0 })
-//     const model = RegionOfInterestAdornmentModel.create()
-//     model.setHeight(heightProp)
-//     expect(model.height).toBe(heightProp)
-//   })
+  it("should set primary axis range", () => {
+    const primaryProp = { position: 0, extent: "100%" }
+    const model = RegionOfInterestAdornmentModel.create()
+    model.setPrimary(primaryProp)
+    expect(model.primary).toEqual(primaryProp)
+  })
 
-//   it("should set width", () => {
-//     const widthProp = RoiUnitValueModel.create({ unit: "coordinate", value: 0})
-//     const model = RegionOfInterestAdornmentModel.create()
-//     model.setWidth(widthProp)
-//     expect(model.width).toBe(widthProp)
-//   })
-
-//   it("should set position", () => {
-//     const xPosition = RoiUnitValueModel.create({ unit: "coordinate", value: 100 })
-//     const yPosition = RoiUnitValueModel.create({ unit: "coordinate", value: 150 })
-//     const model = RegionOfInterestAdornmentModel.create()
-//     model.setPosition(xPosition, yPosition)
-//     expect(model.xPosition).toBe(xPosition)
-//     expect(model.yPosition).toBe(yPosition)
-//   })
-
-//   it("should set size", () => {
-//     const heightProp = RoiUnitValueModel.create({ unit: "coordinate", value: 0 })
-//     const widthProp = RoiUnitValueModel.create({ unit: "coordinate", value: 0})
-//     const model = RegionOfInterestAdornmentModel.create()
-//     model.setSize(widthProp, heightProp)
-//     expect(model.width).toBe(widthProp)
-//     expect(model.height).toBe(heightProp)
-//   })
-// })
+  it("should set secondary axis range", () => {
+    const secondaryProp = { position: 0, extent: "100%", axis: "y" } as ISecondaryAxisRange
+    const model = RegionOfInterestAdornmentModel.create()
+    model.setSecondary(secondaryProp)
+    expect(model.secondary).toEqual(secondaryProp)
+  })
+})
