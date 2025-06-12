cdfcce2ec8a5a300fe23dee0038c2a4af1f59715
Scott Cytacki <scytacki@concord.org>
2025-06-12 14:45:13 -0400

    CODAP-740 initial formulas and utilities packages

---
+++ formulas/src/formula.ts
@@ -0,0 +1,57 @@
+import { getEnv, hasEnv, IAnyStateTreeNode, Instance, SnapshotIn, types } from "mobx-state-tree"
+import { typedId } from "codap-utilities"
+import { IFormulaManager } from "./formula-manager-types"
+
+export interface IFormulaManagerEnvironment
+{
+  formulaManager?: IFormulaManager
+}
+
+export function getFormulaManager(node?: IAnyStateTreeNode) {
+  return node && hasEnv(node) ? getEnv<IFormulaManagerEnvironment | undefined>(node)?.formulaManager : undefined
+}
+
+export const Formula = types.model("Formula", {
+  id: types.optional(types.identifier, () => typedId("FORM")),
+  display: ""
+})
+.volatile(self => ({
+  canonical: ""
+}))
+.views(self => ({
+  get formulaManager() {
+    const manager = getFormulaManager(self)
+    if (!manager) {
+      throw new Error("Using the Formula model requires a FormulaManger")
+    }
+    return manager
+  },
+  get empty() {
+    return self.display.length === 0
+  },
+}))
+.views(self => ({
+  get syntaxError() {
+    return self.formulaManager.getSyntaxError(self.display)
+  },
+  get valid() {
+    return !self.empty && !this.syntaxError
+  },
+  get isRandomFunctionPresent() {
+    return self.formulaManager.isRandomFunctionPresent(self.canonical)
+  },
+}))
+.actions(self => ({
+  setDisplayExpression(displayExpression: string) {
+    self.display = displayExpression
+  },
+  setCanonicalExpression(canonicalExpression: string) {
+    self.canonical = canonicalExpression
+  },
+  rerandomize() {
+    self.formulaManager.rerandomize(self.id)
+  }
+}))
+
+export interface IFormula extends Instance<typeof Formula> {}
+export interface IFormulaSnapshot extends SnapshotIn<typeof Formula> {}
