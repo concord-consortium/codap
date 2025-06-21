7d58d215db052b20b3eacea45f26729ec210881e
Kirk Swenson <kswenson@users.noreply.github.com>
2025-06-20 17:12:26 -0700

    feat: support Markov data game

--- v3/src/components/web-view/web-view-utils.ts
+++ v3/src/components/web-view/web-view-utils.ts
@@ -1,12 +1,26 @@
-import { kRootGuideUrl, kRootPluginUrl } from "../../constants"
+import { kRootDataGamesPluginUrl, kRootGuideUrl, kRootPluginUrl } from "../../constants"
 
 export const kRelativePluginRoot = "../../../../extn/plugins"
 export const kRelativeGuideRoot = "../../../../extn/example-documents/guides"
 export const kRelativeURLRoot = "%_url_%/guides"
 
+// Old/new plugin URL mappings stored as tuples of [oldUrl, newUrl] so that we can use
+// a substring match against the old URL to find the new URL.
+const kMappedUrls: Array<[string, string]> = [
+  // v3 version of Markov has been deployed to s3, but not all data games have been migrated
+  ["//concord-consortium.github.io/codap-data-interactives/Markov", `${kRootDataGamesPluginUrl}/Markov/`]
+]
 
 export function processWebViewUrl(url: string) {
   let updatedUrl = url
+
+  // Many plugins were hosted on GitHub pages at http://concord-consortium.github.io/codap-data-interactives/
+  // or other sites but are now hosted on s3, so we have to change the URL to point to the new location.
+  const mappedUrl = kMappedUrls.find(([oldUrl]) => updatedUrl.includes(oldUrl))
+  if (mappedUrl) {
+    updatedUrl = mappedUrl[1]
+  }
+
   // Some plugins relied on index.html being the default file loaded when pointing to a directory.
   // This is no longer the case with our S3 hosted plugins, so we have to modify the path to point to the
   // correct file in this case.
