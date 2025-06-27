a55c24e25b42106bc78cfd37ec42362b43571dfa
William Finzer <wfinzer@concord.org>
2025-06-27 12:28:06 -0700

    [#CODAP-754] Bug fix: Treating an attribute as *numeric* should convert strings containing numbers to numeric values and plot them
    * In the data configuration models when the user has "treat as numeric" and the  we attempt to *extract* a number from the string value.
    * Note that in the various plots, if we're asking for a numeric value, we can assume that if the attribute type is not numeric, we should attempt the extraction.

--- v3/src/components/data-display/data-display-value-utils.ts
+++ v3/src/components/data-display/data-display-value-utils.ts
@@ -1,13 +1,23 @@
+import { extractNumeric } from "../../utilities/math-utils"
 import { convertToDate } from "../../utilities/date-utils"
 import { IDataSet } from "../../models/data/data-set"
 
 // For graphs and map legends, we need date values to be returned as numbers
-export const dataDisplayGetNumericValue = (dataset: IDataSet | undefined, caseID: string, attrID: string) => {
+export const dataDisplayGetNumericValue = (dataset: IDataSet | undefined,
+                                           caseID: string,
+                                           attrID: string,
+                                           extract: boolean = true) => {
   const attr = dataset?.getAttribute(attrID)
   const index = dataset?.getItemIndexForCaseOrItem(caseID)
-  if (attr?.type === 'date' && index != null) {
+  if (attr?.type === 'numeric') {
+    return dataset?.getNumeric(caseID, attrID)
+  }
+  else if (attr?.type === 'date' && index != null) {
     const dateInMS = convertToDate(dataset?.getStrValueAtItemIndex(index, attrID))?.valueOf()
     return dateInMS ? dateInMS / 1000 : undefined
   }
-  return dataset?.getNumeric(caseID, attrID)
+  else if (index != null && extract) {
+    return extractNumeric(dataset?.getStrValueAtItemIndex(index, attrID))
+  }
+  return undefined
 }
