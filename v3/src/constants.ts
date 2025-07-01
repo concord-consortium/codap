3af76dbe5fd11404e734e43b89b2bf1b1e702eea
Kirk Swenson <kswenson@users.noreply.github.com>
2025-07-01 13:00:22 -0700

    chore: use proxied url for onboarding plugin (#2004)

--- v3/src/constants.ts
+++ v3/src/constants.ts
@@ -1,15 +1,18 @@
 import { urlParams } from "./utilities/url-params"
 
 export const kCodapResourcesUrl = "https://codap-resources.concord.org"
+export const kCodap3RootUrl = "https://codap3.concord.org"
 
 export const codapResourcesUrl = (relUrl: string) => `${kCodapResourcesUrl}/${relUrl}`
 
-export const kRootPluginUrl = codapResourcesUrl("plugins")
+export const kRootPluginsUrl = codapResourcesUrl("plugins")
+// for plugins that are proxied from codap3.concord.org
+export const kCodap3RootPluginsUrl = `${kCodap3RootUrl}/plugins`
 export const kRootDataGamesPluginUrl = codapResourcesUrl("plugins/data-games")
 export const kRootGuideUrl = codapResourcesUrl("example-documents/guides")
 
 export function getPluginsRootUrl() {
-  return urlParams.pluginURL || kRootPluginUrl
+  return urlParams.pluginURL || kRootPluginsUrl
 }
 
 export const kImporterPluginUrl = "/Importer/index.html?lang=en-US"
