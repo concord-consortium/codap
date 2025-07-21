fedc3385cd45a6a5ae7bd9b0d83b2bfdb7578b9a
Kirk Swenson <kswenson@users.noreply.github.com>
2025-07-21 16:08:19 -0700

    fix: Getting Started with CODAP 2 example document

--- v3/src/components/web-view/web-view-utils.ts
+++ v3/src/components/web-view/web-view-utils.ts
@@ -6,11 +6,17 @@ export const kRelativeURLRoot = "%_url_%/guides"
 
 // Old/new plugin URL mappings stored as tuples of [oldUrl, newUrl] so that we can use
 // a substring match against the old URL to find the new URL.
-const kMappedUrls: Array<[string, string]> = [
+const kFullyReplacedUrls: Array<[RegExp, string]> = [
   // v3 version of Markov has been deployed to s3, but not all data games have been migrated
-  ["//concord-consortium.github.io/codap-data-interactives/Markov", `${kRootDataGamesPluginUrl}/Markov/`],
+  [/\/concord-consortium.github.io\/codap-data-interactives\/Markov\/?/, `${kRootDataGamesPluginUrl}/Markov/`],
   // onboarding plugins are proxied so that drag/drop works, so we used the proxied url
-  ["/plugins/onboarding/", `${kCodap3RootPluginsUrl}/onboarding/`],
+  [/\/plugins\/onboarding\/?$/, `${kCodap3RootPluginsUrl}/onboarding/`],
+]
+
+const kReplaceToken = "_$@_"
+const kPartiallyReplacedUrls: Array<[RegExp, string]> = [
+  // onboarding plugins are proxied so that drag/drop works, so we used the proxied url
+  [/\/plugins\/onboarding\/(.+)$/, `${kCodap3RootPluginsUrl}/onboarding/${kReplaceToken}`],
 ]
 
 export function processWebViewUrl(url: string) {
@@ -18,9 +24,16 @@ export function processWebViewUrl(url: string) {
 
   // Many plugins were hosted on GitHub pages at http://concord-consortium.github.io/codap-data-interactives/
   // or other sites but are now hosted on s3, so we have to change the URL to point to the new location.
-  const mappedUrl = kMappedUrls.find(([oldUrl]) => updatedUrl.includes(oldUrl))
-  if (mappedUrl) {
-    updatedUrl = mappedUrl[1]
+  const fullyReplacedUrlEntry = kFullyReplacedUrls.find(([urlRegex]) => urlRegex.test(updatedUrl))
+  if (fullyReplacedUrlEntry) {
+    updatedUrl = fullyReplacedUrlEntry[1]
+  }
+
+  for (const [urlRegex, newUrl] of kPartiallyReplacedUrls) {
+    const execResult: RegExpExecArray | null = urlRegex.exec(updatedUrl)
+    if (execResult && execResult.length > 1) {
+      updatedUrl = newUrl.replace(kReplaceToken, execResult[1])
+    }
   }
 
   // Some plugins relied on index.html being the default file loaded when pointing to a directory.
