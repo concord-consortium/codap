8195b1007b00406fa6ddaf6c49a330b31ce2c376
Teale Fristoe <fristoe@gmail.com>
2025-07-24 07:43:21 -0700

    Add sticky beta button.

---
+++ v3/src/components/beta/beta-button.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+import { urlParams } from "../../utilities/url-params"
+
+import FeedbackIcon from "../../assets/icons/beta/feedback-icon.svg"
+
+import "./beta-button.scss"
+
+export function BetaButton() {
+  if (urlParams.release !== "beta") return null
+
+  return (
+    <div className="beta-button-container">
+      <button className="beta-button">
+        <FeedbackIcon className="feedback-icon" />
+        BETA Feedback
+      </button>
+    </div>
+  )
+}
