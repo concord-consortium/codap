239542989b7159af872bba002ff7e704ed198c10
Kirk Swenson <kswenson@users.noreply.github.com>
2025-07-28 12:23:02 -0700

    chore: code review tweak

--- v3/src/v2/codap-v2-data-context-types.ts
+++ v3/src/v2/codap-v2-data-context-types.ts
@@ -207,8 +207,9 @@ export interface ICodapV2GameContextStorage extends ICodapV2DataContextStorage {
   gameState?: any
 }
 
-export type CodapV2Context = ICodapV2DataContext | ICodapV2GameContext | ICodapV2ExternalContext
-export const isV2ExternalContext = (context?: CodapV2Context): context is ICodapV2ExternalContext =>
+export type CodapV2InternalContext = ICodapV2DataContext | ICodapV2GameContext
+export type CodapV2Context = CodapV2InternalContext | ICodapV2ExternalContext
+export const isV2ExternalContext = (context: Maybe<CodapV2Context>): context is ICodapV2ExternalContext =>
   !!context && !("type" in context) && ("externalDocumentId" in context)
-export const isV2InternalContext = (context?: CodapV2Context): context is ICodapV2DataContext | ICodapV2GameContext =>
+export const isV2InternalContext = (context: Maybe<CodapV2Context>): context is CodapV2InternalContext =>
   !!context && ("type" in context)
