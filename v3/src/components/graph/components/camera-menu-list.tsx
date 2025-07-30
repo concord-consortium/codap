8a115c816ec816c2b7214ae41bef84972f44a747
Teale Fristoe <fristoe@gmail.com>
2025-07-30 12:23:48 -0700

    Convert Unimplemented Menu Items from Toast to Under Construction (#2057)
    * Convert remaining toast menu items to under construction.

--- v3/src/components/graph/components/camera-menu-list.tsx
+++ v3/src/components/graph/components/camera-menu-list.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react"
-import { MenuItem, MenuList, useToast } from "@chakra-ui/react"
+import React from "react"
+import { MenuItem, MenuList } from "@chakra-ui/react"
 import { useCfmContext } from "../../../hooks/use-cfm-context"
 import { useTileModelContext } from "../../../hooks/use-tile-model-context"
 import { t } from "../../../utilities/translation/translate"
+import { UnimplementedMenuItem } from "../../beta/unimplemented-menu-item"
 import { isGraphContentModel } from "../models/graph-content-model"
 import { graphSvg } from "../utilities/image-utils"
 
@@ -10,42 +11,6 @@ export const CameraMenuList = () => {
   const tile = useTileModelContext().tile
   const graphModel = isGraphContentModel(tile?.content) ? tile?.content : undefined
   const cfm = useCfmContext()
-  const [hasBackgroundImage, setHasBackgroundImage] = useState(false)
-  const [imageLocked, setImageLocked] = useState(false)
-  const toast = useToast()
-  const handleMenuItemClick = (menuItem: string) => {
-    toast({
-      title: 'Menu item clicked',
-      description: `You clicked on ${menuItem}`,
-      status: 'success',
-      duration: 5000,
-      isClosable: true,
-    })
-  }
-
-  const handleAddBackgroundImage = () => {
-    setHasBackgroundImage(true)
-    handleMenuItemClick("Add Background Image clicked")
-  }
-
-  const handleRemoveBackgroundImage = () => {
-    setHasBackgroundImage(false)
-    handleMenuItemClick("Remove Background Image clicked")
-  }
-
-  const handleUnlockImage = () => {
-    setImageLocked(false)
-    handleMenuItemClick("Unlock Image clicked")
-  }
-
-  const handleLockImage = () => {
-    setImageLocked(true)
-    handleMenuItemClick("Lock Image clicked")
-  }
-
-  const handleCopyImage = () => {
-    handleMenuItemClick("Copy Image clicked")
-  }
 
   const handleExportPNG = async () => {
     if (!graphModel?.renderState) return
@@ -77,27 +42,37 @@ export const CameraMenuList = () => {
 
   return (
     <MenuList data-testid="graph-camera-menu-list">
+      {/* TODO: This is left in for reference when we're ready to implement background images.
       {hasBackgroundImage
         ? <>
-            <MenuItem onClick={handleRemoveBackgroundImage} data-testid="remove-background-image">
-              {t("DG.DataDisplayMenu.removeBackgroundImage")}
-            </MenuItem>
+            <UnimplementedMenuItem
+              label={t("DG.DataDisplayMenu.addBackgroundImage")}
+              testId="add-background-image-disabled"
+            />
             {imageLocked
-              ? <MenuItem onClick={handleUnlockImage} data-testid="unlock-image">
-                  {t("DG.DataDisplayMenu.unlockImageFromAxes")}
-                </MenuItem>
-              : <MenuItem onClick={handleLockImage} data-testid="lock-image">
-                  {t("DG.DataDisplayMenu.lockImageToAxes")}
-                </MenuItem>
+              ? <UnimplementedMenuItem
+                  label={t("DG.DataDisplayMenu.unlockImageFromAxes")}
+                  testId="unlock-image"
+                />
+              : <UnimplementedMenuItem
+                  label={t("DG.DataDisplayMenu.lockImageToAxes")}
+                  testId="lock-image"
+                />
             }
           </>
-        : <MenuItem onClick={handleAddBackgroundImage} data-testid="add-background-image">
-            {t("DG.DataDisplayMenu.addBackgroundImage")}
-          </MenuItem>
-      }
-      <MenuItem data-testid="open-in-draw-tool" onClick={handleCopyImage}>
-        {t("DG.DataDisplayMenu.copyAsImage")}
-      </MenuItem>
+        : <UnimplementedMenuItem
+            label={t("DG.DataDisplayMenu.addBackgroundImage")}
+            testId="add-background-image"
+          />
+      } */}
+      <UnimplementedMenuItem
+        label={t("DG.DataDisplayMenu.addBackgroundImage")}
+        testId="add-background-image"
+      />
+      <UnimplementedMenuItem
+        label={t("DG.DataDisplayMenu.copyAsImage")}
+        testId="open-in-draw-tool"
+      />
       <MenuItem data-testid="export-png-image" onClick={handleExportPNG}>
         {t("DG.DataDisplayMenu.exportPngImage")}
       </MenuItem>
