1c931d53d72603a55b084a0dfff1259ac0e39b75
Kirk Swenson <kswenson@users.noreply.github.com>
2025-07-31 08:04:40 -0700

    feat: beta release saves in v2 format by default

--- v3/src/lib/config.ts
+++ v3/src/lib/config.ts
@@ -1,5 +1,6 @@
+import { isBeta } from "../utilities/version-utils"
 import { DEBUG_SAVE_AS_V2 } from "./debug"
 
 // For now, this is determined by DEBUG flag, but it may be configured by url parameter
 // or some other means eventually.
-export const CONFIG_SAVE_AS_V2 = DEBUG_SAVE_AS_V2
+export const CONFIG_SAVE_AS_V2 = DEBUG_SAVE_AS_V2 || isBeta()
