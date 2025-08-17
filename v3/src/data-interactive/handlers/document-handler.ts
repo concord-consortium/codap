9bd8ef2124269b157d361050477ad8b7c46df0e3
William Finzer <wfinzer@concord.org>
2025-08-17 14:57:39 -0700

    [#CODAP-849] Bug fix: Graph rehydration bugs evidenced in Story Builder
    * The main problem was that applySnapshot was having to recreate the graph's layer model because it was an element of an array and the snapshot did not contain the model's ID. Solution: Models that have arrays of models where those model elements have volatile state that needs to be preserved now register a postImportSnapshotProcessor that will be called for existing tiles. The job of such a processor is to assign IDs to the model elements where the IDs are pulled from the existing models.
    * The above also applies to maps. So maps and graphs now register post-import-snapshot-processor functions that carry out the assigning of IDs where appropriate.
    * `v2MapExporter` was creating a storage object with center and zoom as unserializable proxies. Fixed this using `toJS`.

--- v3/src/data-interactive/handlers/document-handler.ts
+++ v3/src/data-interactive/handlers/document-handler.ts
@@ -205,7 +205,6 @@ export const diDocumentHandler: DIHandler = {
 
     reinstateDatasets().then(() => {
       reinstateComponents()
-    })
 
       // The 500ms timeout here gives the document time to settle down.
       // Especially any changes to center and zoom of a map
