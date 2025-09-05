41eaf5236356cf63b10e05c8883e41b71d6190ad
Ethan McElroy <emcelroy@concord.org>
2025-09-05 11:06:15 -0400

    chore: ensure cancel doesn't close web view with pre-existing URL value.

--- v3/src/components/web-view/web-view-url-modal.tsx
+++ v3/src/components/web-view/web-view-url-modal.tsx
@@ -33,7 +33,7 @@ export const WebViewUrlModal = ({
   }
 
   const closeModal = () => {
-    if (value === "") {
+    if (value === "" && currentValue === "") {
       onRemoveEmptyWebView()
     }
 
