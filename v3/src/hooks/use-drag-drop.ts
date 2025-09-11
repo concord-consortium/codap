0ab4a5bd8348b7f6a1d7865b27a1bc09ce0c589a
Teale Fristoe <fristoe@gmail.com>
2025-09-11 13:31:52 -0700

    Remove stale code.

--- v3/src/hooks/use-drag-drop.ts
+++ v3/src/hooks/use-drag-drop.ts
@@ -1,7 +1,6 @@
 import {
   Active, DataRef, useDndMonitor, useDraggable, UseDraggableArguments, useDroppable, UseDroppableArguments
 } from "@dnd-kit/core"
-import { kDragContainerClass } from "../components/container/container-constants"
 import { IDataSet } from "../models/data/data-set"
 import { useInstanceIdContext } from "./use-instance-id-context"
 import { useTileSelectionContext } from "./use-tile-selection-context"
@@ -20,8 +19,6 @@ export interface IDragAttributeData extends IDragData {
   type: "attribute"
   dataSet: IDataSet | undefined
   attributeId: string
-  initialScrollLeft?: number
-  initialScrollTop?: number
 }
 export function isDragAttributeData(data: DataRef): data is DataRef<IDragAttributeData> {
   return data.current?.type === "attribute"
@@ -43,14 +40,7 @@ export const useDraggableAttribute = ({ prefix, dataSet, attributeId, ...others 
   // DnDKit sets the tabIndex of draggable elements to 0 by default for keyboard accessibility.
   // For now we set it to -1 to meet RDG's expectations and we'll worry about keyboard drag later.
   const attributes = { tabIndex: -1 }
-  const container = document.getElementsByClassName(kDragContainerClass).item(0)
-  const data: IDragAttributeData = {
-    type: "attribute",
-    dataSet,
-    attributeId,
-    initialScrollLeft: container?.scrollLeft,
-    initialScrollTop: container?.scrollTop
-  }
+  const data: IDragAttributeData = { type: "attribute", dataSet, attributeId }
   return useDraggable({ ...others, id: `${prefix}-${attributeId}`, attributes, data })
 }
 
