6992cbc5545c38c908526cf2920a1f4c8ef6de51
William Finzer <wfinzer@concord.org>
2025-10-14 14:58:52 -0700

    CODAP-907 Open image of map in draw tool (#2166)
    * [#CODAP-907] Feature: Maps respond to the "Open in Draw Tool" menu item under the Image icon to open a draw tool plugin with the map image
    
    * Factored out `OpenInDrawTool` so that it can be called from save-image-menu-list.tsx.
    * Provide mapModel with a renderState in map-component.tsx using the analogous code in graph-component.tsx.
    
    * * Changes to 'to dos' suggested by co-pilot in code review.

--- v3/src/components/map/components/inspector-panel/save-image-menu-list.tsx
+++ v3/src/components/map/components/inspector-panel/save-image-menu-list.tsx
@@ -1,18 +1,36 @@
-import { MenuList } from "@chakra-ui/react"
+import { MenuItem, MenuList } from "@chakra-ui/react"
 import React from "react"
 import { ITileModel } from "../../../../models/tiles/tile-model"
 import { t } from "../../../../utilities/translation/translate"
 import { UnimplementedMenuItem } from "../../../beta/unimplemented-menu-item"
+import { openInDrawTool } from "../../../data-display/data-display-image-utils"
+import { isMapContentModel } from "../../models/map-content-model"
 
 interface IProps {
   tile?: ITileModel
 }
 
 export const SaveImageMenuList = ({tile}: IProps) => {
+  const mapModel = isMapContentModel(tile?.content) ? tile?.content : undefined
+
+  const getImageString = async () => {
+    if (!mapModel?.renderState) return ''
+    // TODO: Confirm that updateSnapshot works correctly for map models. If not, add map-specific handling
+    //  or tests to validate its behavior.
+    await mapModel.renderState.updateSnapshot()
+    return mapModel.renderState.dataUri || ''
+  }
 
   return (
     <MenuList data-testid="save-image-menu-list">
-      <UnimplementedMenuItem label={t('DG.DataDisplayMenu.copyAsImage')} />
+      <MenuItem data-testid="open-in-draw-tool" onClick={ async () => {
+        if (tile) {
+          const imageString = await getImageString()
+          await openInDrawTool(tile, imageString)
+        }
+      }}>
+        {t("DG.DataDisplayMenu.copyAsImage")}
+      </MenuItem>
       <UnimplementedMenuItem label={t('DG.DataDisplayMenu.exportPngImage')} />
       <UnimplementedMenuItem label={t('DG.DataDisplayMenu.exportSvgImage')} />
     </MenuList>
--- v3/src/components/map/components/map-component.tsx
+++ v3/src/components/map/components/map-component.tsx
@@ -1,10 +1,12 @@
 import {useDroppable} from '@dnd-kit/core'
 import {observer} from "mobx-react-lite"
-import React, {useRef} from "react"
+import React, { useCallback, useRef } from "react"
 import {InstanceIdContext, useNextInstanceId} from "../../../hooks/use-instance-id-context"
 import {ITileBaseProps} from '../../tiles/tile-base-props'
 import {DataDisplayLayoutContext} from "../../data-display/hooks/use-data-display-layout"
 import {AttributeDragOverlay} from "../../drag-drop/attribute-drag-overlay"
+import { DataDisplayRenderState } from "../../data-display/models/data-display-render-state"
+import { usePixiPointsArray } from "../../data-display/hooks/use-pixi-points-array"
 import {isMapContentModel} from "../models/map-content-model"
 import {MapModelContext} from "../hooks/use-map-model-context"
 import {useInitMapLayout} from "../hooks/use-init-map-layout"
@@ -16,11 +18,25 @@ export const MapComponent = observer(function MapComponent({tile}: ITileBaseProp
   const instanceId = useNextInstanceId("map")
   const layout = useInitMapLayout(mapModel)
   const mapRef = useRef<HTMLDivElement | null>(null)
+  const {pixiPointsArray} = usePixiPointsArray({ addInitialPixiPoints: true })
 
   // used to determine when a dragged attribute is over the map component
   const dropId = `${instanceId}-component-drop-overlay`
   const {setNodeRef} = useDroppable({id: dropId})
   setNodeRef(mapRef.current ?? null)
+
+  // TODO: Investigate whether sharing or passing pixiPointsArray between map-component.tsx and codap-map.tsx
+  // could lead to unintended side effects or synchronization issues. Confirm that pixiPointsArray is not
+  // mutated in codap-map.tsx in a way that affects its usage here, or document the intended data flow.
+  const setMapRef = useCallback((ref: HTMLDivElement | null) => {
+    mapRef.current = ref
+    const elementParent = ref?.parentElement
+    const dataUri = mapModel?.renderState?.dataUri
+    if (elementParent) {
+      const renderState = new DataDisplayRenderState(pixiPointsArray, elementParent, dataUri)
+      mapModel?.setRenderState(renderState)
+    }
+  }, [mapModel, pixiPointsArray])
 
   if (!mapModel) return null
 
@@ -28,7 +44,7 @@ export const MapComponent = observer(function MapComponent({tile}: ITileBaseProp
     <InstanceIdContext.Provider value={instanceId}>
       <DataDisplayLayoutContext.Provider value={layout}>
         <MapModelContext.Provider value={mapModel}>
-          <CodapMap mapRef={mapRef}/>
+          <CodapMap setMapRef={setMapRef}/>
           <AttributeDragOverlay dragIdPrefix={instanceId}/>
         </MapModelContext.Provider>
       </DataDisplayLayoutContext.Provider>
