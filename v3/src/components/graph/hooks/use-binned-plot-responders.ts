802243e604dcdd7d0da80581c3cf007619db6b97
William Finzer <wfinzer@concord.org>
2025-10-20 13:15:53 -0700

    Merge pull request #2186
    * [#CODAP-935] Bug fix: Histogram doesn't update when bar width is changed
    
    * chore: alternative fix

--- v3/src/components/graph/hooks/use-binned-plot-responders.ts
+++ v3/src/components/graph/hooks/use-binned-plot-responders.ts
@@ -1,22 +1,35 @@
-import { comparer } from "mobx"
+import { comparer, observable, runInAction } from "mobx"
 import { useEffect } from "react"
+import { useMemo } from "use-memo-one"
 import { useDataSetContext } from "../../../hooks/use-data-set-context"
 import { mstReaction } from "../../../utilities/mst-reaction"
 import { isBinnedPlotModel } from "../plots/histogram/histogram-model"
 import { useGraphContentModelContext } from "./use-graph-content-model-context"
 
-export const useBinnedPlotResponders = (refreshPointPositions: (selected: boolean) => void) => {
+export const useBinnedPlotResponders = (refreshFn: (selected: boolean) => void) => {
   const graphModel = useGraphContentModelContext()
   const dataset = useDataSetContext()
 
-  // Respond to binAlignment and binWidth changes
+  // stash the current refresh function in a MobX observable property
+  const observableRefreshFn = useMemo(() => observable.box<Maybe<(selected: boolean) => void>>(), [])
+
+  // update the observable property when the refresh function changes
+  useEffect(() => {
+    if (refreshFn !== observableRefreshFn.get()) {
+      runInAction(() => {
+        observableRefreshFn.set(refreshFn)
+      })
+    }
+  }, [observableRefreshFn, refreshFn])
+
+  // Respond to binAlignment, binWidth, and refresh function changes
   useEffect(function respondToGraphBinSettings() {
     return mstReaction(
       () => {
         const plot = graphModel.plot
-        return isBinnedPlotModel(plot) ? [plot.binAlignment, plot.binWidth] : []
+        return isBinnedPlotModel(plot) ? [plot.binAlignment, plot.binWidth, observableRefreshFn.get()] as const : []
       },
-      () => refreshPointPositions(false),
+      ([binAlignment, binWidth, _refreshFn]) => _refreshFn?.(false),
       {name: "respondToGraphBinSettings", equals: comparer.structural}, [graphModel, graphModel.plot])
-  }, [dataset, graphModel, refreshPointPositions])
+  }, [dataset, graphModel, observableRefreshFn])
 }
