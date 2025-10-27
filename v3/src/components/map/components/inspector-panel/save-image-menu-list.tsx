f14d5b1d14e6a0917302d80e3986eaa842df2cfa
Doug Martin <dmartin@concord.org>
2025-10-27 09:52:54 -0400

    feat: Export maps as pngs and svg [CODAP-908]
    This commit adds functionality to export map visualizations as PNG or SVG images.
    
    It uses the 'html-to-image' library to convert the map's display element
    into a PNG/SVG data URI, which is then downloaded as a file or is passed to the
    drawing tool (as a PNG).
    
    TODO:
    
    1. Add a progress indicator while the image is being generated.
    2. Handle translatable error display during the image generation process.

--- v3/src/components/map/components/inspector-panel/save-image-menu-list.tsx
+++ v3/src/components/map/components/inspector-panel/save-image-menu-list.tsx
@@ -1,38 +1,90 @@
 import { MenuItem, MenuList } from "@chakra-ui/react"
 import React from "react"
+import { toPng, toSvg } from 'html-to-image';
+
 import { ITileModel } from "../../../../models/tiles/tile-model"
 import { t } from "../../../../utilities/translation/translate"
-import { UnimplementedMenuItem } from "../../../beta/unimplemented-menu-item"
 import { openInDrawTool } from "../../../data-display/data-display-image-utils"
 import { isMapContentModel } from "../../models/map-content-model"
+import { useCfmContext } from "../../../../hooks/use-cfm-context"
+
+type ExportableFormat = "png" | "svg"
 
 interface IProps {
   tile?: ITileModel
 }
 
 export const SaveImageMenuList = ({tile}: IProps) => {
+  const cfm = useCfmContext()
   const mapModel = isMapContentModel(tile?.content) ? tile?.content : undefined
 
-  const getImageString = async () => {
-    if (!mapModel?.renderState) return ''
-    // TODO: Confirm that updateSnapshot works correctly for map models. If not, add map-specific handling
-    //  or tests to validate its behavior.
-    await mapModel.renderState.updateSnapshot()
-    return mapModel.renderState.dataUri || ''
+  const getImageDataUri = async (format: ExportableFormat) => {
+    if (!mapModel?.renderState) {
+      return undefined
+    }
+    const {displayElement} = mapModel.renderState
+    let dataUri: string|undefined = undefined
+
+    // TODO: show a "in progress" indicator while the image is being generated
+    // as it can take a noticeable amount of time
+    try {
+      dataUri = await (format === "png"
+        ? toPng(displayElement)
+        : toSvg(displayElement))
+    } catch (error) {
+      console.error(`Error generating ${format.toUpperCase()} image:`, error)
+      // TODO: report error to user in translatable string
+    }
+
+    return dataUri
+  }
+
+  const getImageDataString = async (format: ExportableFormat) => {
+    const dataUri = await getImageDataUri(format)
+    const parts = dataUri?.split(",")
+    if (parts?.length === 2) {
+      if (parts[0].indexOf("charset=utf-8") >= 0) {
+        return decodeURIComponent(parts[1])
+      }
+      return parts[1]
+    }
+    return undefined
+  }
+
+  const handleOpenInDrawTool = async () => {
+    if (tile) {
+      const imageDataUri = await getImageDataUri("png")
+      await openInDrawTool(tile, imageDataUri || "")
+    }
+  }
+
+  const handleExportPNG = async () => {
+    const imageString = await getImageDataString("png")
+    if (imageString) {
+      cfm?.client.saveSecondaryFileAsDialog(imageString, "png", "image/png", () => null)
+    }
+  }
+
+  const handleExportSVG = async () => {
+    const imageString = await getImageDataString("svg")
+    if (imageString) {
+      // NOTE: Using application/octet-stream so that the CFM does not try to convert from base64
+      // as it does by default for image-based mime types.
+      cfm?.client.saveSecondaryFileAsDialog(imageString, "svg", "application/octet-stream", () => null)
+    }
   }
 
   return (
     <MenuList data-testid="save-image-menu-list">
-      <MenuItem data-testid="open-in-draw-tool" onClick={ async () => {
-        if (tile) {
-          const imageString = await getImageString()
-          await openInDrawTool(tile, imageString)
-        }
-      }}>
+      <MenuItem data-testid="open-in-draw-tool" onClick={handleOpenInDrawTool}>
         {t("DG.DataDisplayMenu.copyAsImage")}
       </MenuItem>
-      <UnimplementedMenuItem label={t('DG.DataDisplayMenu.exportPngImage')} />
-      <UnimplementedMenuItem label={t('DG.DataDisplayMenu.exportSvgImage')} />
+      <MenuItem data-testid="export-png-image" onClick={handleExportPNG}>
+        {t("DG.DataDisplayMenu.exportPngImage")}
+      </MenuItem>
+      <MenuItem data-testid="export-svg-image" onClick={handleExportSVG}>
+        {t("DG.DataDisplayMenu.exportSvgImage")}
+      </MenuItem>
     </MenuList>
   )
 }
