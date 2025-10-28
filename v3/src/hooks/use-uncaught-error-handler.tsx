a4ffe0e483a483e1f117ebe6730459cb06a9d1a2
Kirk Swenson <kswenson@users.noreply.github.com>
2025-10-28 13:52:36 -0700

    chore: ignore error message in Safari

--- v3/src/hooks/use-uncaught-error-handler.tsx
+++ v3/src/hooks/use-uncaught-error-handler.tsx
@@ -7,7 +7,8 @@ import { t } from "../utilities/translation/translate"
 const errorsToIgnore = [
   // This error occurs when Google Docs sends a non-JSON postMessage that is ignorable.
   // Ideally, iFramePhone would handle this internally, but for now we just ignore it here.
-  "Unexpected token '!', \"!_{\"h\":\"\"}\" is not valid JSON"
+  "Unexpected token '!', \"!_{\"h\":\"\"}\" is not valid JSON",
+  "JSON Parse error: Unrecognized token '!'"
 ]
 
 export function useUncaughtErrorHandler(cfm: CloudFileManager) {
