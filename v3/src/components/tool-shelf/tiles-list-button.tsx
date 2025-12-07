6443a0db88444ebfc248b8c20bffe4f8e00c0e7f
Kirk Swenson <kswenson@users.noreply.github.com>
2025-12-06 17:20:44 -0800

    fix: don't show hidden tiles in Tiles menu (#2249)

--- v3/src/components/tool-shelf/tiles-list-button.tsx
+++ v3/src/components/tool-shelf/tiles-list-button.tsx
@@ -1,16 +1,18 @@
-import React from "react"
-import { observer } from "mobx-react-lite"
-import { clsx } from "clsx"
 import { Menu, MenuButton, MenuItem, MenuList, useDisclosure } from "@chakra-ui/react"
+import { clsx } from "clsx"
+import { observer } from "mobx-react-lite"
+import React from "react"
+import WebViewIcon from "../../assets/icons/icon-media-tool.svg"
+import TileListIcon from "../../assets/icons/icon-tile-list.svg"
 import { useDocumentContent } from "../../hooks/use-document-content"
+import { isFreeTileLayout, isFreeTileRow } from "../../models/document/free-tile-row"
 import { persistentState } from "../../models/persistent-state"
+import { getTileComponentIcon } from "../../models/tiles/tile-component-info"
+import { getTileContentInfo } from "../../models/tiles/tile-content-info"
+import { ITileModel } from "../../models/tiles/tile-model"
 import { uiState } from "../../models/ui-state"
 import { getSpecialLangFontClassName, t } from "../../utilities/translation/translate"
 import { ToolShelfButtonTag } from "./tool-shelf-button"
-import { getTileComponentIcon } from "../../models/tiles/tile-component-info"
-import { getTileContentInfo } from "../../models/tiles/tile-content-info"
-import WebViewIcon from "../../assets/icons/icon-media-tool.svg"
-import TileListIcon from "../../assets/icons/icon-tile-list.svg"
 import { handleSelectTile } from "./tool-shelf-utilities"
 
 import "./tool-shelf.scss"
@@ -18,6 +20,13 @@ import "./tool-shelf.scss"
 export const TilesListShelfButton = observer(function TilesListShelfButton() {
   const documentContent = useDocumentContent()
   const tilesArr = documentContent?.tileMap ? Array.from(documentContent.tileMap.values()) : []
+  // assume the tile is in the first row, since CODAP currently only supports one row
+  const container = documentContent?.rowMap.get(documentContent?.rowOrder[0])
+  const freeTileContainer = isFreeTileRow(container) ? container : undefined
+  const isTileHidden = (tile: ITileModel) => {
+    const tileLayout = freeTileContainer?.getTileLayout(tile.id)
+    return isFreeTileLayout(tileLayout) ? tileLayout.isHidden : false
+  }
   const langClass = getSpecialLangFontClassName()
   const {isOpen, onOpen, onClose} = useDisclosure()
 
@@ -47,7 +56,7 @@ export const TilesListShelfButton = observer(function TilesListShelfButton() {
           />
         </MenuButton>
         <MenuList className="tool-shelf-menu-list top-menu tiles-list" data-testid="tiles-list-menu" >
-          {tilesArr?.map((tile) => {
+          {tilesArr?.filter(tile => !isTileHidden(tile)).map((tile) => {
             const tileType = tile.content.type
             const _Icon = getTileComponentIcon(tileType)
             const Icon = _Icon ?? WebViewIcon
