193c771c2618a3a6dabc0d673c51a04aba2742cd
Kirk Swenson <kswenson@users.noreply.github.com>
2025-12-17 12:00:06 -0800

    fix: bugs in prior implementation of `di-override` url param

--- v3/src/components/web-view/web-view-model.test.ts
+++ v3/src/components/web-view/web-view-model.test.ts
@@ -1,9 +1,19 @@
 import iframePhone from "iframe-phone"
 import { getSnapshot } from "mobx-state-tree"
+import { setUrlParams } from "../../utilities/url-params"
 import { WebViewModel } from "./web-view-model"
 
-describe('WebViewContentModel', () => {
-  it('prepareSnapshot works correctly', async () => {
+describe("WebViewContentModel", () => {
+  it("performs url parameter processing in preProcessSnapshot", () => {
+    const originalUrl = "http://example.com"
+    const dataInteractiveUrl = "http://data-interactive-url.com"
+    setUrlParams(`?di-override=example&di=${encodeURIComponent(dataInteractiveUrl)}`)
+    const webView = WebViewModel.create({ url: originalUrl })
+
+    // URL should be replaced by data interactive URL from URL params
+    expect(webView.url).toBe(dataInteractiveUrl)
+  })
+  it("prepareSnapshot works correctly", async () => {
     const state = { status: "Looking good" }
     const dataInteractiveController = {
       call(message: string, callback: iframePhone.ListenerCallback) {
