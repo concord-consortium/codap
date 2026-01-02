d4f60e86dcbf160901c00b069e0e714d0e0b0d74
William Finzer <wfinzer@concord.org>
2026-01-01 17:12:30 -0800

    [#CODAP-188] Bug fix: Slider thumb in Safari leaves gunk behind when dragging the thumb from right to left
    * The slider thumb, when pressed, was adding a drop shadow to help the user know that something had taken place. It turns out that Safari doesn't deal properly with erasing the drop shadow when the element moves. The solution is *not* to use a drop shadow at all, but to add a stroke to the SVG that draws the thumb. We do this no matter the browser and I think it looks better than the somewhat confusing drop shadow.
    * We also note that there was no hover tip for the slider thumb. (There is in V2.) Simply adding a `title` doesn't work due to the way React treats svg. We had to make a ThumbIcon component that renders the svg *and* has its own `title`.

--- v3/src/components/slider/slider-thumb.tsx
+++ v3/src/components/slider/slider-thumb.tsx
@@ -1,9 +1,10 @@
 import { clsx } from "clsx"
 import { observer } from "mobx-react-lite"
 import React, {CSSProperties, useEffect, useState, useRef} from "react"
-import ThumbIcon from "../../assets/icons/icon-thumb.svg"
 import { isAliveSafe } from "../../utilities/mst-utils"
+import { t } from "../../utilities/translation/translate"
 import { useAxisLayoutContext } from "../axis/models/axis-layout-context"
+import { ThumbIcon } from "./thumb-icon"
 import { ISliderModel } from "./slider-model"
 import { valueChangeNotification } from "./slider-utils"
 import { useSliderAnimation } from "./use-slider-animation"
@@ -106,6 +107,7 @@ export const CodapSliderThumb = observer(function CodapSliderThumb({
       onPointerDown={handlePointerDown}
       style={thumbStyle}
       data-testid="slider-thumb-icon"
+      title={t("DG.SliderView.thumbView.toolTip")}
     />
   )
 })
