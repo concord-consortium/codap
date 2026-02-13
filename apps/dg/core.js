2fda1d5542e6ad42c07a8f3c6f4d3ddc500fb69d
Kirk Swenson <kswenson@users.noreply.github.com>
2026-02-12 18:38:52 -0800

    Build 0743

--- apps/dg/core.js
+++ apps/dg/core.js
@@ -128,7 +128,7 @@ DG = SC.Application.create((function () // closure
     /*
      * Build number
      */
-    BUILD_NUM: '0742',
+    BUILD_NUM: '0743',
 
     IS_DG_BUILD: isDGBuild.property(),
 
